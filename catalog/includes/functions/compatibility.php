cf36f556b0f93fb04d41aa56f111610df26fa14d
Harald Ponce de Leon <hpdl@oscommerce.com>
2007-04-23 19:52:01 +0000

    update register_globals compatibility layer logic

--- catalog/includes/functions/compatibility.php
+++ catalog/includes/functions/compatibility.php
@@ -27,17 +27,16 @@
     }
   }
 
-// $HTTP_xxx_VARS are always set on php4
-  if (!is_array($HTTP_GET_VARS)) $HTTP_GET_VARS = array();
-  if (!is_array($HTTP_POST_VARS)) $HTTP_POST_VARS = array();
-  if (!is_array($HTTP_COOKIE_VARS)) $HTTP_COOKIE_VARS = array();
-
-  if ( (PHP_VERSION >= 5.0) && function_exists('ini_get') && (ini_get('register_long_arrays') == false) ) {
+  if (PHP_VERSION >= 4.1) {
     $HTTP_GET_VARS =& $_GET;
     $HTTP_POST_VARS =& $_POST;
     $HTTP_COOKIE_VARS =& $_COOKIE;
     $HTTP_SESSION_VARS =& $_SESSION;
     $HTTP_SERVER_VARS =& $_SERVER;
+  } else {
+    if (!is_array($HTTP_GET_VARS)) $HTTP_GET_VARS = array();
+    if (!is_array($HTTP_POST_VARS)) $HTTP_POST_VARS = array();
+    if (!is_array($HTTP_COOKIE_VARS)) $HTTP_COOKIE_VARS = array();
   }
 
 // handle magic_quotes_gpc turned off.
