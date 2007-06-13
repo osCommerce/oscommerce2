9fcea119ab69a9f53aeac518fb7d458d66023942
Harald Ponce de Leon <hpdl@oscommerce.com>
2007-06-13 22:52:50 +0000

    fix typo

--- catalog/products_new.php
+++ catalog/products_new.php
@@ -90,7 +90,7 @@
             <td align="right" valign="middle" class="main"><?php echo '<a href="' . tep_href_link(FILENAME_PRODUCTS_NEW, tep_get_all_get_params(array('action')) . 'action=buy_now&products_id=' . $products_new['products_id']) . '">' . tep_image_button('button_in_cart.gif', IMAGE_BUTTON_IN_CART) . '</a>'; ?></td>
           </tr>
           <tr>
-            <td colspüan="3"><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+            <td colspan="3"><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
           </tr>
 <?php
     }
