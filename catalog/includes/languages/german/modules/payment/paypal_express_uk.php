c2213d382f16921e487931937b6629285337dc2d
Harald Ponce de Leon <hpdl@oscommerce.com>
2007-12-18 13:04:04 +0000

    update module description

--- catalog/includes/languages/german/modules/payment/paypal_express_uk.php
+++ catalog/includes/languages/german/modules/payment/paypal_express_uk.php
@@ -12,7 +12,7 @@
 
   define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_TEXT_TITLE', 'PayPal Express Checkout (UK)');
   define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_TEXT_PUBLIC_TITLE', 'PayPal Express');
-  define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">PayPal Webseite besuchen</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalExpressUKInfo\');">(info)</a><span id="paypalExpressUKInfo" style="display: none;"><br><i>Using the above link to signup at PayPal grants osCommerce a small financial bonus for referring a customer.</i></span>');
+  define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_TEXT_DESCRIPTION', '<b>Note: PayPal requires the PayPal Website Payments Pro (UK) Direct Payments payment module to be enabled if this module is activated.</b><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">PayPal Webseite besuchen</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalExpressUKInfo\');">(info)</a><span id="paypalExpressUKInfo" style="display: none;"><br><i>Using the above link to signup at PayPal grants osCommerce a small financial bonus for referring a customer.</i></span>');
   define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_TEXT_BUTTON', 'Express-Kauf mit PayPal');
   define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_ERROR_GENERAL', 'Error: A general problem has occurred with the transaction. Please try again.');
   define('MODULE_PAYMENT_PAYPAL_EXPRESS_UK_ERROR_CFG_ERROR', 'Error: Payment module configuration error. Please verify the login credentials.');
