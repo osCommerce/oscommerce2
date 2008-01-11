b3bc66a0ae7ff38e6c74a533e024df28b7a86814
Harald Ponce de Leon <hpdl@oscommerce.com>
2008-01-11 13:21:23 +0000

    translations

--- catalog/includes/languages/german/modules/payment/paypal_direct.php
+++ catalog/includes/languages/german/modules/payment/paypal_direct.php
@@ -5,22 +5,22 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2007 osCommerce
+  Copyright (c) 2008 osCommerce
 
   Released under the GNU General Public License
 */
 
   define('MODULE_PAYMENT_PAYPAL_DIRECT_TEXT_TITLE', 'PayPal Website Payments Pro Direct Payments (US)');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_TEXT_PUBLIC_TITLE', 'Kredit- oder Debitkarte (Sichere bezahlung durch PayPal)');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_TEXT_DESCRIPTION', '<b>Note: PayPal requires the PayPal Express Checkout payment module to be enabled if this module is activated.</b><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit PayPal Website</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalDirectInfo\');">(info)</a><span id="paypalDirectInfo" style="display: none;"><br><i>Using the above link to signup at PayPal grants osCommerce a small financial bonus for referring a customer.</i></span>');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_OWNER', 'Card Owner:');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_TYPE', 'Card Type:');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_NUMBER', 'Card Number:');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_VALID_FROM', 'Card Valid From Date:');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_VALID_FROM_INFO', '(if available)');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_EXPIRES', 'Card Expiry Date:');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_CVC', 'Card Security Code (CVV2):');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_ISSUE_NUMBER', 'Card Issue Number:');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_ISSUE_NUMBER_INFO', '(for Maestro and Solo cards only)');
-  define('MODULE_PAYMENT_PAYPAL_DIRECT_ERROR_ALL_FIELDS_REQUIRED', 'Error: All payment information fields are required.');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_TEXT_PUBLIC_TITLE', 'Kredit- oder Debitkarte (Sichere Bezahlung durch PayPal)');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_TEXT_DESCRIPTION', '<b>Hinweis: PayPal ben&ouml;tigt die Aktivierung des PayPal Express Checkout Zahlungsmodul wenn dieses Modul installiert ist.</b><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">PayPal Webseite besuchen</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalDirectInfo\');">(info)</a><span id="paypalDirectInfo" style="display: none;"><br><i>Bei benutzung dieses Links gibt osCommerce f&uuml;r Neukunden einen kleinen Bonus.</i></span>');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_OWNER', 'Karteninhaber:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_TYPE', 'Kartentyp:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_NUMBER', 'Kartennummer:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_VALID_FROM', 'g&uuml;ltig von:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_VALID_FROM_INFO', '(falls vorhanden)');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_EXPIRES', 'g&uuml;ltig bis:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_CVC', 'Kartenpr&uuml;fnummer:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_ISSUE_NUMBER', 'Kartenausgabenummer:');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_CARD_ISSUE_NUMBER_INFO', '(nur f&uuml;r Maestro und Solo Karten)');
+  define('MODULE_PAYMENT_PAYPAL_DIRECT_ERROR_ALL_FIELDS_REQUIRED', 'Fehler: Alle Eingabefeld sind ben&ouml;tigt.');
 ?>
--- catalog/includes/languages/german/modules/payment/paypal_uk_direct.php
+++ catalog/includes/languages/german/modules/payment/paypal_uk_direct.php
@@ -12,17 +12,17 @@
 
   define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_TEXT_TITLE', 'PayPal Website Payments Pro (UK) Direct Payments');
   define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_TEXT_PUBLIC_TITLE', 'Kredit- oder Debitkarte (Sichere bezahlung durch PayPal)');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_TEXT_DESCRIPTION', '<b>Note: PayPal requires the PayPal Express Checkout (UK) payment module to be enabled if PayPal Direct (UK) is activated.</b><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">PayPal Webseite besuchen</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalDirectUKInfo\');">(info)</a><span id="paypalDirectUKInfo" style="display: none;"><br><i>Using the above link to signup at PayPal grants osCommerce a small financial bonus for referring a customer.</i></span>');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_OWNER', 'Card Owner:');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_TYPE', 'Card Type:');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_NUMBER', 'Card Number:');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_VALID_FROM', 'Card Valid From Date:');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_VALID_FROM_INFO', '(if available)');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_EXPIRES', 'Card Expiry Date:');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_CVC', 'Card Security Code (CVV2):');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_ISSUE_NUMBER', 'Card Issue Number:');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_ISSUE_NUMBER_INFO', '(for Maestro and Solo cards only)');
-  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_ERROR_ALL_FIELDS_REQUIRED', 'Error: All payment information fields are required.');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_TEXT_DESCRIPTION', '<b>Hinweis: PayPal ben&ouml;tigt die Aktivierung des PayPal Express Checkout (UK) Zahlungsmodul wenn dieses Modul installiert ist.</b><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">PayPal Webseite besuchen</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalDirectUKInfo\');">(info)</a><span id="paypalDirectUKInfo" style="display: none;"><br><i>Bei benutzung dieses Links gibt osCommerce f&uuml;r Neukunden einen kleinen Bonus.</i></span>');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_OWNER', 'Karteninhaber:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_TYPE', 'Kartentyp:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_NUMBER', 'Kartennummer:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_VALID_FROM', 'g&uuml;ltig von:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_VALID_FROM_INFO', '(falls vorhanden)');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_EXPIRES', 'g&uuml;ltig bis:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_CVC', 'Kartenpr&uuml;fnummer:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_ISSUE_NUMBER', 'Kartenausgabenummer:');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_CARD_ISSUE_NUMBER_INFO', '(nur f&uuml;r Maestro und Solo Karten)');
+  define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_ERROR_ALL_FIELDS_REQUIRED', 'Fehler: Alle Eingabefeld sind ben&ouml;tigt.');
   define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_ERROR_GENERAL', 'Error: A general problem has occurred with the transaction. Please try again.');
   define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_ERROR_CFG_ERROR', 'Error: Payment module configuration error. Please verify the login credentials.');
   define('MODULE_PAYMENT_PAYPAL_UK_DIRECT_ERROR_ADDRESS', 'Error: A match of the Shipping Address City, State, and Postal Code failed. Please try again.');
