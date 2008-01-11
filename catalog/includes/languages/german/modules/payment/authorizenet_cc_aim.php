cf4024e8e9aaa32e4eadbdff46637ee24444f88c
Nick Weisser <nick@openstream.ch>
2008-01-11 04:08:40 +0000

    Corrected unprecise translation from RC1

--- catalog/includes/languages/german/modules/payment/authorizenet_cc_aim.php
+++ catalog/includes/languages/german/modules/payment/authorizenet_cc_aim.php
@@ -17,7 +17,7 @@
   define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_CREDIT_CARD_NUMBER', 'Kreditkarten-Nr.:');
   define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_CREDIT_CARD_EXPIRES', 'G&uuml;ltig bis:');
   define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_CREDIT_CARD_CVC', 'Kreditkartenprüfnummer (CVC):');
-  define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_ERROR_TITLE', 'Fehler bei der &Uuml;berpr&uuml;fung der Kreditkarte');
+  define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_ERROR_TITLE', 'Fehler bei der Verarbeitung der Kreditkarte');
   define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_ERROR_GENERAL', 'Bitte versuchen Sie erneut. Wenn das Problem weiterhin besteht, w&auml;hlen Sie bitte eine andere Zahlungsweise.');
   define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_ERROR_DECLINED', 'Ihre Kreditkarte wurde abgelehnt. Bitte versuchen Sie es mit einer anderen Karte oder w&auml;hlen Sie eine andere Zahlungsweise.');
   define('MODULE_PAYMENT_AUTHORIZENET_CC_AIM_ERROR_INVALID_EXP_DATE', 'Das G&uuml;ltigkeitsdatum der Kreditkarte ist falsch. Bitte &uuml;berpr&uuml;fen Sie die Kreditkarteninformationen.');
