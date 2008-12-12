3f66a16ed75da737da6648144bac7a8b71741f0e
Harald Ponce de Leon <hpdl@oscommerce.com>
2008-12-12 10:54:24 +0000

    Escape shell arguments in the checkdnsrr() compatibility function. Fix OSC-762

--- catalog/admin/includes/functions/compatibility.php
+++ catalog/admin/includes/functions/compatibility.php
@@ -80,7 +80,7 @@
   if (!function_exists('checkdnsrr')) {
     function checkdnsrr($host, $type) {
       if(tep_not_null($host) && tep_not_null($type)) {
-        @exec("nslookup -type=$type $host", $output);
+        @exec("nslookup -type=" . escapeshellarg($type) . " " . escapeshellarg($host), $output);
         while(list($k, $line) = each($output)) {
           if(eregi("^$host", $line)) {
             return true;
