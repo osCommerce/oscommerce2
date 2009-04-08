55655ee13c004dbd1af419ac4715eaf21e34a16e
Harald Ponce de Leon <hpdl@oscommerce.com>
2009-04-08 18:17:10 +0200

    rename module to PayPal Website Payments Pro - Direct Payments

---
+++ catalog/includes/languages/english/modules/payment/paypal_pro_dp.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2009 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_TEXT_TITLE', 'PayPal Website Payments Pro - Direct Payments');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_TEXT_PUBLIC_TITLE', 'Credit or Debit Card');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_TEXT_DESCRIPTION', '<b>Note: PayPal requires the PayPal Express Checkout payment module to be enabled if this module is activated.</b><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit PayPal Website</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalDirectInfo\');">(info)</a><span id="paypalDirectInfo" style="display: none;"><br><i>Using the above link to signup at PayPal grants osCommerce a small financial bonus for referring a customer.</i></span>');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_OWNER', 'Card Owner:');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_TYPE', 'Card Type:');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_NUMBER', 'Card Number:');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_VALID_FROM', 'Card Valid From Date:');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_VALID_FROM_INFO', '(if available)');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_EXPIRES', 'Card Expiry Date:');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_CVC', 'Card Security Code (CVV2):');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_ISSUE_NUMBER', 'Card Issue Number:');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_CARD_ISSUE_NUMBER_INFO', '(for Maestro and Solo cards only)');
+  define('MODULE_PAYMENT_PAYPAL_PRO_DP_ERROR_ALL_FIELDS_REQUIRED', 'Error: All payment information fields are required.');
+?>
