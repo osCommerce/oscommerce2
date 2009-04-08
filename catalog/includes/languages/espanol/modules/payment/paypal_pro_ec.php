8a8ed70ed18fdf339f24da21ab98e469a13a50c5
Harald Ponce de Leon <hpdl@oscommerce.com>
2009-04-08 23:22:36 +0200

    Rename module to PayPal Website Payments Pro - Express Checkout

---
+++ catalog/includes/languages/espanol/modules/payment/paypal_pro_ec.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2009 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYPAL_PRO_EC_TEXT_TITLE', 'PayPal Website Payments Pro - Express Checkout');
+  define('MODULE_PAYMENT_PAYPAL_PRO_EC_TEXT_PUBLIC_TITLE', 'PayPal Express Checkout');
+  define('MODULE_PAYMENT_PAYPAL_PRO_EC_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">Visita la web de PayPal</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalExpressInfo\');">(info)</a><span id="paypalExpressInfo" style="display: none;"><br><i>Con el uso del Link para usar PayPal osCommerce dar a cada Cliente nuevo un pequeno Bonus.</i></span>');
+  define('MODULE_PAYMENT_PAYPAL_PRO_EC_TEXT_BUTTON', 'Pague con PayPal');
+  define('MODULE_PAYMENT_PAYPAL_PRO_EC_TEXT_COMMENTS', 'Comentarios:');
+?>
