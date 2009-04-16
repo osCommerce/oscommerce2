7e535ef4eb2780b725162bf17a6863f8b3c39681
Anthony Hicks <anthony@anthonyjhicks.com>
2009-04-16 18:07:26 +0800

    Update public title for PayPal Express ES/DE/UK
    Signed-off-by: Harald Ponce de Leon <hpdl@oscommerce.com>

--- catalog/includes/languages/german/modules/payment/paypal_standard.php
+++ catalog/includes/languages/german/modules/payment/paypal_standard.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_PAYMENT_PAYPAL_STANDARD_TEXT_TITLE', 'PayPal Website Payments Standard');
+  define('MODULE_PAYMENT_PAYPAL_STANDARD_TEXT_TITLE', 'PayPal Starter');
   define('MODULE_PAYMENT_PAYPAL_STANDARD_TEXT_PUBLIC_TITLE', 'PayPal');
   define('MODULE_PAYMENT_PAYPAL_STANDARD_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://www.paypal.com/mrb/pal=PS2X9Q773CKG4" target="_blank" style="text-decoration: underline; font-weight: bold;">PayPal Webseite besuchen</a>&nbsp;<a href="javascript:toggleDivBlock(\'paypalStdInfo\');">(info)</a><span id="paypalStdInfo" style="display: none;"><br><i>Bei Benutzung dieses Links erh&auml;lt osCommerce f&uuml;r eine Neukundenvermittlung einen kleinen Bonus.</i></span>');
 ?>
