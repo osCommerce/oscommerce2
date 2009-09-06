c3a8407aa74131c59da0fdf3f874f1abe8f75997
Mark Evans <markevans@mark-evanss-macbook-pro.local>
2009-09-06 15:13:58 +0100

    Fix typo in variable name

--- catalog/index.php
+++ catalog/index.php
@@ -16,8 +16,8 @@
   $category_depth = 'top';
   if (isset($cPath) && tep_not_null($cPath)) {
     $categories_products_query = tep_db_query("select count(*) as total from " . TABLE_PRODUCTS_TO_CATEGORIES . " where categories_id = '" . (int)$current_category_id . "'");
-    $cateqories_products = tep_db_fetch_array($categories_products_query);
-    if ($cateqories_products['total'] > 0) {
+    $categories_products = tep_db_fetch_array($categories_products_query);
+    if ($categories_products['total'] > 0) {
       $category_depth = 'products'; // display products
     } else {
       $category_parent_query = tep_db_query("select count(*) as total from " . TABLE_CATEGORIES . " where parent_id = '" . (int)$current_category_id . "'");
