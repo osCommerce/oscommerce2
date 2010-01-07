2fe31e35a349825e12b49b61169f1a089f9d41d2
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-01-07 21:05:51 +0800

    Verify length of email address
    Update tld file
    
    Update the email validation routine to remove the reliance on the tld file
    
    Standards updates

--- catalog/admin/includes/functions/validations.php
+++ catalog/admin/includes/functions/validations.php
@@ -21,99 +21,50 @@
   //
   // Description : function for validating email address that conforms to RFC 822 specs
   //
-  //               This function is converted from a JavaScript written by
-  //               Sandeep V. Tamhankar (stamhankar@hotmail.com). The original JavaScript
-  //               is available at http://javascript.internet.com
+  //              This function will first attempt to validate the Email address using the filter
+  //              extension for performance. If this extension is not available it will
+  //              fall back to a regex based validator which doesn't validate all RFC822
+  //              addresses but catches 99.9% of them. The regex is based on the code found at
+  //              http://www.regular-expressions.info/email.html
+  //
+  //              Optional validation for validating the domain name is also valid is supplied
+  //              and can be enabled using the administration tool.
   //
   // Sample Valid Addresses:
   //
   //    first.last@host.com
   //    firstlast@host.to
-  //    "first last"@host.com
-  //    "first@last"@host.com
   //    first-last@host.com
-  //    first.last@[123.123.123.123]
+  //    first_last@host.com
   //
   // Invalid Addresses:
   //
   //    first last@host.com
-  //
+  //    first@last@host.com
   //
   ////////////////////////////////////////////////////////////////////////////////////////////////
   function tep_validate_email($email) {
-    $valid_address = true;
+    $email = trim($email);
+    if (strlen($email) > 255) {
+      $valid_address = false;
+    } elseif (function_exists('filter_var') && defined('FILTER_VALIDATE_EMAIL')) {
 
-    $mail_pat = '/^(.+)@(.+)$/i';
-    $valid_chars = "[^] \(\)<>@,;:\.\\\"\[]";
-    $atom = "$valid_chars+";
-    $quoted_user='(\"[^\"]*\")';
-    $word = "($atom|$quoted_user)";
-    $user_pat = "/^$word(\.$word)*$/i";
-    $ip_domain_pat='/^\[([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\]$/i';
-    $domain_pat = "/^$atom(\.$atom)*$/i";
-
-    if (preg_match($mail_pat, $email, $components)) {
-      $user = $components[1];
-      $domain = $components[2];
-      // validate user
-      if (preg_match($user_pat, $user)) {
-        // validate domain
-        if (preg_match($ip_domain_pat, $domain, $ip_components)) {
-          // this is an IP address
-      	  for ($i=1;$i<=4;$i++) {
-      	    if ($ip_components[$i] > 255) {
-      	      $valid_address = false;
-      	      break;
-      	    }
-          }
-        }
-        else {
-          // Domain is a name, not an IP
-          if (preg_match($domain_pat, $domain)) {
-            /* domain name seems valid, but now make sure that it ends in a valid TLD or ccTLD
-               and that there's a hostname preceding the domain or country. */
-            $domain_components = explode(".", $domain);
-            // Make sure there's a host name preceding the domain.
-            if (sizeof($domain_components) < 2) {
-              $valid_address = false;
-            } else {
-              $top_level_domain = strtolower($domain_components[sizeof($domain_components)-1]);
-              // Allow all 2-letter TLDs (ccTLDs)
-              if (preg_match('/^[a-z][a-z]$/i', $top_level_domain) != 1) {
-                $tld_pattern = '';
-                // Get authorized TLDs from text file
-                $tlds = file(DIR_WS_INCLUDES . 'tld.txt');
-                while (list(,$line) = each($tlds)) {
-                  // Get rid of comments
-                  $words = explode('#', $line);
-                  $tld = trim($words[0]);
-                  // TLDs should be 3 letters or more
-                  if (preg_match('/^[a-z]{3,}$/i', $tld) == 1) {
-                    $tld_pattern .= '^' . $tld . '$|';
-                  }
-                }
-                // Remove last '|'
-                $tld_pattern = substr($tld_pattern, 0, -1);
-                if (preg_match("/$tld_pattern/i", $top_level_domain) == 0) {
-                    $valid_address = false;
-                }
-              }
-            }
-          }
-          else {
-      	    $valid_address = false;
-      	  }
-      	}
+     $valid_address = (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
+    } else {
+      if ( substr_count( $email, '@' ) > 1 ) {
+        $valid_address = false;
       }
-      else {
+
+      if ( preg_match("/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i", $email)) {
+        $valid_address = true;
+      } else {
         $valid_address = false;
       }
     }
-    else {
-      $valid_address = false;
-    }
+
     if ($valid_address && ENTRY_EMAIL_ADDRESS_CHECK == 'true') {
-      if (!checkdnsrr($domain, "MX") && !checkdnsrr($domain, "A")) {
+      $domain = explode('@', $email);
+      if (!checkdnsrr($domain[1], "MX") && !checkdnsrr($domain[1], "A")) {
         $valid_address = false;
       }
     }
