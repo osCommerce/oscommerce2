d17266d7292b47f26577e835f16d2eed49fe901b
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-01-15 02:48:49 +0800

    New Contact Us action recorder module Verify processing of enquiry with session token ID

---
+++ catalog/includes/languages/german/modules/action_recorder/ar_contact_us.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ACTION_RECORDER_CONTACT_US_TITLE', 'Contact Us');
+?>
