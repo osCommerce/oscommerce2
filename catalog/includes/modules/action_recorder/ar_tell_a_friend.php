7b83e0ced98260521c8bfd5338be9afacb57a351
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-01-14 18:53:04 +0100

    Add language definitions for module titles Add success database table field to show if an action was successful (1) or not (0) Add expireEntries() class method to remove old entries in the database

--- catalog/includes/modules/action_recorder/ar_tell_a_friend.php
+++ catalog/includes/modules/action_recorder/ar_tell_a_friend.php
@@ -11,11 +11,14 @@
 */
 
   class ar_tell_a_friend {
+    var $_title;
     var $_log_retries = false;
     var $_min_minutes = 15;
     var $_identifier;
 
     function ar_tell_a_friend() {
+      $this->_title = MODULE_ACTION_RECORDER_TELL_A_FRIEND_TITLE;
+
       if (defined('MIN_TELL_A_FRIEND_EMAIL_MINUTES') && is_numeric(MIN_TELL_A_FRIEND_EMAIL_MINUTES) && (MIN_TELL_A_FRIEND_EMAIL_MINUTES > 0)) {
         $this->_min_minutes = (int)MIN_TELL_A_FRIEND_EMAIL_MINUTES;
       }
@@ -26,12 +29,16 @@
     function check() {
       global $customer_id;
 
-      $check_query = tep_db_query("select date_added from " . TABLE_ACTION_RECORDER . " where module = 'ar_tell_a_friend' and (" . (tep_session_is_registered('customer_id') ? "customer_id = '" . (int)$customer_id . "' or " : "") . " identifier = '" . tep_db_input($this->_identifier) . "') and date_added >= date_sub(now(), interval " . (int)$this->_min_minutes  . " minute) order by date_added desc limit 1");
+      $check_query = tep_db_query("select date_added from " . TABLE_ACTION_RECORDER . " where module = 'ar_tell_a_friend' and (" . (tep_session_is_registered('customer_id') ? "customer_id = '" . (int)$customer_id . "' or " : "") . " identifier = '" . tep_db_input($this->_identifier) . "') and date_added >= date_sub(now(), interval " . (int)$this->_min_minutes  . " minute) and success = 1 order by date_added desc limit 1");
       if (tep_db_num_rows($check_query)) {
         return false;
       } else {
         return true;
       }
+    }
+
+    function expireEntries() {
+      tep_db_query("delete from " . TABLE_ACTION_RECORDER . " where module = 'ar_tell_a_friend' and date_added < date_sub(now(), interval " . (int)$this->_min_minutes  . " minute)");
     }
   }
 ?>
