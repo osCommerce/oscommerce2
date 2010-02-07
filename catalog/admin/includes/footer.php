63a70734be365059f10843c065d4050e27e5e760
Mark Evans <markkevans@gmail.com>
2010-02-07 22:54:21 +0100

    Update administration tool copyright year

--- catalog/admin/includes/footer.php
+++ catalog/admin/includes/footer.php
@@ -30,7 +30,7 @@
   copyright announcement.
 */
 ?>
-osCommerce Online Merchant Copyright &copy; 2008 <a href="http://www.oscommerce.com" target="_blank">osCommerce</a><br>
+osCommerce Online Merchant Copyright &copy; 2010 <a href="http://www.oscommerce.com" target="_blank">osCommerce</a><br>
 osCommerce provides no warranty and is redistributable under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a>
     </td>
   </tr>
