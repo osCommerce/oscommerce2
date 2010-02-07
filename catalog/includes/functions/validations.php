775fa65738a7f98c65e2aaf8b27258e17e6efa97
Mark Evans <markkevans@gmail.com>
2010-02-07 20:47:29 +0100

    Standards updates

--- catalog/includes/functions/validations.php
+++ catalog/includes/functions/validations.php
@@ -45,17 +45,17 @@
   ////////////////////////////////////////////////////////////////////////////////////////////////
   function tep_validate_email($email) {
     $email = trim($email);
-    if (strlen($email) > 255) {
-      $valid_address = false;
-    } elseif (function_exists('filter_var') && defined('FILTER_VALIDATE_EMAIL')) {
 
+    if ( strlen($email) > 255 ) {
+      $valid_address = false;
+    } elseif ( function_exists('filter_var') && defined('FILTER_VALIDATE_EMAIL') ) {
      $valid_address = (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
     } else {
       if ( substr_count( $email, '@' ) > 1 ) {
         $valid_address = false;
       }
 
-      if ( preg_match("/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i", $email)) {
+      if ( preg_match("/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i", $email) ) {
         $valid_address = true;
       } else {
         $valid_address = false;
@@ -64,10 +64,12 @@
 
     if ($valid_address && ENTRY_EMAIL_ADDRESS_CHECK == 'true') {
       $domain = explode('@', $email);
-      if (!checkdnsrr($domain[1], "MX") && !checkdnsrr($domain[1], "A")) {
+
+      if ( !checkdnsrr($domain[1], "MX") && !checkdnsrr($domain[1], "A") ) {
         $valid_address = false;
       }
     }
+
     return $valid_address;
   }
 ?>
