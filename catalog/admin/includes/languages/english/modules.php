d6c19e3db07c8f3abead06bdbcc6d568ddfffd54
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-02-08 11:37:34 +0100

    Add new API Version tag to modules

--- catalog/admin/includes/languages/english/modules.php
+++ catalog/admin/includes/languages/english/modules.php
@@ -21,6 +21,7 @@ define('TABLE_HEADING_ACTION', 'Action');
 
 define('TEXT_INFO_VERSION', 'Version:');
 define('TEXT_INFO_ONLINE_STATUS', 'online status');
+define('TEXT_INFO_API_VERSION', 'API Version:');
 
 define('TEXT_MODULE_DIRECTORY', 'Module Directory:');
 ?>
