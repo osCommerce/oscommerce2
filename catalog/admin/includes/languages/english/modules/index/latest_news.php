623b75644c0a82a455e8c073177925357b679f88
Mark Evans <markkevans@gmail.com>
2010-02-14 18:53:13 +0000

    Add new index modules for latest news and latest addons. Also added links to the facebook / twitter / rss and newsletter feeds. News and Latest Addons feed is cached for 1 day

---
+++ catalog/admin/includes/languages/english/modules/index/latest_news.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('ADMIN_INDEX_NEWS_TITLE', 'Latest osCommerce News');
+define('ADMIN_INDEX_NEWS_DATE', 'Published Date');
+define('ADMIN_INDEX_NEWS_FEED_ERROR', 'Could not load news feed');
+
+define('ADMIN_INDEX_NEWS_FACEBOOK', 'View the osCommerce Facebook Fan Page');
+define('ADMIN_INDEX_NEWS_TWITTER', 'View the osCommerce Twitter Feed');
+define('ADMIN_INDEX_NEWS_RSS', 'View the osCommerce News RSS Feed');
+define('ADMIN_INDEX_NEWS_NEWSLETTER', 'Signup for the osCommerce Newsletter');
+
+?>
---
+++ catalog/admin/includes/languages/espanol/modules/index/latest_addons.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('ADMIN_INDEX_ADDONS_TITLE', 'Latest osCommerce Addons');
+define('ADMIN_INDEX_ADDONS_DATE', 'Updated Date');
+define('ADMIN_INDEX_ADDONS_FEED_ERROR', 'Could not connect to the addons feed. Please try later');
+
+?>
