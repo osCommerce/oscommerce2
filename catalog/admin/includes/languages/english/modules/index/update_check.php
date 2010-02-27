6094ea31dc6ef8c879a5de7a6e24c192e3d8d0f7
Mark Evans <markkevans@gmail.com>
2010-02-27 08:36:31 +0000

    Initial check-in of version upgrade checker

---
+++ catalog/admin/includes/languages/english/modules/index/update_check.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('ADMIN_INDEX_UPDATE_CHECK_TITLE', 'osCommerce Updates');
+define('ADMIN_INDEX_UPDATE_CHECK_DATE', 'Last Checked on');
+define('ADMIN_INDEX_UPDATE_CHECK_NEVER', 'Never');
+define('ADMIN_INDEX_UPDATE_CHECK_NOW', 'Check Now');
+define('ADMIN_INDEX_UPDATE_RUNNING_LATEST', 'You are running the latest version of osCommerce');
+define('ADMIN_INDEX_UPDATE_UPGRADES_AVAILABLE', 'A new version of osCommerce is available for download.');
+
+define('ADMIN_INDEX_UPDATE_UPGRADES_VERSION', 'Version');
+define('ADMIN_INDEX_UPDATE_UPGRADES_RELEASED', 'Release Date');
+define('ADMIN_INDEX_UPDATE_UPGRADES_DOWNLOAD', 'Download');
+
+?>
