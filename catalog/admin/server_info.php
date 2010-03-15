869423665d360b48f86785398cf3a4a54c159e9d
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-03-15 14:10:19 +0100

    Update version to v2.2; introduce new tep_get_version() function to replace PROJECT_VERSION

--- catalog/admin/server_info.php
+++ catalog/admin/server_info.php
@@ -111,7 +111,7 @@ hr {display: none;}
     $phpinfo = str_replace('border: 1px', '', $phpinfo);
     preg_match('/<body>(.*)<\/body>/is', $phpinfo, $regs);
     echo '<table border="1" cellpadding="3" width="600" style="border: 0px; border-color: #000000;">' .
-         '  <tr><td><a href="http://www.oscommerce.com"><img border="0" src="images/oscommerce.png" title="' . PROJECT_VERSION . '" /></a><h1 class="p"> ' . PROJECT_VERSION . '</h1></td>' . 
+         '  <tr><td><a href="http://www.oscommerce.com"><img border="0" src="images/oscommerce.png" title="osCommerce Online Merchant v' . tep_get_version() . '" /></a><h1 class="p">osCommerce Online Merchant v' . tep_get_version() . '</h1></td>' .
          '  </tr>' .
          '</table>';
     echo $regs[1];
