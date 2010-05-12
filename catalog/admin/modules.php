df965c7de4bd66b5c5ca810edd3348093137e0a2
Shadow-Lord <sales@itvault.com>
2010-05-12 09:45:20 +0800

    Payment and Shipping Modules not updating for some users
    standards updates

--- catalog/admin/modules.php
+++ catalog/admin/modules.php
@@ -43,6 +43,7 @@
   if (tep_not_null($action)) {
     switch ($action) {
       case 'save':
+        reset($HTTP_POST_VARS['configuration']);
         while (list($key, $value) = each($HTTP_POST_VARS['configuration'])) {
           tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '" . $value . "' where configuration_key = '" . $key . "'");
         }
