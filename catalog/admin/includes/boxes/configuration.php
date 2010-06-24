407eef5043aed1d680bcaceb8a010db444f56ca7
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-06-24 21:57:54 +0800

    New Store Logo page to upload a new store logo

--- catalog/admin/includes/boxes/configuration.php
+++ catalog/admin/includes/boxes/configuration.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2007 osCommerce
+  Copyright (c) 2010 osCommerce
 
   Released under the GNU General Public License
 */
@@ -21,7 +21,8 @@
                      'link'  => tep_href_link(FILENAME_CONFIGURATION, 'gID=1&selected_box=configuration'));
 
   if ($selected_box == 'configuration') {
-    $cfg_groups = '<a href="' . tep_href_link(FILENAME_ADMINISTRATORS, '', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_CONFIGURATION_ADMINISTRATORS . '</a><br>';
+    $cfg_groups = '<a href="' . tep_href_link(FILENAME_ADMINISTRATORS, '', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_CONFIGURATION_ADMINISTRATORS . '</a><br>' .
+                  '<a href="' . tep_href_link(FILENAME_STORE_LOGO, '', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_CONFIGURATION_STORE_LOGO . '</a><br>';
     $configuration_groups_query = tep_db_query("select configuration_group_id as cgID, configuration_group_title as cgTitle from " . TABLE_CONFIGURATION_GROUP . " where visible = '1' order by sort_order");
     while ($configuration_groups = tep_db_fetch_array($configuration_groups_query)) {
       $cfg_groups .= '<a href="' . tep_href_link(FILENAME_CONFIGURATION, 'gID=' . $configuration_groups['cgID'], 'NONSSL') . '" class="menuBoxContentLink">' . $configuration_groups['cgTitle'] . '</a><br>';
---
+++ catalog/admin/includes/languages/english/store_logo.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Store Logo');
+
+define('TEXT_LOGO_IMAGE', 'New Logo:');
+define('TEXT_FORMAT_AND_LOCATION', 'The store logo must be in PNG format and will be saved as:');
+
+define('SUCCESS_LOGO_UPDATED', 'Success: The store logo has successfully been updated!');
+
+define('ERROR_IMAGES_DIRECTORY_NOT_WRITEABLE', 'Error: The images directory cannot be uploaded to. (<a href="%s">click here to review the directory permissions</a>)');
+?>
