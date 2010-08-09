10499e7f44c975022dc4b778398020cc60a74ded
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-08-09 10:09:06 +0200

    New Facebook Like social bookmark module

---
+++ catalog/includes/languages/english/modules/social_bookmarks/sb_facebook_like.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_SOCIAL_BOOKMARKS_FACEBOOK_LIKE_TITLE', 'Facebook Like');
+  define('MODULE_SOCIAL_BOOKMARKS_FACEBOOK_LIKE_DESCRIPTION', 'Share products on Facebook Like.');
+  define('MODULE_SOCIAL_BOOKMARKS_FACEBOOK_LIKE_PUBLIC_TITLE', 'Share on Facebook');
+?>
