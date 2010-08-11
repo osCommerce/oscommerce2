5a972f3cdea7b6fe8196b852cd4c5d3d0220e36b
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-08-11 11:35:24 +0200

    New social bookmark module for Twitter Tweet Button

---
+++ catalog/includes/languages/english/modules/social_bookmarks/sb_twitter_button.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_SOCIAL_BOOKMARKS_TWITTER_BUTTON_TITLE', 'Twitter Button');
+  define('MODULE_SOCIAL_BOOKMARKS_TWITTER_BUTTON_DESCRIPTION', 'Share products with Twitter Button.');
+  define('MODULE_SOCIAL_BOOKMARKS_TWITTER_BUTTON_PUBLIC_TITLE', 'Tweet');
+?>
