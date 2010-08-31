a96ba9780df4392cbbc081d98a4eeb30f5953cec
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-08-31 20:39:38 +0200

    Add a Google Analytics header tag module

---
+++ catalog/includes/languages/english/modules/header_tags/ht_google_analytics.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_GOOGLE_ANALYTICS_TITLE', 'Google Analytics');
+  define('MODULE_HEADER_TAGS_GOOGLE_ANALYTICS_DESCRIPTION', 'Add Google Analytics to the shop');
+?>
