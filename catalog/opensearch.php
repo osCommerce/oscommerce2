021d8e2274deee65b411b2c53414fab841aa894e
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-08-31 22:33:57 +0200

    Update OpenSearch header tag module with parameters

--- catalog/opensearch.php
+++ catalog/opensearch.php
@@ -14,10 +14,40 @@
 
   require('includes/application_top.php');
 
+  if ( !defined('MODULE_HEADER_TAGS_OPENSEARCH_STATUS') || (MODULE_HEADER_TAGS_OPENSEARCH_STATUS != 'True') ) {
+    exit;
+  }
+
   echo '<?xml version="1.0"?>' . "\n";
 ?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-  <ShortName><?php echo tep_output_string(STORE_NAME); ?></ShortName>
+  <ShortName><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SHORT_NAME); ?></ShortName>
+  <Description><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_DESCRIPTION); ?></Description>
+<?php
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_CONTACT)) {
+    echo '  <Contact>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_CONTACT) . '</Contact>' . "\n";
+  }
+
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_TAGS)) {
+    echo '  <Tags>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_TAGS) . '</Tags>' . "\n";
+  }
+
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_ATTRIBUTION)) {
+    echo '  <Attribution>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_ATTRIBUTION) . '</Attribution>' . "\n";
+  }
+
+  if (MODULE_HEADER_TAGS_OPENSEARCH_ADULT_CONTENT == 'True') {
+    echo '  <AdultContent>True</AdultContent>' . "\n";
+  }
+
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_ICON)) {
+    echo '  <Image height="16" width="16" type="image/x-icon">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_ICON) . '</Image>' . "\n";
+  }
+
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_IMAGE)) {
+    echo '  <Image height="64" width="64" type="image/png">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_IMAGE) . '</Image>' . "\n";
+  }
+?>
   <InputEncoding>UTF-8</InputEncoding>
   <Url type="text/html" method="get" template="<?php echo tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, 'keywords={searchTerms}', 'NONSSL', false); ?>" />
 </OpenSearchDescription>
