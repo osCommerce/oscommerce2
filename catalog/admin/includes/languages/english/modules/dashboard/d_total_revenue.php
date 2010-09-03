272bb59211cfb563e5889bed2b68c0dbbdbf2f6f
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-09-03 15:32:50 +0200

    New Total Revenue Dashboard module to show a chart of total revenue from the last 30 days

---
+++ catalog/admin/includes/languages/english/modules/dashboard/d_total_revenue.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_ADMIN_DASHBOARD_TOTAL_REVENUE_TITLE', 'Total Revenue Chart (last 30 days)');
+define('MODULE_ADMIN_DASHBOARD_TOTAL_REVENUE_DESCRIPTION', 'Show the total revenue chart of the last 30 days');
+define('MODULE_ADMIN_DASHBOARD_TOTAL_REVENUE_CHART_LINK', 'Total Revenue');
+?>
