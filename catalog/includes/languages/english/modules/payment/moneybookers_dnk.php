62ce6445820e8ed1617d3599a759ee5e9c9c5506
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-09-09 09:59:10 +0200

    New Moneybookers Payment Modules

---
+++ catalog/includes/languages/english/modules/payment/moneybookers_dnk.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_MONEYBOOKERS_DNK_TEXT_TITLE', 'Moneybookers - Dankort (Denmark)');
+  define('MODULE_PAYMENT_MONEYBOOKERS_DNK_TEXT_PUBLIC_TITLE', 'Dankort');
+  define('MODULE_PAYMENT_MONEYBOOKERS_DNK_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="http://www.moneybookers.com/partners/oscommerce" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit Moneybookers Website</a>');
+?>
---
+++ catalog/includes/languages/english/modules/payment/moneybookers_psp.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_MONEYBOOKERS_PSP_TEXT_TITLE', 'Moneybookers - Postepay (Italy)');
+  define('MODULE_PAYMENT_MONEYBOOKERS_PSP_TEXT_PUBLIC_TITLE', 'Postepay');
+  define('MODULE_PAYMENT_MONEYBOOKERS_PSP_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="http://www.moneybookers.com/partners/oscommerce" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit Moneybookers Website</a>');
+?>
