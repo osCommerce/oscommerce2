e77a3d44b45ec94a856ac1918196f6117af68b3d
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-09-09 15:22:43 +0200

    New total customers chart for the admin dashboard

---
+++ catalog/admin/includes/languages/english/modules/dashboard/d_total_customers.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_ADMIN_DASHBOARD_TOTAL_CUSTOMERS_TITLE', 'Total Customers Chart (last 30 days)');
+define('MODULE_ADMIN_DASHBOARD_TOTAL_CUSTOMERS_DESCRIPTION', 'Show the total customers chart of the last 30 days');
+define('MODULE_ADMIN_DASHBOARD_TOTAL_CUSTOMERS_CHART_LINK', 'Total Customers');
+?>
