155ae24cd68d756039702255a53392bfe5042206
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-10-14 14:58:57 +0200

    Update configuration parameter names to avoid conflict with language definitions

--- catalog/opensearch.php
+++ catalog/opensearch.php
@@ -21,31 +21,31 @@
   echo '<?xml version="1.0"?>' . "\n";
 ?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-  <ShortName><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SHORT_NAME); ?></ShortName>
-  <Description><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_DESCRIPTION); ?></Description>
+  <ShortName><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_SHORT_NAME); ?></ShortName>
+  <Description><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_DESCRIPTION); ?></Description>
 <?php
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_CONTACT)) {
-    echo '  <Contact>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_CONTACT) . '</Contact>' . "\n";
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT)) {
+    echo '  <Contact>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT) . '</Contact>' . "\n";
   }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_TAGS)) {
-    echo '  <Tags>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_TAGS) . '</Tags>' . "\n";
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS)) {
+    echo '  <Tags>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS) . '</Tags>' . "\n";
   }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_ATTRIBUTION)) {
-    echo '  <Attribution>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_ATTRIBUTION) . '</Attribution>' . "\n";
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION)) {
+    echo '  <Attribution>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION) . '</Attribution>' . "\n";
   }
 
-  if (MODULE_HEADER_TAGS_OPENSEARCH_ADULT_CONTENT == 'True') {
+  if (MODULE_HEADER_TAGS_OPENSEARCH_SITE_ADULT_CONTENT == 'True') {
     echo '  <AdultContent>True</AdultContent>' . "\n";
   }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_ICON)) {
-    echo '  <Image height="16" width="16" type="image/x-icon">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_ICON) . '</Image>' . "\n";
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON)) {
+    echo '  <Image height="16" width="16" type="image/x-icon">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON) . '</Image>' . "\n";
   }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_IMAGE)) {
-    echo '  <Image height="64" width="64" type="image/png">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_IMAGE) . '</Image>' . "\n";
+  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE)) {
+    echo '  <Image height="64" width="64" type="image/png">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE) . '</Image>' . "\n";
   }
 ?>
   <InputEncoding>UTF-8</InputEncoding>
