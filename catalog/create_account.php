c6d7714dea6d85023b6a7358f757a3ef96701423
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-10-18 21:44:27 +0800

    Add missing </span> tag (fix bug #51)

--- catalog/create_account.php
+++ catalog/create_account.php
@@ -392,7 +392,7 @@
       echo tep_draw_input_field('state');
     }
 
-    if (tep_not_null(ENTRY_STATE_TEXT)) echo '&nbsp;<span class="inputRequirement">' . ENTRY_STATE_TEXT;
+    if (tep_not_null(ENTRY_STATE_TEXT)) echo '&nbsp;<span class="inputRequirement">' . ENTRY_STATE_TEXT . '</span>';
 ?>
         </td>
       </tr>
