6f811e63018f720e34eec45c573361e7e4c0981e
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-10-22 11:57:11 +0200

    Add template_top.php/template_bottom.php to the Administration Tool layout Update column_left boxes (boxes now defined in an array)

--- catalog/admin/includes/column_left.php
+++ catalog/admin/includes/column_left.php
@@ -5,17 +5,66 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce
+  Copyright (c) 2010 osCommerce
 
   Released under the GNU General Public License
 */
 
-  require(DIR_WS_BOXES . 'configuration.php');
-  require(DIR_WS_BOXES . 'catalog.php');
-  require(DIR_WS_BOXES . 'modules.php');
-  require(DIR_WS_BOXES . 'customers.php');
-  require(DIR_WS_BOXES . 'taxes.php');
-  require(DIR_WS_BOXES . 'localization.php');
-  require(DIR_WS_BOXES . 'reports.php');
-  require(DIR_WS_BOXES . 'tools.php');
+  if (tep_session_is_registered('admin')) {
+    $cl_box_groups = array();
+
+    include(DIR_WS_BOXES . 'configuration.php');
+    include(DIR_WS_BOXES . 'catalog.php');
+    include(DIR_WS_BOXES . 'modules.php');
+    include(DIR_WS_BOXES . 'customers.php');
+    include(DIR_WS_BOXES . 'taxes.php');
+    include(DIR_WS_BOXES . 'localization.php');
+    include(DIR_WS_BOXES . 'reports.php');
+    include(DIR_WS_BOXES . 'tools.php');
+?>
+
+<div id="adminAppMenu">
+
+<?php
+    foreach ($cl_box_groups as $groups) {
+      echo '<h3><a href="#">' . $groups['heading'] . '</a></h3>' .
+           '<div><ul>';
+
+      foreach ($groups['apps'] as $app) {
+        echo '<li><a href="' . $app['link'] . '">' . $app['title'] . '</a></li>';
+      }
+
+      echo '</ul></div>';
+    }
+?>
+
+</div>
+
+<script type="text/javascript">
+$('#adminAppMenu').accordion({
+  autoHeight: false,
+  icons: {
+    'header': 'ui-icon-plus',
+    'headerSelected': 'ui-icon-minus'
+  }
+
+<?php
+    $counter = 0;
+    foreach ($cl_box_groups as $groups) {
+      foreach ($groups['apps'] as $app) {
+        if ($app['code'] == $PHP_SELF) {
+          echo ',active: ' . $counter;
+          break;
+        }
+      }
+
+      $counter++;
+    }
+?>
+
+});
+</script>
+
+<?php
+  }
 ?>
--- catalog/admin/store_logo.php
+++ catalog/admin/store_logo.php
@@ -45,30 +45,11 @@
   if (!tep_is_writable(DIR_FS_CATALOG_IMAGES)) {
     $messageStack->add(sprintf(ERROR_IMAGES_DIRECTORY_NOT_WRITEABLE, tep_href_link(FILENAME_SEC_DIR_PERMISSIONS, 'selected_box=tools')), 'error');
   }
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html <?php echo HTML_PARAMS; ?>>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
-<title><?php echo TITLE; ?></title>
-<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-<script language="javascript" src="includes/general.js"></script>
-</head>
-<body onload="SetFocus();">
-<!-- header //-->
-<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
-<!-- header_eof //-->
 
-<!-- body //-->
-<table border="0" width="100%" cellspacing="2" cellpadding="2">
-  <tr>
-    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
-<!-- left_navigation //-->
-<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-<!-- left_navigation_eof //-->
-    </table></td>
-<!-- body_text //-->
-    <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+  require(DIR_WS_INCLUDES . 'template_top.php');
+?>
+
+    <table border="0" width="100%" cellspacing="0" cellpadding="2">
       <tr>
         <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
@@ -106,16 +87,9 @@
       <tr>
         <td class="main"><?php echo DIR_FS_CATALOG_IMAGES . 'store_logo.png'; ?></td>
       </tr>
-    </table></td>
-<!-- body_text_eof //-->
-  </tr>
-</table>
-<!-- body_eof //-->
+    </table>
 
-<!-- footer //-->
-<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
-<!-- footer_eof //-->
-<br />
-</body>
-</html>
-<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
+<?php
+  require(DIR_WS_INCLUDES . 'template_bottom.php');
+  require(DIR_WS_INCLUDES . 'application_bottom.php');
+?>
