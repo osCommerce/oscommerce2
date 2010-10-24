1d71a1d9b21273fb55eac86115a714d7b8285fa7
Mark Evans <markkevans@gmail.com>
2010-10-24 21:43:27 +0100

    Use tep_get_ip_address() rather accessing the variable directly so it works via proxies as well. Fixes issue #75

--- catalog/includes/languages/english/password_forgotten.php
+++ catalog/includes/languages/english/password_forgotten.php
@@ -20,7 +20,7 @@ define('TEXT_MAIN', 'If you\'ve forgotten your password, enter your e-mail addre
 define('TEXT_NO_EMAIL_ADDRESS_FOUND', 'Error: The E-Mail Address was not found in our records, please try again.');
 
 define('EMAIL_PASSWORD_REMINDER_SUBJECT', STORE_NAME . ' - New Password');
-define('EMAIL_PASSWORD_REMINDER_BODY', 'A new password was requested from ' . $REMOTE_ADDR . '.' . "\n\n" . 'Your new password to \'' . STORE_NAME . '\' is:' . "\n\n" . '   %s' . "\n\n");
+define('EMAIL_PASSWORD_REMINDER_BODY', 'A new password was requested from ' . tep_get_ip_address() . '.' . "\n\n" . 'Your new password to \'' . STORE_NAME . '\' is:' . "\n\n" . '   %s' . "\n\n");
 
 define('SUCCESS_PASSWORD_SENT', 'Success: A new password has been sent to your e-mail address.');
 ?>
