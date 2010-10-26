33f5c207fb71d4c8370e5b24e24ce71f5c2077b3
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-10-26 13:22:29 +0200

    Update product listing header and data css definitions

--- catalog/includes/modules/product_listing.php
+++ catalog/includes/modules/product_listing.php
@@ -32,7 +32,7 @@
 
   $prod_list_contents = '<div class="ui-widget infoBoxContainer">' .
                         '  <div class="ui-widget-header ui-corner-top infoBoxHeading">' .
-                        '    <table border="0" width="100%" cellspacing="0" cellpadding="2" style="table-layout: fixed; padding: 0;">' .
+                        '    <table border="0" width="100%" cellspacing="0" cellpadding="2" class="productListingHeader">' .
                         '      <tr>';
 
   for ($col=0, $n=sizeof($column_list); $col<$n; $col++) {
@@ -89,7 +89,7 @@
     $listing_query = tep_db_query($listing_split->sql_query);
 
     $prod_list_contents .= '  <div class="ui-widget-content ui-corner-bottom productListTable">' .
-                           '    <table border="0" width="100%" cellspacing="0" cellpadding="2" style="table-layout: fixed;">';
+                           '    <table border="0" width="100%" cellspacing="0" cellpadding="2" class="productListingData">';
 
     while ($listing = tep_db_fetch_array($listing_query)) {
       $rows++;
