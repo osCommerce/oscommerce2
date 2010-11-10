32315c2b91bef5ab5423834941461ae3132cf537
foxp2 <foxp2projects@googlemail.com>
2010-11-11 07:42:04 +0800

    update jquery

--- catalog/includes/template_top.php
+++ catalog/includes/template_top.php
@@ -18,9 +18,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
 <title><?php echo tep_output_string_protected($oscTemplate->getTitle()); ?></title>
 <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>" />
-<link rel="stylesheet" type="text/css" href="ext/jquery/ui/redmond/jquery-ui-1.8.5.css" />
+<link rel="stylesheet" type="text/css" href="ext/jquery/ui/redmond/jquery-ui-1.8.6.css" />
 <script type="text/javascript" src="ext/jquery/jquery-1.4.2.min.js"></script>
-<script type="text/javascript" src="ext/jquery/ui/jquery-ui-1.8.5.min.js"></script>
+<script type="text/javascript" src="ext/jquery/ui/jquery-ui-1.8.6.min.js"></script>
 <script type="text/javascript" src="ext/jquery/jquery.bxGallery.1.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="ext/jquery/fancybox/jquery.fancybox-1.3.1.css" />
 <script type="text/javascript" src="ext/jquery/fancybox/jquery.fancybox-1.3.1.pack.js"></script>
