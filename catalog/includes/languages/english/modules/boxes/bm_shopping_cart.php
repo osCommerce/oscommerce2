f365833f190a3c2deec347c35cd5104f1049279b
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-11-12 17:00:00 +0100

    fix definition keys

--- catalog/includes/languages/english/modules/boxes/bm_shopping_cart.php
+++ catalog/includes/languages/english/modules/boxes/bm_shopping_cart.php
@@ -13,5 +13,5 @@
   define('MODULE_BOXES_SHOPPING_CART_TITLE', 'Shopping Cart');
   define('MODULE_BOXES_SHOPPING_CART_DESCRIPTION', 'Show shopping cart contents');
   define('MODULE_BOXES_SHOPPING_CART_BOX_TITLE', 'Shopping Cart');
-  define('MODULE_BOXES_SHOPPING_CART_CART_EMPTY', '0 items');
+  define('MODULE_BOXES_SHOPPING_CART_BOX_CART_EMPTY', '0 items');
 ?>
