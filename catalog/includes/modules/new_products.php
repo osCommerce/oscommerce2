85f556a7d66b0dd94fb58fac3a986b8e5a65ccdb
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-11-12 19:30:08 +0800

    Fix missing </tr> (report #107) Only show new products if products exist

--- catalog/includes/modules/new_products.php
+++ catalog/includes/modules/new_products.php
@@ -16,26 +16,32 @@
     $new_products_query = tep_db_query("select distinct p.products_id, p.products_image, p.products_tax_class_id, pd.products_name, if(s.status, s.specials_new_products_price, p.products_price) as products_price from " . TABLE_PRODUCTS . " p left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c, " . TABLE_CATEGORIES . " c where p.products_id = p2c.products_id and p2c.categories_id = c.categories_id and c.parent_id = '" . (int)$new_products_category_id . "' and p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' order by p.products_date_added desc limit " . MAX_DISPLAY_NEW_PRODUCTS);
   }
 
-  $col = 0;
+  $num_new_products = tep_db_num_rows($new_products_query);
 
-  $new_prods_content = '<table border="0" width="100%" cellspacing="0" cellpadding="2">';
-  while ($new_products = tep_db_fetch_array($new_products_query)) {
-    if ($col === 0) {
-      $new_prods_content .= '<tr>';
+  if ($new_products_query > 0) {
+    $counter = 0;
+    $col = 0;
+
+    $new_prods_content = '<table border="0" width="100%" cellspacing="0" cellpadding="2">';
+    while ($new_products = tep_db_fetch_array($new_products_query)) {
+      $counter++;
+
+      if ($col === 0) {
+        $new_prods_content .= '<tr>';
+      }
+
+      $new_prods_content .= '<td width="33%" align="center" valign="top"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $new_products['products_image'], $new_products['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br /><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . $new_products['products_name'] . '</a><br />' . $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id'])) . '</td>';
+
+      $col ++;
+
+      if (($col > 2) || ($counter == $num_new_products)) {
+        $new_prods_content .= '</tr>';
+
+        $col = 0;
+      }
     }
 
-    $new_prods_content .= '<td width="33%" align="center" valign="top"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $new_products['products_image'], $new_products['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br /><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . $new_products['products_name'] . '</a><br />' . $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id'])) . '</td>';
-
-    $col ++;
-
-    if ($col > 2) {
-      $new_prods_content .= '</tr>';
-
-      $col = 0;
-    }
-  }
-
-  $new_prods_content .= '</table>';
+    $new_prods_content .= '</table>';
 ?>
 
   <h2><?php echo sprintf(TABLE_HEADING_NEW_PRODUCTS, strftime('%B')); ?></h2>
@@ -43,3 +49,7 @@
   <div class="contentText">
     <?php echo $new_prods_content; ?>
   </div>
+
+<?php
+  }
+?>
