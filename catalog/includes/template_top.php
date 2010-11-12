0a4e9f0bc7dac98b7cc425904d7f00ab15b97c60
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-11-12 14:33:32 +0100

    move template specific logic out of the template class to template_top.php

--- catalog/includes/template_top.php
+++ catalog/includes/template_top.php
@@ -11,6 +11,14 @@
 */
 
   $oscTemplate->buildBlocks();
+
+  if (!$oscTemplate->hasBlocks('boxes_column_left')) {
+    $oscTemplate->setGridContentWidth($oscTemplate->getGridContentWidth() + $oscTemplate->getGridColumnWidth());
+  }
+
+  if (!$oscTemplate->hasBlocks('boxes_column_right')) {
+    $oscTemplate->setGridContentWidth($oscTemplate->getGridContentWidth() + $oscTemplate->getGridColumnWidth());
+  }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html <?php echo HTML_PARAMS; ?>>
