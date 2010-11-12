d2fda328bbd8d39d5c0cf4bc9f5de7da7627fec4
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-11-12 22:04:45 +0800

    Move xmlns out of the language definition to the template file

--- catalog/includes/template_top.php
+++ catalog/includes/template_top.php
@@ -21,7 +21,7 @@
   }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html <?php echo HTML_PARAMS; ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
 <title><?php echo tep_output_string_protected($oscTemplate->getTitle()); ?></title>
