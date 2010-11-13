14c6cbbf27a4be72d4efc3b2c368876087ef0fe4
oscsparky <markkevans@gmail.com>
2010-11-14 06:39:55 +0800

    Add a message after a review is posted to inform user moderation is needed. Fixes issue #123

--- catalog/includes/languages/english/product_reviews_write.php
+++ catalog/includes/languages/english/product_reviews_write.php
@@ -20,5 +20,7 @@ define('TEXT_NO_HTML', '<small><font color="#ff0000"><strong>NOTE:</strong></fon
 define('TEXT_BAD', '<small><font color="#ff0000"><strong>BAD</strong></font></small>');
 define('TEXT_GOOD', '<small><font color="#ff0000"><strong>GOOD</strong></font></small>');
 
+define('TEXT_REVIEW_RECEIVED', 'Thank you for your review. It has been submitted to the store owner for approval and should appear shortly.');
+
 define('TEXT_CLICK_TO_ENLARGE', 'Click to enlarge');
 ?>
