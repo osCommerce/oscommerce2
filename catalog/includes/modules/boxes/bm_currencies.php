d64571edd8c844de214c9957d6df111266893d59
unknown <Acidvertigo@.(none)>
2011-08-14 22:04:05 +0200

    error typo

--- catalog/includes/modules/boxes/bm_currencies.php
+++ catalog/includes/modules/boxes/bm_currencies.php
@@ -31,19 +31,17 @@
     }
 
     function execute() {
-      global $PHP_SELF, $currencies, $HTTP_GET_VARS, $request_type, $currency, $oscTemplate;
+      global $currencies, $request_type, $currency, $oscTemplate;
 
-      if (substr(basename($PHP_SELF), 0, 8) != 'checkout') {
+      if (substr(basename($_SERVER['PHP_SELF']), 0, 8) != 'checkout') {
         if (isset($currencies) && is_object($currencies) && (count($currencies->currencies) > 1)) {
-          reset($currencies->currencies);
           $currencies_array = array();
-          while (list($key, $value) = each($currencies->currencies)) {
+		  foreach($currencies->currencies as $key => $value) {
             $currencies_array[] = array('id' => $key, 'text' => $value['title']);
           }
 
           $hidden_get_variables = '';
-          reset($HTTP_GET_VARS);
-          while (list($key, $value) = each($HTTP_GET_VARS)) {
+		  foreach($_GET as $key => $value) {
             if ( is_string($value) && ($key != 'currency') && ($key != tep_session_name()) && ($key != 'x') && ($key != 'y') ) {
               $hidden_get_variables .= tep_draw_hidden_field($key, $value);
             }
@@ -52,7 +50,7 @@
           $data = '<div class="ui-widget infoBoxContainer">' .
                   '  <div class="ui-widget-header infoBoxHeading">' . MODULE_BOXES_CURRENCIES_BOX_TITLE . '</div>' .
                   '  <div class="ui-widget-content infoBoxContents">' . 
-                  '    ' . tep_draw_form('currencies', tep_href_link(basename($PHP_SELF), '', $request_type, false), 'get') .
+                  '    ' . tep_draw_form('currencies', tep_href_link(basename($_SERVER['PHP_SELF']), '', $request_type, false), 'get') .
                   '    ' . tep_draw_pull_down_menu('currency', $currencies_array, $currency, 'onchange="this.form.submit();" style="width: 100%"') . $hidden_get_variables . tep_hide_session_id() . '</form>' .
                   '  </div>' .
                   '</div>';
