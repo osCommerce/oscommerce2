ba4fba38b7726b3636a3dcc8d86730bb7dca07ba
unknown <Acidvertigo@.(none)>
2011-08-14 22:12:55 +0200

    Replaced deprecated $HTTP_XXX_VARS with superglobal ones

--- catalog/includes/modules/boxes/bm_manufacturer_info.php
+++ catalog/includes/modules/boxes/bm_manufacturer_info.php
@@ -31,10 +31,10 @@
     }
 
     function execute() {
-      global $HTTP_GET_VARS, $languages_id, $oscTemplate;
+      global $languages_id, $oscTemplate;
 
-      if (isset($HTTP_GET_VARS['products_id'])) {
-        $manufacturer_query = tep_db_query("select m.manufacturers_id, m.manufacturers_name, m.manufacturers_image, mi.manufacturers_url from " . TABLE_MANUFACTURERS . " m left join " . TABLE_MANUFACTURERS_INFO . " mi on (m.manufacturers_id = mi.manufacturers_id and mi.languages_id = '" . (int)$languages_id . "'), " . TABLE_PRODUCTS . " p  where p.products_id = '" . (int)$HTTP_GET_VARS['products_id'] . "' and p.manufacturers_id = m.manufacturers_id");
+      if (isset($_GET['products_id'])) {
+        $manufacturer_query = tep_db_query("select m.manufacturers_id, m.manufacturers_name, m.manufacturers_image, mi.manufacturers_url from " . TABLE_MANUFACTURERS . " m left join " . TABLE_MANUFACTURERS_INFO . " mi on (m.manufacturers_id = mi.manufacturers_id and mi.languages_id = '" . (int)$languages_id . "'), " . TABLE_PRODUCTS . " p  where p.products_id = '" . (int)$_GET['products_id'] . "' and p.manufacturers_id = m.manufacturers_id");
         if (tep_db_num_rows($manufacturer_query)) {
           $manufacturer = tep_db_fetch_array($manufacturer_query);
 
