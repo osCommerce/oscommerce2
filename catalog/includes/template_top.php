41c8329ff341834654d65ccd0d653f0b79c5d2c0
Luca <ldanzelmo@gmail.com>
2011-08-22 09:48:44 +0200

    Change DOCTYPE to HTML 5

--- catalog/includes/template_top.php
+++ catalog/includes/template_top.php
@@ -20,32 +20,32 @@
     $oscTemplate->setGridContentWidth($oscTemplate->getGridContentWidth() + $oscTemplate->getGridColumnWidth());
   }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
+<meta charset=<?php echo CHARSET; ?>" />
 <title><?php echo tep_output_string_protected($oscTemplate->getTitle()); ?></title>
 <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>" />
-<link rel="stylesheet" type="text/css" href="ext/jquery/ui/redmond/jquery-ui-1.8.15.css" />
-<script type="text/javascript" src="ext/jquery/jquery-1.6.2.min.js"></script>
+<link rel="stylesheet" href="ext/jquery/ui/redmond/jquery-ui-1.8.15.css" />
+<scriptsrc="ext/jquery/jquery-1.6.2.min.js"></script>
 <script type="text/javascript" src="ext/jquery/ui/jquery-ui-1.8.15.min.js"></script>
 
 <?php
   if (tep_not_null(JQUERY_DATEPICKER_I18N_CODE)) {
 ?>
-<script type="text/javascript" src="ext/jquery/ui/i18n/jquery.ui.datepicker-<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>.js"></script>
-<script type="text/javascript">
+<script src="ext/jquery/ui/i18n/jquery.ui.datepicker-<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>.js"></script>
+<script>
 $.datepicker.setDefaults($.datepicker.regional['<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>']);
 </script>
 <?php
   }
 ?>
 
-<script type="text/javascript" src="ext/jquery/bxGallery/jquery.bxGallery.1.1.min.js"></script>
-<link rel="stylesheet" type="text/css" href="ext/jquery/fancybox/jquery.fancybox-1.3.4.css" />
-<script type="text/javascript" src="ext/jquery/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<link rel="stylesheet" type="text/css" href="ext/960gs/min/<?php echo ((stripos(HTML_PARAMS, 'dir="rtl"') !== false) ? 'rtl_' : ''); ?>960_24_col.css" />
-<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+<script src="ext/jquery/bxGallery/jquery.bxGallery.1.1.min.js"></script>
+<link rel="stylesheet" href="ext/jquery/fancybox/jquery.fancybox-1.3.4.css" />
+<script src="ext/jquery/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<link rel="stylesheet" href="ext/960gs/min/<?php echo ((stripos(HTML_PARAMS, 'dir="rtl"') !== false) ? 'rtl_' : ''); ?>960_24_col.css" />
+<link rel="stylesheet" href="stylesheet.css" />
 <?php echo $oscTemplate->getBlocks('header_tags'); ?>
 </head>
 <body>
