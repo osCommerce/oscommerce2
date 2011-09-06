24c25983892041bb51193d593c37826fb6f0cd9d
Luca <ldanzelmo@gmail.com>
2011-09-06 11:40:13 +0200

    Remove compatibility from catalog side

--- catalog/includes/application_top.php
+++ catalog/includes/application_top.php
@@ -37,9 +37,6 @@
 // define the project version --- obsolete, now retrieved with tep_get_version()
   define('PROJECT_VERSION', 'osCommerce Online Merchant v2.3');
 
-// some code to solve compatibility issues
-  require(DIR_WS_FUNCTIONS . 'compatibility.php');
-
 // set the type of request (secure or not)
   $request_type = ($_ENV['HTTPS'] == 'on') ? 'SSL' : 'NONSSL';
 
