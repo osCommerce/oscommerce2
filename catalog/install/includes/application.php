d73376b3e1d694edf69b86b29a2c4f71bf8e9860
Luca <ldanzelmo@gmail.com>
2012-01-11 10:24:38 +0100

    Remove compatibility.php from project

--- catalog/install/includes/application.php
+++ catalog/install/includes/application.php
@@ -13,7 +13,6 @@
 // Set the level of error reporting
   error_reporting(E_ALL & ~E_NOTICE);
 
-  require('includes/functions/compatibility.php');
   require('includes/functions/general.php');
   require('includes/functions/database.php');
   require('includes/functions/html_output.php');
