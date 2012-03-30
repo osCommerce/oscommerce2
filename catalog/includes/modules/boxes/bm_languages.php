7a7f7cb31450c2d3d8478cf4a514ea5bc5d64924
acidvertigo <ldanzelmo@gmail.com>
2012-03-30 20:09:52 +0300

    Remove deprecated PHP_SELF

--- catalog/includes/modules/boxes/bm_languages.php
+++ catalog/includes/modules/boxes/bm_languages.php
@@ -31,9 +31,9 @@
     }
 
     function execute() {
-      global $PHP_SELF, $lng, $request_type, $oscTemplate;
+      global $lng, $request_type, $oscTemplate;
 
-      if (substr(basename($PHP_SELF), 0, 8) != 'checkout') {
+      if (substr(basename($_SERVER['PHP_SELF']), 0, 8) != 'checkout') {
         if (!isset($lng) || (isset($lng) && !is_object($lng))) {
           include(DIR_WS_CLASSES . 'language.php');
           $lng = new language;
@@ -43,7 +43,7 @@
           $languages_string = '';
           reset($lng->catalog_languages);
           while (list($key, $value) = each($lng->catalog_languages)) {
-            $languages_string .= ' <a href="' . tep_href_link(basename($PHP_SELF), tep_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . tep_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a> ';
+            $languages_string .= ' <a href="' . tep_href_link(basename($_SERVER['PHP_SELF']), tep_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . tep_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a> ';
           }
 
           $data = '<div class="ui-widget infoBoxContainer">' .
