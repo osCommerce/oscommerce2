028512329de985a91885bb6e96944ff31983d0bd
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-07-11 00:19:30 +0200

    Change the password forgotten routine to send a personal link to the customer instead of a new password. Here they can enter their own new password or continue with their old password if they remembered it. This prevents abuse of resetting account passwords of known email addresses.

---
+++ catalog/includes/languages/english/password_reset.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2012 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Login');
+define('NAVBAR_TITLE_2', 'Password Reset');
+
+define('HEADING_TITLE', 'Password Reset');
+
+define('TEXT_MAIN', 'Please enter a new password for your account.');
+
+define('TEXT_NO_RESET_LINK_FOUND', 'Error: The password reset link was not found in our records, please try again by generating a new link.');
+define('TEXT_NO_EMAIL_ADDRESS_FOUND', 'Error: The E-Mail Address was not found in our records, please try again.');
+
+define('SUCCESS_PASSWORD_RESET', 'Your password has been successfully updated. Please login with your new password.');
+?>
