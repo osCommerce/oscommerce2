ab26d82f98422e9a24169403af7f7310df387f5e
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-07-25 16:08:12 +0200

    Work around PHP bug 55544 regarding conflict with ob_gzhandler and zlib output compression (thanks to Robert Fisher for pointing this out)

--- catalog/includes/application_top.php
+++ catalog/includes/application_top.php
@@ -71,14 +71,16 @@
   }
 
 // if gzip_compression is enabled, start to buffer the output
-  if ( (GZIP_COMPRESSION == 'true') && ($ext_zlib_loaded = extension_loaded('zlib')) && (PHP_VERSION >= '4') ) {
+  if ( (GZIP_COMPRESSION == 'true') && ($ext_zlib_loaded = extension_loaded('zlib')) && !headers_sent() ) {
     if (($ini_zlib_output_compression = (int)ini_get('zlib.output_compression')) < 1) {
-      if (PHP_VERSION >= '4.0.4') {
-        ob_start('ob_gzhandler');
-      } else {
-        include(DIR_WS_FUNCTIONS . 'gzip_compression.php');
-        ob_start();
-        ob_implicit_flush();
+      if (PHP_VERSION < '5.4' || PHP_VERSION > '5.4.5') { // see PHP bug 55544
+        if (PHP_VERSION >= '4.0.4') {
+          ob_start('ob_gzhandler');
+        } else {
+          include(DIR_WS_FUNCTIONS . 'gzip_compression.php');
+          ob_start();
+          ob_implicit_flush();
+        }
       }
     } else {
       ini_set('zlib.output_compression_level', GZIP_LEVEL);
