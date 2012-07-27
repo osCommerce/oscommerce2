28cfbda451752cfbe713185d5782783294c4e16b
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-07-27 22:18:05 +0200

    Propose Robot NoIndex Header Tag Module

---
+++ catalog/includes/languages/english/modules/header_tags/ht_robot_noindex.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2012 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_ROBOT_NOINDEX_TITLE', 'Robot NoIndex');
+  define('MODULE_HEADER_TAGS_ROBOT_NOINDEX_DESCRIPTION', 'Add robot noindex tags to specified pages');
+?>
