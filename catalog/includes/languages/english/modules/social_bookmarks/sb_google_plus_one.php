301082713b4b16252c012d23e76049e446663c36
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-08-03 20:12:07 +0200

    Introduce Google+ +1 and Google+ Share Social Bookmark modules

---
+++ catalog/includes/languages/english/modules/social_bookmarks/sb_google_plus_one.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2012 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_SOCIAL_BOOKMARKS_GOOGLE_PLUS_ONE_TITLE', 'Google+ +1 Button');
+  define('MODULE_SOCIAL_BOOKMARKS_GOOGLE_PLUS_ONE_DESCRIPTION', 'Recommend products through Google+ +1 Button.');
+  define('MODULE_SOCIAL_BOOKMARKS_GOOGLE_PLUS_ONE_PUBLIC_TITLE', 'Recommend on Google+');
+?>
