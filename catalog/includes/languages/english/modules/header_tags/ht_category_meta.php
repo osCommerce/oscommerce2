c279f815fc7ef3005299832a1c4efd3144052605
Gary Burton <oscshops@gmail.com>
2012-08-07 11:07:52 +0100

    Add language files for Header Tags SEO

---
+++ catalog/includes/languages/english/modules/header_tags/ht_category_meta.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_CATEGORY_META_TITLE', 'Category Meta');
+  define('MODULE_HEADER_TAGS_CATEGORY_META_DESCRIPTION', 'Add the META elements defined when setting up the category, to the header of the catgegory page');
+?>
---
+++ catalog/includes/languages/english/modules/header_tags/ht_manufacturer_meta.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_MANUFACTURERS_META_TITLE', 'Manufacturer Meta');
+  define('MODULE_HEADER_TAGS_MANUFACTURERS_META_DESCRIPTION', 'Add the META elements defined when setting up the manufacturer, to the header of the manufacturer page');
+?>
