f4bafd9a17978546ce076f8108cb722a6bd8e14a
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-08-12 14:00:50 +0200

    Improve Shopping Cart ID Detection (bug #473)
    * With a specific checkout routine it is possible for the shipping selection page to not detect a change in shopping cart contents and can present a list of outdated shipping options to the customer. Thanks to Wojtek Ruszczewski (wrwrwr) for bringing this to our attention.
    
    * Also reset the shopping cart ID in the shopping cart class update_quantity() method (this method is used by Add-Ons).

--- catalog/includes/classes/shopping_cart.php
+++ catalog/includes/classes/shopping_cart.php
@@ -178,6 +178,9 @@
             if (tep_session_is_registered('customer_id')) tep_db_query("update " . TABLE_CUSTOMERS_BASKET_ATTRIBUTES . " set products_options_value_id = '" . (int)$value . "' where customers_id = '" . (int)$customer_id . "' and products_id = '" . tep_db_input($products_id_string) . "' and products_options_id = '" . (int)$option . "'");
           }
         }
+
+// assign a temporary unique ID to the order contents to prevent hack attempts during the checkout procedure
+        $this->cartID = $this->generate_cart_id();
       }
     }
 
