cfa53fae1a757b9e14d3e79c0c68a2469d97a29c
Gary Burton <oscshops@gmail.com>
2012-08-14 08:45:39 +0100

    Introduce Canonical Header Tag Module
    Also fixed manufacturers_id omission.

---
+++ catalog/includes/languages/english/modules/header_tags/ht_canonical.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2012 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_CANONICAL_TITLE', 'Canonical Header Links');
+  define('MODULE_HEADER_TAGS_CANONICAL_DESCRIPTION', 'Add header canonical links to category and product pages');
+?>
