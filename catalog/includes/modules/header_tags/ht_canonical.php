7a0597d22d1a8726298cabb5a7fdd7de112a0fcc
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-08-14 10:29:35 +0200

    Add canonical link to manufacturers listing (bug #481)

--- catalog/includes/modules/header_tags/ht_canonical.php
+++ catalog/includes/modules/header_tags/ht_canonical.php
@@ -33,8 +33,12 @@
 
       if (basename($PHP_SELF) == FILENAME_PRODUCT_INFO) {
         $oscTemplate->addBlock('<link rel="canonical" href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $HTTP_GET_VARS['products_id'], 'NONSSL', false) . '" />' . "\n", $this->group);
-      } elseif ((basename($PHP_SELF) == FILENAME_DEFAULT) && isset($cPath) && tep_not_null($cPath)) {
-        $oscTemplate->addBlock('<link rel="canonical" href="' . tep_href_link(FILENAME_DEFAULT, 'cPath=' . $cPath, 'NONSSL', false) . '" />' . "\n", $this->group);
+      } elseif (basename($PHP_SELF) == FILENAME_DEFAULT) {
+        if (isset($cPath) && tep_not_null($cPath)) {
+          $oscTemplate->addBlock('<link rel="canonical" href="' . tep_href_link(FILENAME_DEFAULT, 'cPath=' . $cPath, 'NONSSL', false) . '" />' . "\n", $this->group);
+        } elseif (isset($HTTP_GET_VARS['manufacturers_id']) && tep_not_null($HTTP_GET_VARS['manufacturers_id'])) {
+          $oscTemplate->addBlock('<link rel="canonical" href="' . tep_href_link(FILENAME_DEFAULT, 'manufacturers_id=' . $HTTP_GET_VARS['manufacturers_id'], 'NONSSL', false) . '" />' . "\n", $this->group);
+        }
       }
     }
 
