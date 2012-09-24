435955aa1268d2dbce51dc41fb9baf14b793eb07
Gary Burton <oscshops@gmail.com>
2012-09-24 13:20:16 +0100

    Add text showing how users can enable javascript.

--- catalog/includes/languages/english/modules/header_tags/ht_noscript.php
+++ catalog/includes/languages/english/modules/header_tags/ht_noscript.php
@@ -12,5 +12,5 @@
 
   define('MODULE_HEADER_TAGS_NOSCRIPT_TITLE', 'No Script Message');
   define('MODULE_HEADER_TAGS_NOSCRIPT_DESCRIPTION', 'Shows a message to the user if they are browsing with javascript off.');
-  define('MODULE_HEADER_TAGS_NOSCRIPT_TEXT', '<p><strong>JavaScript seems to be disabled in your browser.</strong></p><p>You must have JavaScript enabled in your browser to utilize the functionality of this website.</p>');
+  define('MODULE_HEADER_TAGS_NOSCRIPT_TEXT', '<p><strong>JavaScript seems to be disabled in your browser.</strong></p><p>You must have JavaScript enabled in your browser to utilize the functionality of this website. <a href="http://www.enable-javascript.com/" target="_blank" rel="nofollow">Click here for instructions on enabling javascript in your browser</a>.');
 ?>
