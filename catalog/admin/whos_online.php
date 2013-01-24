b83153190e5360641197f58f3f02f7878871073c
Gergely Tóth <tgely@gumisarok.hu>
2013-01-24 18:15:11 +0100

    issue #567 - non defined fix in who is online
    Signed-off-by: Gergely Tóth <tgely@gumisarok.hu>

--- catalog/admin/whos_online.php
+++ catalog/admin/whos_online.php
@@ -53,7 +53,7 @@
       $info = $whos_online['session_id'];
     }
 
-    if ($whos_online['session_id'] == $info) {
+    if (isset($info) && $whos_online['session_id'] == $info) {
       echo '              <tr id="defaultSelected" class="dataTableRowSelected" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)">' . "\n";
     } else {
       echo '              <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . tep_href_link(FILENAME_WHOS_ONLINE, tep_get_all_get_params(array('info', 'action')) . 'info=' . $whos_online['session_id'], 'NONSSL') . '\'">' . "\n";
