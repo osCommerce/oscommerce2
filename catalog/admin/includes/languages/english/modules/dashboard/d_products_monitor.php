bbe1d282247b666d2930a298282287ea91769a13
Gergely Tóth <tgely@gumisarok.hu>
2013-01-26 23:10:15 +0100

    Dashboard products monitor
    Signed-off-by: Gergely Tóth <tgely@gumisarok.hu>

---
+++ catalog/admin/includes/languages/english/modules/dashboard/d_products_monitor.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_TITLE', 'Products Monitor');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_DESCRIPTION', 'Display Products Errors in Monitor System');
+
+define('MODULE_ADMIN_DASHBOARD_MODEL_INFO_PRODUCTS_NAME', 'Inconsistent Products');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_PRODUCTS_PRODUCTS_LAST_MODIFIED', 'Last Modified');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_PRODUCTS_ERRORS', 'Estimated Errors');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_NO_MODEL', 'no model');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_NO_PICTURE', 'no picture');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_NO_TAX', 'no tax class');
+define('MODULE_ADMIN_DASHBOARD_PRODUCTS_MONITOR_NO_PRICE', 'zero price');
+
+define('MODULE_ADMIN_DASHBOARD_PERFECT_PRODUCTS', 'Good work! No critical products.');
+?>
