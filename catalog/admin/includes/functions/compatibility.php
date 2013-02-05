0cc35b385ea6dca89e0cfeeec05db86816c5417e
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-05 23:59:48 +0100

    Remove stripos compatibility layer

--- catalog/admin/includes/functions/compatibility.php
+++ catalog/admin/includes/functions/compatibility.php
@@ -14,33 +14,4 @@
   if (PHP_VERSION >= '5.2') {
     date_default_timezone_set(defined('CFG_TIME_ZONE') ? CFG_TIME_ZONE : date_default_timezone_get());
   }
-
-/*
- * stripos() natively supported from PHP 5.0
- * From Pear::PHP_Compat
- */
-
-  if (!function_exists('stripos')) {
-    function stripos($haystack, $needle, $offset = null) {
-      $fix = 0;
-
-      if (!is_null($offset)) {
-        if ($offset > 0) {
-          $haystack = substr($haystack, $offset, strlen($haystack) - $offset);
-          $fix = $offset;
-        }
-      }
-
-      $segments = explode(strtolower($needle), strtolower($haystack), 2);
-
-// Check there was a match
-      if (count($segments) == 1) {
-        return false;
-      }
-
-      $position = strlen($segments[0]) + $fix;
-
-      return $position;
-    }
-  }
 ?>
