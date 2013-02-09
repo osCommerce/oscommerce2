b757912509704ff22d1ee4cf93efefc40ff037c9
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-09 15:00:43 +0100

    Convert login to an app

--- catalog/includes/apps/login/controller.php
+++ catalog/includes/apps/login/controller.php
@@ -1,132 +1,89 @@
 <?php
-/*
-  $Id$
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2013 osCommerce
-
-  Released under the GNU General Public License
-*/
-
-  require('includes/application_top.php');
+  class app_login extends app {
+    public function __construct() {
+      global $session_started, $OSCOM_PDO, $messageStack, $breadcrumb;
 
 // redirect the customer to a friendly cookie-must-be-enabled page if cookies are disabled (or the session has not started)
-  if ($session_started == false) {
-    tep_redirect(tep_href_link(FILENAME_COOKIE_USAGE));
-  }
+      if ( $session_started === false ) {
+        tep_redirect(tep_href_link(FILENAME_COOKIE_USAGE));
+      }
 
-  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_LOGIN);
+      $error = false;
 
-  $error = false;
-  if (isset($_GET['action']) && ($_GET['action'] == 'process') && isset($_POST['formid']) && ($_POST['formid'] == $_SESSION['sessiontoken'])) {
-    $email_address = tep_db_prepare_input($_POST['email_address']);
-    $password = tep_db_prepare_input($_POST['password']);
+      if ( isset($_GET['action']) && ($_GET['action'] == 'process') && isset($_POST['formid']) && ($_POST['formid'] == $_SESSION['sessiontoken']) ) {
+        $email_address = isset($_POST['email_address']) ? trim($_POST['email_address']) : null;
+        $password = isset($_POST['password']) ? trim($_POST['password']) : null;
 
 // Check if email exists
-    $check_customer_query = tep_db_query("select customers_id, customers_firstname, customers_password, customers_email_address, customers_default_address_id from " . TABLE_CUSTOMERS . " where customers_email_address = '" . tep_db_input($email_address) . "'");
-    if (!tep_db_num_rows($check_customer_query)) {
-      $error = true;
-    } else {
-      $check_customer = tep_db_fetch_array($check_customer_query);
+        $Qcheck = $OSCOM_PDO->prepare('select customers_id, customers_firstname, customers_password, customers_email_address, customers_default_address_id from :table_customers where customers_email_address = :customers_email_address');
+        $Qcheck->bindValue(':customers_email_address', $email_address);
+        $Qcheck->execute();
+
+        if ( $Qcheck->fetch() === false ) {
+          $error = true;
+        } else {
 // Check that password is good
-      if (!tep_validate_password($password, $check_customer['customers_password'])) {
-        $error = true;
-      } else {
-        if (SESSION_RECREATE == 'True') {
-          tep_session_recreate();
-        }
+          if ( !tep_validate_password($password, $Qcheck->value('customers_password')) ) {
+            $error = true;
+          } else {
+            if ( SESSION_RECREATE == 'True' ) {
+              tep_session_recreate();
+            }
 
 // migrate old hashed password to new phpass password
-        if (tep_password_type($check_customer['customers_password']) != 'phpass') {
-          tep_db_query("update " . TABLE_CUSTOMERS . " set customers_password = '" . tep_encrypt_password($password) . "' where customers_id = '" . (int)$check_customer['customers_id'] . "'");
-        }
+            if ( tep_password_type($Qcheck->value('customers_password')) != 'phpass' ) {
+              $Qupdate = $OSCOM_PDO->prepare('update :table_customers set customers_password = :customers_password where customers_id = :customers_id');
+              $Qupdate->bindValue(':customers_password', tep_encrypt_password($password));
+              $Qupdate->bindInt(':customers_id', $Qcheck->valueInt('customers_id'));
+              $Qupdate->execute();
+            }
 
-        $check_country_query = tep_db_query("select entry_country_id, entry_zone_id from " . TABLE_ADDRESS_BOOK . " where customers_id = '" . (int)$check_customer['customers_id'] . "' and address_book_id = '" . (int)$check_customer['customers_default_address_id'] . "'");
-        $check_country = tep_db_fetch_array($check_country_query);
+            $Qcountry = $OSCOM_PDO->prepare('select entry_country_id, entry_zone_id from :table_address_book where customers_id = :customers_id and address_book_id = :address_book_id');
+            $Qcountry->bindInt(':customers_id', $Qcheck->valueInt('customers_id'));
+            $Qcountry->bindInt(':address_book_id', $Qcheck->valueInt('customers_default_address_id'));
+            $Qcountry->execute();
 
-        $_SESSION['customer_id'] = $check_customer['customers_id'];
-        $_SESSION['customer_default_address_id'] = $check_customer['customers_default_address_id'];
-        $_SESSION['customer_first_name'] = $check_customer['customers_firstname'];
-        $_SESSION['customer_country_id'] = $check_country['entry_country_id'];
-        $_SESSION['customer_zone_id'] = $check_country['entry_zone_id'];
+            $_SESSION['customer_id'] = $Qcheck->valueInt('customers_id');
+            $_SESSION['customer_default_address_id'] = $Qcheck->valueInt('customers_default_address_id');
+            $_SESSION['customer_first_name'] = $Qcheck->value('customers_firstname');
+            $_SESSION['customer_country_id'] = $Qcountry->valueInt('entry_country_id');
+            $_SESSION['customer_zone_id'] = $Qcountry->valueInt('entry_zone_id');
 
-        tep_db_query("update " . TABLE_CUSTOMERS_INFO . " set customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1, password_reset_key = null, password_reset_date = null where customers_info_id = '" . (int)$_SESSION['customer_id'] . "'");
+            $Qupdate = $OSCOM_PDO->prepare('update :table_customers_info set customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1, password_reset_key = null, password_reset_date = null where customers_info_id = :customers_info_id');
+            $Qupdate->bindInt(':customers_info_id', $_SESSION['customer_id']);
+            $Qupdate->execute();
 
 // reset session token
-        $_SESSION['sessiontoken'] = md5(tep_rand() . tep_rand() . tep_rand() . tep_rand());
+            $_SESSION['sessiontoken'] = md5(tep_rand() . tep_rand() . tep_rand() . tep_rand());
 
 // restore cart contents
-        $_SESSION['cart']->restore_contents();
+            $_SESSION['cart']->restore_contents();
 
-        if (sizeof($_SESSION['navigation']->snapshot) > 0) {
-          $origin_href = tep_href_link($_SESSION['navigation']->snapshot['page'], tep_array_to_string($_SESSION['navigation']->snapshot['get'], array(session_name())), $_SESSION['navigation']->snapshot['mode']);
-          $_SESSION['navigation']->clear_snapshot();
-          tep_redirect($origin_href);
-        } else {
-          tep_redirect(tep_href_link(FILENAME_DEFAULT));
+            if ( sizeof($_SESSION['navigation']->snapshot) > 0 ) {
+              $origin_href = tep_href_link($_SESSION['navigation']->snapshot['page'], tep_array_to_string($_SESSION['navigation']->snapshot['get'], array(session_name())), $_SESSION['navigation']->snapshot['mode']);
+
+              $_SESSION['navigation']->clear_snapshot();
+
+              tep_redirect($origin_href);
+            } else {
+              tep_redirect(tep_href_link(FILENAME_DEFAULT));
+            }
+          }
         }
       }
+
+      if ( $error === true ) {
+        $messageStack->add('login', TEXT_LOGIN_ERROR);
+      }
+
+      $breadcrumb->add(NAVBAR_TITLE, tep_href_link(FILENAME_LOGIN, '', 'SSL'));
     }
   }
-
-  if ($error == true) {
-    $messageStack->add('login', TEXT_LOGIN_ERROR);
-  }
-
-  $breadcrumb->add(NAVBAR_TITLE, tep_href_link(FILENAME_LOGIN, '', 'SSL'));
-
-  require(DIR_WS_INCLUDES . 'template_top.php');
-?>
-
-<h1><?php echo HEADING_TITLE; ?></h1>
-
-<?php
-  if ($messageStack->size('login') > 0) {
-    echo $messageStack->output('login');
-  }
-?>
-
-<div class="contentContainer" style="width: 45%; float: left;">
-  <h2><?php echo HEADING_NEW_CUSTOMER; ?></h2>
-
-  <div class="contentText">
-    <p><?php echo TEXT_NEW_CUSTOMER; ?></p>
-    <p><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION; ?></p>
-
-    <p align="right"><?php echo tep_draw_button(IMAGE_BUTTON_CONTINUE, 'triangle-1-e', tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL')); ?></p>
-  </div>
-</div>
-
-<div class="contentContainer" style="width: 45%; float: left; border-left: 1px dashed #ccc; padding-left: 3%; margin-left: 3%;">
-  <h2><?php echo HEADING_RETURNING_CUSTOMER; ?></h2>
-
-  <div class="contentText">
-    <p><?php echo TEXT_RETURNING_CUSTOMER; ?></p>
-
-    <?php echo tep_draw_form('login', tep_href_link(FILENAME_LOGIN, 'action=process', 'SSL'), 'post', '', true); ?>
-
-    <table border="0" cellspacing="0" cellpadding="2" width="100%">
-      <tr>
-        <td class="fieldKey"><?php echo ENTRY_EMAIL_ADDRESS; ?></td>
-        <td class="fieldValue"><?php echo tep_draw_input_field('email_address'); ?></td>
-      </tr>
-      <tr>
-        <td class="fieldKey"><?php echo ENTRY_PASSWORD; ?></td>
-        <td class="fieldValue"><?php echo tep_draw_password_field('password'); ?></td>
-      </tr>
-    </table>
-
-    <p><?php echo '<a href="' . tep_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></p>
-
-    <p align="right"><?php echo tep_draw_button(IMAGE_BUTTON_LOGIN, 'key', null, 'primary'); ?></p>
-
-    </form>
-  </div>
-</div>
-
-<?php
-  require(DIR_WS_INCLUDES . 'template_bottom.php');
-  require(DIR_WS_INCLUDES . 'application_bottom.php');
 ?>
