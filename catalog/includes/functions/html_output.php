f749f38dff4dcedb4d2b9084364e197714613377
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-09 02:03:03 +0100

    Update links to reflect apps

--- catalog/includes/functions/html_output.php
+++ catalog/includes/functions/html_output.php
@@ -12,36 +12,38 @@
 
 ////
 // The HTML href link wrapper function
-  function tep_href_link($page = '', $parameters = '', $connection = 'NONSSL', $add_session_id = true, $search_engine_safe = true) {
+  function tep_href_link($page = null, $parameters = null, $connection = 'NONSSL', $add_session_id = true, $search_engine_safe = true) {
     global $request_type, $session_started, $SID;
 
     $page = tep_output_string($page);
 
-    if (!tep_not_null($page)) {
-      die('</td></tr></table></td></tr></table><br /><br /><font color="#ff0000"><strong>Error!</strong></font><br /><br /><strong>Unable to determine the page link!<br /><br />');
+    if ( $page == 'index' ) {
+      $page = null;
     }
 
-    if ($connection == 'NONSSL') {
-      $link = HTTP_SERVER . DIR_WS_HTTP_CATALOG;
-    } elseif ($connection == 'SSL') {
-      if (ENABLE_SSL == true) {
+    if ( $connection == 'SSL' ) {
+      if ( ENABLE_SSL === true ) {
         $link = HTTPS_SERVER . DIR_WS_HTTPS_CATALOG;
       } else {
         $link = HTTP_SERVER . DIR_WS_HTTP_CATALOG;
       }
     } else {
-      die('</td></tr></table></td></tr></table><br /><br /><font color="#ff0000"><strong>Error!</strong></font><br /><br /><strong>Unable to determine connection method on a link!<br /><br />Known methods: NONSSL SSL</strong><br /><br />');
+      $link = HTTP_SERVER . DIR_WS_HTTP_CATALOG;
     }
 
-    if (tep_not_null($parameters)) {
-      $link .= $page . '?' . tep_output_string($parameters);
+    $link = 'index.php';
+
+    if ( isset($page) ) {
+      $link .= '?' . $page;
       $separator = '&';
     } else {
-      $link .= $page;
       $separator = '?';
     }
 
-    while ( (substr($link, -1) == '&') || (substr($link, -1) == '?') ) $link = substr($link, 0, -1);
+    if ( isset($parameters) ) {
+      $link .= $separator . tep_output_string($parameters);
+      $separator = '&';
+    }
 
 // Add the session ID when moving from different HTTP and HTTPS servers, or when SID is defined
     if ( ($add_session_id == true) && ($session_started == true) && (SESSION_FORCE_COOKIE_USE == 'False') ) {
