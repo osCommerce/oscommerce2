c8ca8bd66bde623650775b30fd95b092cbde4a60
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-10 00:19:04 +0100

    Convert logoff to app

--- catalog/includes/apps/logoff/controller.php
+++ catalog/includes/apps/logoff/controller.php
@@ -1,46 +1,29 @@
 <?php
-/*
-  $Id$
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  class app_logoff extends app {
+    public function __construct() {
+      global $breadcrumb;
 
-  Copyright (c) 2010 osCommerce
+      $breadcrumb->add(NAVBAR_TITLE);
 
-  Released under the GNU General Public License
-*/
+      unset($_SESSION['customer_id']);
+      unset($_SESSION['customer_default_address_id']);
+      unset($_SESSION['customer_first_name']);
+      unset($_SESSION['customer_country_id']);
+      unset($_SESSION['customer_zone_id']);
+      unset($_SESSION['comments']);
 
-  require('includes/application_top.php');
+      $_SESSION['cart']->reset();
 
-  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_LOGOFF);
-
-  $breadcrumb->add(NAVBAR_TITLE);
-
-  unset($_SESSION['customer_id']);
-  unset($_SESSION['customer_default_address_id']);
-  unset($_SESSION['customer_first_name']);
-  unset($_SESSION['customer_country_id']);
-  unset($_SESSION['customer_zone_id']);
-  unset($_SESSION['comments']);
-
-  $_SESSION['cart']->reset();
-
-  require(DIR_WS_INCLUDES . 'template_top.php');
-?>
-
-<h1><?php echo HEADING_TITLE; ?></h1>
-
-<div class="contentContainer">
-  <div class="contentText">
-    <?php echo TEXT_MAIN; ?>
-  </div>
-
-  <div class="buttonSet">
-    <span class="buttonAction"><?php echo tep_draw_button(IMAGE_BUTTON_CONTINUE, 'triangle-1-e', tep_href_link(FILENAME_DEFAULT)); ?></span>
-  </div>
-</div>
-
-<?php
-  require(DIR_WS_INCLUDES . 'template_bottom.php');
-  require(DIR_WS_INCLUDES . 'application_bottom.php');
+      if ( SESSION_RECREATE == 'True' ) {
+        tep_session_recreate();
+      }
+    }
+  }
 ?>
