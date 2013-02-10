5ae1968fe50ca08eb1978324fc87ed1c299a0655
acidvertigo <ldanzelmo@gmail.com>
2013-02-10 16:16:56 +0100

    Update catalog/includes/modules/templates/gosling/content/template_top.php
    HTML5 compliant changes remove type specification

--- catalog/includes/modules/templates/gosling/content/template_top.php
+++ catalog/includes/modules/templates/gosling/content/template_top.php
@@ -22,7 +22,7 @@
 
 <head>
 
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
+<meta charset=<?php echo CHARSET; ?>" />
 
 <title><?php echo tep_output_string_protected($oscTemplate->getTitle()); ?></title>
 
@@ -32,33 +32,33 @@
 
 <meta name="generator" content="osCommerce Online Merchant" />
 
-<script type="text/javascript" src="ext/jquery/jquery-1.9.1.min.js"></script>
+<script src="ext/jquery/jquery-1.9.1.min.js"></script>
 
-<script type="text/javascript" src="ext/bootstrap/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="public/template/gosling/css/general.css" />
-
-
+<script src="ext/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="public/template/gosling/css/general.css" />
 
 
 
 
-<link rel="stylesheet" type="text/css" href="ext/jquery/ui/redmond/jquery-ui-1.8.23.css" />
-<script type="text/javascript" src="ext/jquery/ui/jquery-ui-1.8.23.min.js"></script>
+
+
+<link rel="stylesheet" href="ext/jquery/ui/redmond/jquery-ui-1.8.23.css" />
+<script src="ext/jquery/ui/jquery-ui-1.8.23.min.js"></script>
 
 <?php
   if (tep_not_null(JQUERY_DATEPICKER_I18N_CODE)) {
 ?>
-<script type="text/javascript" src="ext/jquery/ui/i18n/jquery.ui.datepicker-<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>.js"></script>
-<script type="text/javascript">
+<script src="ext/jquery/ui/i18n/jquery.ui.datepicker-<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>.js"></script>
+<script>
 $.datepicker.setDefaults($.datepicker.regional['<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>']);
 </script>
 <?php
   }
 ?>
 
-<script type="text/javascript" src="ext/jquery/bxGallery/jquery.bxGallery.1.1.min.js"></script>
-<link rel="stylesheet" type="text/css" href="ext/jquery/fancybox/jquery.fancybox-1.3.4.css" />
-<script type="text/javascript" src="ext/jquery/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<script src="ext/jquery/bxGallery/jquery.bxGallery.1.1.min.js"></script>
+<link rel="stylesheet" href="ext/jquery/fancybox/jquery.fancybox-1.3.4.css" />
+<script src="ext/jquery/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 
 <?php
   echo $oscTemplate->getBlocks('header_tags');
