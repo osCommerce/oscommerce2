b622cb2c2b48f98015ab399f70d7d9255502b817
GLWalker <webmaster@wsfive.com>
2013-02-10 17:53:45 -0600

    Bootstrap Addition
    Redid footer to HTML5 footer element
    
    Conflicts:
    	admin/includes/footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -8,14 +8,7 @@
   Copyright (c) 2008 osCommerce
 
   Released under the GNU General Public License
-*/
-?>
-<br />
-<table border="0" width="100%" cellspacing="0" cellpadding="2">
-  <tr>
-    <td align="center" class="smallText">
-<?php
-/*
+
   The following copyright announcement is in compliance
   to section 2c of the GNU General Public License, and
   can not be removed, or can only be modified
@@ -30,14 +23,12 @@
   copyright announcement.
 */
 ?>
-osCommerce Online Merchant Copyright &copy; 2010 <a href="http://www.oscommerce.com" target="_blank">osCommerce</a><br />
-osCommerce provides no warranty and is redistributable under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a>
-    </td>
-  </tr>
-  <tr>
-    <td><?php echo osc_image(DIR_WS_IMAGES . 'pixel_trans.gif', '', '1', '5'); ?></td>
-  </tr>
-  <tr>
-    <td align="center" class="smallText">Powered by <a href="http://www.oscommerce.com" target="_blank">osCommerce</a></td>
-  </tr>
-</table>
+<footer class="span12">
+	<p class="text-center">
+		osCommerce Online Merchant Copyright &copy; 2013 <a href="http://www.oscommerce.com" target="_blank">osCommerce</a>
+		<br>
+		osCommerce provides no warranty and is redistributable under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a>
+		<br>
+    	<small>Powered by <a href="http://www.oscommerce.com" target="_blank">osCommerce</a></small>
+	</p>
+</footer>
