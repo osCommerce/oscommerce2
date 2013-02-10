90c3052e3eafe2360a79699ed00251c94f9549ef
GLWalker <webmaster@wsfive.com>
2013-02-10 17:52:56 -0600

    Bootstrap Addition
    Added new class for messageStack & bootstrap alert messages (could be
    in a seperate class file)

--- admin/includes/classes/table_block.php
+++ admin/includes/classes/table_block.php
@@ -77,4 +77,32 @@
       return $tableBox_string;
     }
   }
+  
+  
+// message stack output
+  class alertBlock {
+    var $alert_parameters = '';
+    
+	// class constructor
+    function alertBlock($contents, $alert_output = false) {
+		  
+      for ($i=0, $n=sizeof($contents); $i<$n; $i++) {
+        $alertBox_string .= '  <div class="span8 offset2">' . "\n";
+        $alertBox_string .= '  <div';
+		  
+        if (isset($contents[$i]['params']) && tep_not_null($contents[$i]['params']))
+		  $alertBox_string .= ' ' . $contents[$i]['params'];
+        
+		$alertBox_string .= '>' . "\n";
+        $alertBox_string .= '	<button type="button" class="close" data-dismiss="alert">&times;</button>' . "\n";
+        $alertBox_string .= $contents[$i]['text'];
+    
+        $alertBox_string .= '  </div>' . "\n";
+		$alertBox_string .= '  </div>' . "\n";
+      }
+
+      if ($alert_output == true) echo $alertBox_string;
+        return $alertBox_string;
+    }
+  }    
 ?>
