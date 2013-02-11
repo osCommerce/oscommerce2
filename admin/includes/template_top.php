e191638620f9cb475845a3d94db791b344f75250
GLWalker <webmaster@wsfive.com>
2013-02-10 18:00:50 -0600

    Bootstrap Addition
    Mucho changes for including bootstrap and any needed specifications. A
    temporary CDN call for jQuery UI as updating jQuery broke some elements
    of the older UI. *A start *
    
    Conflicts:
    	admin/includes/template_top.php

--- admin/includes/template_top.php
+++ admin/includes/template_top.php
@@ -10,36 +10,62 @@
   Released under the GNU General Public License
 */
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!doctype html>
 <html <?php echo HTML_PARAMS; ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
 <meta name="robots" content="noindex,nofollow">
 <title><?php echo TITLE; ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <base href="<?php echo HTTP_SERVER . DIR_WS_ADMIN; ?>" />
-<!--[if IE]><script type="text/javascript" src="<?php echo osc_catalog_href_link('ext/flot/excanvas.min.js'); ?>"></script><![endif]-->
+<!--[if IE]><script src="<?php echo osc_catalog_href_link('ext/flot/excanvas.min.js'); ?>"></script><![endif]-->
+<link rel="stylesheet" type="text/css" href="<?php echo osc_catalog_href_link('ext/bootstrap/css/bootstrap.min.css'); ?>" />
+    <style type="text/css">
+    body {
+		padding-top: 60px;
+		padding-bottom: 40px;
+	 }
+      @media (max-width: 980px) {
+        /* Enable use of floated navbar text */
+        .navbar-text.pull-right {
+          float: none;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+      }
+    </style>
+<link rel="stylesheet" type="text/css" href="<?php echo osc_catalog_href_link('ext/bootstrap/css/bootstrap-responsive.min.css'); ?>" />
+<!-- this is until boot strap is 100% -->
 <link rel="stylesheet" type="text/css" href="<?php echo osc_catalog_href_link('ext/jquery/ui/redmond/jquery-ui-1.8.23.css'); ?>">
-<script type="text/javascript" src="<?php echo osc_catalog_href_link('ext/jquery/jquery-1.8.0.min.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo osc_catalog_href_link('ext/jquery/ui/jquery-ui-1.8.23.min.js'); ?>"></script>
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+<script src="includes/general.js"></script>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="<?php echo osc_catalog_href_link('ext/assets/js/html5shiv.js'); ?>"></script>
+    <![endif]-->
+<script src="<?php echo osc_catalog_href_link('ext/jquery/jquery-1.9.1.min.js'); ?>"></script>
+<script src="<?php echo osc_catalog_href_link('ext/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<!-- CDN is only for UI compatibility with jquery-1.9.1 untill boot strap is 100% integrated -->
+<script src="//code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
 
 <?php
   if (osc_not_null(JQUERY_DATEPICKER_I18N_CODE)) {
 ?>
-<script type="text/javascript" src="<?php echo osc_catalog_href_link('ext/jquery/ui/i18n/jquery.ui.datepicker-' . JQUERY_DATEPICKER_I18N_CODE . '.js'); ?>"></script>
+<script src="<?php echo osc_catalog_href_link('ext/jquery/ui/i18n/jquery.ui.datepicker-' . JQUERY_DATEPICKER_I18N_CODE . '.js'); ?>"></script>
 <script type="text/javascript">
 $.datepicker.setDefaults($.datepicker.regional['<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>']);
 </script>
 <?php
   }
 ?>
-
-<script type="text/javascript" src="<?php echo osc_catalog_href_link('ext/flot/jquery.flot.js'); ?>"></script>
-<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-<script type="text/javascript" src="includes/general.js"></script>
+<script src="<?php echo osc_catalog_href_link('ext/flot/jquery.flot.js'); ?>"></script>
 </head>
 <body>
 
 <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+
+<div class="container-fluid">
+	<div class="row-fluid">
 
 <?php
   if (isset($_SESSION['admin'])) {
@@ -48,7 +74,7 @@ $.datepicker.setDefaults($.datepicker.regional['<?php echo JQUERY_DATEPICKER_I18
 ?>
 
 <style>
-#contentText {
+#bodyContent {
   margin-left: 0;
 }
 </style>
@@ -57,4 +83,4 @@ $.datepicker.setDefaults($.datepicker.regional['<?php echo JQUERY_DATEPICKER_I18
   }
 ?>
 
-<div id="contentText">
+<section id="bodyContent">
