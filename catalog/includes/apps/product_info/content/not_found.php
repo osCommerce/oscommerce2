f3c92fa75059debcf0a808bb070860d2f7113bfa
Gergely Tóth <tgely@gumisarok.hu>
2013-02-14 18:30:21 +0100

    Convert product info to apps
    Signed-off-by: Gergely Tóth <tgely@gumisarok.hu>

---
+++ catalog/includes/apps/product_info/content/not_found.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ *
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
+?>
+
+<div class="contentContainer">
+  <div class="contentText">
+    <?php echo TEXT_PRODUCT_NOT_FOUND; ?>
+  </div>
+
+  <div style="float: right;">
+    <?php echo tep_draw_button(IMAGE_BUTTON_CONTINUE, 'triangle-1-e', tep_href_link(FILENAME_DEFAULT)); ?>
+  </div>
+</div>
