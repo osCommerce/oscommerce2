9f836c9be47d1827a4e1842042110c246bd3293e
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-19 00:48:33 +0100

    Move global update_product action to cart app action

---
+++ catalog/includes/apps/cart/actions/update.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
+
+  class app_cart_action_update {
+    public static function execute(app $app) {
+      if ( isset($_POST['formid']) && ($_POST['formid'] == $_SESSION['sessiontoken']) ) {
+        for ($i=0, $n=sizeof($_POST['products_id']); $i<$n; $i++) {
+          $attributes = ($_POST['id'][$_POST['products_id'][$i]]) ? $_POST['id'][$_POST['products_id'][$i]] : '';
+          $_SESSION['cart']->add_cart($_POST['products_id'][$i], $_POST['cart_quantity'][$i], $attributes, false);
+        }
+      }
+
+      tep_redirect(tep_href_link('cart'));
+    }
+  }
+?>
