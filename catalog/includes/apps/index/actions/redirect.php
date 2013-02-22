b5d59688c853936bff05a9fd5bf17cfff43909af
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 20:26:58 +0100

    Update index app redirect action

--- catalog/includes/apps/index/actions/redirect.php
+++ catalog/includes/apps/index/actions/redirect.php
@@ -1,65 +1,13 @@
 <?php
-/*
-  $Id$
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2013 osCommerce
-
-  Released under the GNU General Public License
-*/
-
-  require('includes/application_top.php');
-
-  switch ($_GET['action']) {
-    case 'banner':
-      $banner_query = tep_db_query("select banners_url from " . TABLE_BANNERS . " where banners_id = '" . (int)$_GET['goto'] . "'");
-      if (tep_db_num_rows($banner_query)) {
-        $banner = tep_db_fetch_array($banner_query);
-        tep_update_banner_click_count($_GET['goto']);
-
-        tep_redirect($banner['banners_url']);
-      }
-      break;
-
-    case 'url':
-      if (isset($_GET['goto']) && tep_not_null($_GET['goto'])) {
-        $check_query = tep_db_query("select products_url from " . TABLE_PRODUCTS_DESCRIPTION . " where products_url = '" . tep_db_input($_GET['goto']) . "' limit 1");
-        if (tep_db_num_rows($check_query)) {
-          tep_redirect('http://' . $_GET['goto']);
-        }
-      }
-      break;
-
-    case 'manufacturer':
-      if (isset($_GET['manufacturers_id']) && tep_not_null($_GET['manufacturers_id'])) {
-        $manufacturer_query = tep_db_query("select manufacturers_url from " . TABLE_MANUFACTURERS_INFO . " where manufacturers_id = '" . (int)$_GET['manufacturers_id'] . "' and languages_id = '" . (int)$_SESSION['languages_id'] . "'");
-        if (tep_db_num_rows($manufacturer_query)) {
-// url exists in selected language
-          $manufacturer = tep_db_fetch_array($manufacturer_query);
-
-          if (tep_not_null($manufacturer['manufacturers_url'])) {
-            tep_db_query("update " . TABLE_MANUFACTURERS_INFO . " set url_clicked = url_clicked+1, date_last_click = now() where manufacturers_id = '" . (int)$_GET['manufacturers_id'] . "' and languages_id = '" . (int)$_SESSION['languages_id'] . "'");
-
-            tep_redirect($manufacturer['manufacturers_url']);
-          }
-        } else {
-// no url exists for the selected language, lets use the default language then
-          $manufacturer_query = tep_db_query("select mi.languages_id, mi.manufacturers_url from " . TABLE_MANUFACTURERS_INFO . " mi, " . TABLE_LANGUAGES . " l where mi.manufacturers_id = '" . (int)$_GET['manufacturers_id'] . "' and mi.languages_id = l.languages_id and l.code = '" . DEFAULT_LANGUAGE . "'");
-          if (tep_db_num_rows($manufacturer_query)) {
-            $manufacturer = tep_db_fetch_array($manufacturer_query);
-
-            if (tep_not_null($manufacturer['manufacturers_url'])) {
-              tep_db_query("update " . TABLE_MANUFACTURERS_INFO . " set url_clicked = url_clicked+1, date_last_click = now() where manufacturers_id = '" . (int)$_GET['manufacturers_id'] . "' and languages_id = '" . (int)$manufacturer['languages_id'] . "'");
-
-              tep_redirect($manufacturer['manufacturers_url']);
-            }
-          }
-        }
-      }
-      break;
+  class app_index_action_redirect {
+    public static function execute(app $app) {
+    }
   }
-
-  tep_redirect(tep_href_link(FILENAME_DEFAULT));
 ?>
