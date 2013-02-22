c6ebe3c4e5100c38f7a461ab51f4190a13f38f3c
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 12:10:19 +0100

    Update search app

---
+++ catalog/includes/apps/search/content/results.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ *
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
+?>
+
+<h1><?php echo HEADING_TITLE_2; ?></h1>
+
+<div class="contentContainer">
+
+<?php
+// create column list
+  $define_list = array('PRODUCT_LIST_MODEL' => PRODUCT_LIST_MODEL,
+                       'PRODUCT_LIST_NAME' => PRODUCT_LIST_NAME,
+                       'PRODUCT_LIST_MANUFACTURER' => PRODUCT_LIST_MANUFACTURER,
+                       'PRODUCT_LIST_PRICE' => PRODUCT_LIST_PRICE,
+                       'PRODUCT_LIST_QUANTITY' => PRODUCT_LIST_QUANTITY,
+                       'PRODUCT_LIST_WEIGHT' => PRODUCT_LIST_WEIGHT,
+                       'PRODUCT_LIST_IMAGE' => PRODUCT_LIST_IMAGE,
+                       'PRODUCT_LIST_BUY_NOW' => PRODUCT_LIST_BUY_NOW);
+
+  asort($define_list);
+
+  $column_list = array();
+  reset($define_list);
+  while (list($key, $value) = each($define_list)) {
+    if ($value > 0) $column_list[] = $key;
+  }
+
+  $select_column_list = '';
+
+  for ($i=0, $n=sizeof($column_list); $i<$n; $i++) {
+    switch ($column_list[$i]) {
+      case 'PRODUCT_LIST_MODEL':
+        $select_column_list .= 'p.products_model, ';
+        break;
+      case 'PRODUCT_LIST_MANUFACTURER':
+        $select_column_list .= 'm.manufacturers_name, ';
+        break;
+      case 'PRODUCT_LIST_QUANTITY':
+        $select_column_list .= 'p.products_quantity, ';
+        break;
+      case 'PRODUCT_LIST_IMAGE':
+        $select_column_list .= 'p.products_image, ';
+        break;
+      case 'PRODUCT_LIST_WEIGHT':
+        $select_column_list .= 'p.products_weight, ';
+        break;
+    }
+  }
+
+  $select_str = "select distinct " . $select_column_list . " m.manufacturers_id, p.products_id, pd.products_name, p.products_price, p.products_tax_class_id, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, IF(s.status, s.specials_new_products_price, p.products_price) as final_price ";
+
+  if ( (DISPLAY_PRICE_WITH_TAX == 'true') && (tep_not_null($pfrom) || tep_not_null($pto)) ) {
+    $select_str .= ", SUM(tr.tax_rate) as tax_rate ";
+  }
+
+  $from_str = "from " . TABLE_PRODUCTS . " p left join " . TABLE_MANUFACTURERS . " m using(manufacturers_id) left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id";
+
+  if ( (DISPLAY_PRICE_WITH_TAX == 'true') && (tep_not_null($pfrom) || tep_not_null($pto)) ) {
+    if (!isset($_SESSION['customer_country_id'])) {
+      $_SESSION['customer_country_id'] = STORE_COUNTRY;
+      $_SESSION['customer_zone_id'] = STORE_ZONE;
+    }
+    $from_str .= " left join " . TABLE_TAX_RATES . " tr on p.products_tax_class_id = tr.tax_class_id left join " . TABLE_ZONES_TO_GEO_ZONES . " gz on tr.tax_zone_id = gz.geo_zone_id and (gz.zone_country_id is null or gz.zone_country_id = '0' or gz.zone_country_id = '" . (int)$_SESSION['customer_country_id'] . "') and (gz.zone_id is null or gz.zone_id = '0' or gz.zone_id = '" . (int)$_SESSION['customer_zone_id'] . "')";
+  }
+
+  $from_str .= ", " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_CATEGORIES . " c, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c";
+
+  $where_str = " where p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' and p.products_id = p2c.products_id and p2c.categories_id = c.categories_id ";
+
+  if (isset($_GET['categories_id']) && tep_not_null($_GET['categories_id'])) {
+    if (isset($_GET['inc_subcat']) && ($_GET['inc_subcat'] == '1')) {
+      $subcategories_array = array();
+      tep_get_subcategories($subcategories_array, $_GET['categories_id']);
+
+      $where_str .= " and p2c.products_id = p.products_id and p2c.products_id = pd.products_id and (p2c.categories_id = '" . (int)$_GET['categories_id'] . "'";
+
+      for ($i=0, $n=sizeof($subcategories_array); $i<$n; $i++ ) {
+        $where_str .= " or p2c.categories_id = '" . (int)$subcategories_array[$i] . "'";
+      }
+
+      $where_str .= ")";
+    } else {
+      $where_str .= " and p2c.products_id = p.products_id and p2c.products_id = pd.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' and p2c.categories_id = '" . (int)$_GET['categories_id'] . "'";
+    }
+  }
+
+  if (isset($_GET['manufacturers_id']) && tep_not_null($_GET['manufacturers_id'])) {
+    $where_str .= " and m.manufacturers_id = '" . (int)$_GET['manufacturers_id'] . "'";
+  }
+
+  if (isset($search_keywords) && (sizeof($search_keywords) > 0)) {
+    $where_str .= " and (";
+    for ($i=0, $n=sizeof($search_keywords); $i<$n; $i++ ) {
+      switch ($search_keywords[$i]) {
+        case '(':
+        case ')':
+        case 'and':
+        case 'or':
+          $where_str .= " " . $search_keywords[$i] . " ";
+          break;
+        default:
+          $keyword = tep_db_prepare_input($search_keywords[$i]);
+          $where_str .= "(pd.products_name like '%" . tep_db_input($keyword) . "%' or p.products_model like '%" . tep_db_input($keyword) . "%' or m.manufacturers_name like '%" . tep_db_input($keyword) . "%' or pd.products_description like '%" . tep_db_input($keyword) . "%')";
+          break;
+      }
+    }
+    $where_str .= " )";
+  }
+
+  if (tep_not_null($dfrom)) {
+    $where_str .= " and p.products_date_added >= '" . tep_date_raw($dfrom) . "'";
+  }
+
+  if (tep_not_null($dto)) {
+    $where_str .= " and p.products_date_added <= '" . tep_date_raw($dto) . "'";
+  }
+
+  if (tep_not_null($pfrom)) {
+    if ($currencies->is_set($_SESSION['currency'])) {
+      $rate = $currencies->get_value($_SESSION['currency']);
+
+      $pfrom = $pfrom / $rate;
+    }
+  }
+
+  if (tep_not_null($pto)) {
+    if (isset($rate)) {
+      $pto = $pto / $rate;
+    }
+  }
+
+  if (DISPLAY_PRICE_WITH_TAX == 'true') {
+    if ($pfrom > 0) $where_str .= " and (IF(s.status, s.specials_new_products_price, p.products_price) * if(gz.geo_zone_id is null, 1, 1 + (tr.tax_rate / 100) ) >= " . (double)$pfrom . ")";
+    if ($pto > 0) $where_str .= " and (IF(s.status, s.specials_new_products_price, p.products_price) * if(gz.geo_zone_id is null, 1, 1 + (tr.tax_rate / 100) ) <= " . (double)$pto . ")";
+  } else {
+    if ($pfrom > 0) $where_str .= " and (IF(s.status, s.specials_new_products_price, p.products_price) >= " . (double)$pfrom . ")";
+    if ($pto > 0) $where_str .= " and (IF(s.status, s.specials_new_products_price, p.products_price) <= " . (double)$pto . ")";
+  }
+
+  if ( (DISPLAY_PRICE_WITH_TAX == 'true') && (tep_not_null($pfrom) || tep_not_null($pto)) ) {
+    $where_str .= " group by p.products_id, tr.tax_priority";
+  }
+
+  if ( (!isset($_GET['sort'])) || (!preg_match('/^[1-8][ad]$/', $_GET['sort'])) || (substr($_GET['sort'], 0, 1) > sizeof($column_list)) ) {
+    for ($i=0, $n=sizeof($column_list); $i<$n; $i++) {
+      if ($column_list[$i] == 'PRODUCT_LIST_NAME') {
+        $_GET['sort'] = $i+1 . 'a';
+        $order_str = " order by pd.products_name";
+        break;
+      }
+    }
+  } else {
+    $sort_col = substr($_GET['sort'], 0 , 1);
+    $sort_order = substr($_GET['sort'], 1);
+
+    switch ($column_list[$sort_col-1]) {
+      case 'PRODUCT_LIST_MODEL':
+        $order_str = " order by p.products_model " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_NAME':
+        $order_str = " order by pd.products_name " . ($sort_order == 'd' ? 'desc' : '');
+        break;
+      case 'PRODUCT_LIST_MANUFACTURER':
+        $order_str = " order by m.manufacturers_name " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_QUANTITY':
+        $order_str = " order by p.products_quantity " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_IMAGE':
+        $order_str = " order by pd.products_name";
+        break;
+      case 'PRODUCT_LIST_WEIGHT':
+        $order_str = " order by p.products_weight " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_PRICE':
+        $order_str = " order by final_price " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+    }
+  }
+
+  $listing_sql = $select_str . $from_str . $where_str . $order_str;
+
+  require(DIR_FS_CATALOG . DIR_WS_MODULES . FILENAME_PRODUCT_LISTING);
+?>
+
+  <br />
+
+  <div class="buttonSet">
+    <?php echo tep_draw_button(IMAGE_BUTTON_BACK, 'triangle-1-w', tep_href_link('search', tep_get_all_get_params(array('search', 'sort', 'page')), 'NONSSL', true, false)); ?>
+  </div>
+</div>
