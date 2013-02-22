a71de7c6d7813803d179b370f2fec1e9e89bbcd1
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 22:34:44 +0100

    Update open search

--- catalog/includes/apps/search/actions/opensearch.php
+++ catalog/includes/apps/search/actions/opensearch.php
@@ -1,56 +1,55 @@
 <?php
-/*
-  $Id$
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  class app_search_action_opensearch {
+    public static function execute(app $app) {
+      if ( !defined('MODULE_HEADER_TAGS_OPENSEARCH_STATUS') || (MODULE_HEADER_TAGS_OPENSEARCH_STATUS != 'True') ) {
+        exit;
+      }
 
-  Copyright (c) 2010 osCommerce
+      header('Content-Type: text/xml');
 
-  Released under the GNU General Public License
-*/
-
-  header('Content-Type: text/xml');
-
-  require('includes/application_top.php');
-
-  if ( !defined('MODULE_HEADER_TAGS_OPENSEARCH_STATUS') || (MODULE_HEADER_TAGS_OPENSEARCH_STATUS != 'True') ) {
-    exit;
-  }
-
-  echo '<?xml version="1.0"?>' . "\n";
-?>
+      $result = '<?xml version="1.0"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-  <ShortName><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_SHORT_NAME); ?></ShortName>
-  <Description><?php echo tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_DESCRIPTION); ?></Description>
-<?php
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT)) {
-    echo '  <Contact>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT) . '</Contact>' . "\n";
-  }
+  <ShortName>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_SHORT_NAME) . '</ShortName>
+  <Description>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_DESCRIPTION) . '</Description>';
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS)) {
-    echo '  <Tags>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS) . '</Tags>' . "\n";
-  }
+      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT)) {
+        $result .= '  <Contact>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT) . '</Contact>' . "\n";
+      }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION)) {
-    echo '  <Attribution>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION) . '</Attribution>' . "\n";
-  }
+      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS)) {
+        $result .= '  <Tags>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS) . '</Tags>' . "\n";
+      }
 
-  if (MODULE_HEADER_TAGS_OPENSEARCH_SITE_ADULT_CONTENT == 'True') {
-    echo '  <AdultContent>True</AdultContent>' . "\n";
-  }
+      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION)) {
+        $result .= '  <Attribution>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION) . '</Attribution>' . "\n";
+      }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON)) {
-    echo '  <Image height="16" width="16" type="image/x-icon">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON) . '</Image>' . "\n";
-  }
+      if (MODULE_HEADER_TAGS_OPENSEARCH_SITE_ADULT_CONTENT == 'True') {
+        $result .= '  <AdultContent>True</AdultContent>' . "\n";
+      }
 
-  if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE)) {
-    echo '  <Image height="64" width="64" type="image/png">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE) . '</Image>' . "\n";
+      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON)) {
+        $result .= '  <Image height="16" width="16" type="image/x-icon">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON) . '</Image>' . "\n";
+      }
+
+      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE)) {
+        $result .= '  <Image height="64" width="64" type="image/png">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE) . '</Image>' . "\n";
+      }
+
+      $result .= '  <InputEncoding>UTF-8</InputEncoding>
+  <Url type="text/html" method="get" template="' . tep_href_link('search', 'q={searchTerms}', 'NONSSL', false) . '" />
+</OpenSearchDescription>';
+
+      echo $result;
+
+      exit;
+    }
   }
-?>
-  <InputEncoding>UTF-8</InputEncoding>
-  <Url type="text/html" method="get" template="<?php echo tep_href_link('search', 'q={searchTerms}', 'NONSSL', false); ?>" />
-</OpenSearchDescription>
-<?php
-  require('includes/application_bottom.php');
 ?>
