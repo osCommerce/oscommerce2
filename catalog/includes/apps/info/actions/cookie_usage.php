c9c26340db1614629078f9dbf4f6f0103b997556
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 12:18:32 +0100

    Update info app cookie usage action

--- catalog/includes/apps/info/actions/cookie_usage.php
+++ catalog/includes/apps/info/actions/cookie_usage.php
@@ -1,45 +1,18 @@
 <?php
-/*
-  $Id$
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  class app_info_action_cookie_usage {
+    public static function execute(app $app) {
+      global $breadcrumb;
 
-  Copyright (c) 2010 osCommerce
+      $app->setContentFile('cookie_usage.php');
 
-  Released under the GNU General Public License
-*/
-
-  require('includes/application_top.php');
-
-  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_COOKIE_USAGE);
-
-  $breadcrumb->add(NAVBAR_TITLE, tep_href_link(FILENAME_COOKIE_USAGE));
-
-  require(DIR_WS_INCLUDES . 'template_top.php');
-?>
-
-<h1><?php echo HEADING_TITLE; ?></h1>
-
-<div class="contentContainer">
-  <div class="contentText">
-    <div class="ui-widget infoBoxContainer" style=" width: 40%; float: right; padding: 0 0 10px 10px;">
-      <div class="ui-widget-header infoBoxHeading"><?php echo BOX_INFORMATION_HEADING; ?></div>
-
-      <div class="ui-widget-content infoBoxContents">
-        <?php echo BOX_INFORMATION; ?>
-      </div>
-    </div>
-
-    <?php echo TEXT_INFORMATION; ?>
-  </div>
-
-  <div class="buttonSet">
-    <span class="buttonAction"><?php echo tep_draw_button(IMAGE_BUTTON_CONTINUE, 'triangle-1-e', tep_href_link(FILENAME_DEFAULT)); ?></span>
-  </div>
-</div>
-
-<?php
-  require(DIR_WS_INCLUDES . 'template_bottom.php');
-  require(DIR_WS_INCLUDES . 'application_bottom.php');
+      $breadcrumb->add(NAVBAR_TITLE_COOKIE_USAGE, tep_href_link('info', 'cookie_usage'));
+    }
+  }
 ?>
