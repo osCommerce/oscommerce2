af213a03cbf5d80c4b00d7ccac3b370fa87318f5
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 20:26:58 +0100

    Update index app redirect action

--- catalog/includes/functions/banner.php
+++ catalog/includes/functions/banner.php
@@ -83,7 +83,7 @@
     if (tep_not_null($banner['banners_html_text'])) {
       $banner_string = $banner['banners_html_text'];
     } else {
-      $banner_string = '<a href="' . tep_href_link(FILENAME_REDIRECT, 'action=banner&goto=' . $banner['banners_id']) . '" target="_blank">' . tep_image(DIR_WS_IMAGES . $banner['banners_image'], $banner['banners_title']) . '</a>';
+      $banner_string = '<a href="' . tep_href_link('index', 'redirect&banner=' . $banner['banners_id']) . '" target="_blank">' . tep_image(DIR_WS_IMAGES . $banner['banners_image'], $banner['banners_title']) . '</a>';
     }
 
     tep_update_banner_display_count($banner['banners_id']);
