a0d81c83f67607850b071b6bf6c5ee97d4e84414
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 18:02:32 +0100

    Update products app reviews action

--- catalog/includes/languages/english/products.php
+++ catalog/includes/languages/english/products.php
@@ -57,4 +57,16 @@ define('ERROR_FROM_ADDRESS', 'Error: Your e-mail address must be a valid e-mail 
 define('ERROR_ACTION_RECORDER', 'Error: An e-mail has already been sent. Please try again in %s minutes.');
 
 define('TEXT_OF_5_STARS', '%s of 5 Stars!');
+
+define('SUB_TITLE_PRODUCT', 'Product:');
+define('SUB_TITLE_FROM', 'From:');
+define('SUB_TITLE_DATE', 'Date:');
+define('SUB_TITLE_REVIEW', 'Review:');
+define('SUB_TITLE_RATING', 'Rating:');
+
+define('TEXT_NO_HTML', '<small><font color="#ff0000"><strong>NOTE:</strong></font></small>&nbsp;HTML is not translated!');
+define('TEXT_BAD', '<small><font color="#ff0000"><strong>BAD</strong></font></small>');
+define('TEXT_GOOD', '<small><font color="#ff0000"><strong>GOOD</strong></font></small>');
+
+define('TEXT_REVIEW_RECEIVED', 'Thank you for your review. It has been submitted to the store owner for approval and should appear shortly.');
 ?>
