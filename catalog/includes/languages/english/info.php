0e48c1e66106c0fa7a8affbf33a5c5272ac6c939
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-22 12:18:32 +0100

    Update info app cookie usage action

--- catalog/includes/languages/english/info.php
+++ catalog/includes/languages/english/info.php
@@ -29,4 +29,12 @@ define('ENTRY_NAME', 'Full Name:');
 define('ENTRY_EMAIL', 'E-Mail Address:');
 define('ENTRY_ENQUIRY', 'Enquiry:');
 define('ERROR_ACTION_RECORDER', 'Error: An enquiry has already been sent. Please try again in %s minutes.');
+
+define('NAVBAR_TITLE_COOKIE_USAGE', 'Cookie Usage');
+define('HEADING_TITLE_COOKIE_USAGE', 'Cookie Usage');
+
+define('TEXT_INFORMATION_COOKIE_USAGE', 'We have detected that your browser does not support cookies, or has set cookies to be disabled.<br /><br />To continue shopping online, we encourage you to enable cookies on your browser.<br /><br />For <strong>Internet Explorer</strong> browsers, please follow these instructions:<br /><ol><li>Click on the Tools menubar, and select Internet Options</li><li>Select the Security tab, and reset the security level to Medium</li></ol>We have taken this measurement of security for your benefit, and apologize upfront if any inconveniences are caused.<br /><br />Please contact the store owner if you have any questions relating to this requirement, or to continue purchasing products offline.');
+
+define('BOX_INFORMATION_HEADING_COOKIE_USAGE', 'Cookie Privacy and Security');
+define('BOX_INFORMATION_COOKIE_USAGE', 'Cookies must be enabled to purchase online on this store to embrace privacy and security related issues regarding your visit to this site.<br /><br />By enabling cookie support on your browser, the communication between you and this site is strengthened to be certain it is you who are making transactions on your own behalf, and to prevent leakage of your privacy information.');
 ?>
