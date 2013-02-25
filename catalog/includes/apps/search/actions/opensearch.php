8af7dff70f7a4217b03664ae8bd88e88a3affaf4
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-02-25 19:13:45 +0100

    Replace tep_ with osc_

--- catalog/includes/apps/search/actions/opensearch.php
+++ catalog/includes/apps/search/actions/opensearch.php
@@ -16,35 +16,35 @@
 
       $result = '<?xml version="1.0"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-  <ShortName>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_SHORT_NAME) . '</ShortName>
-  <Description>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_DESCRIPTION) . '</Description>';
+  <ShortName>' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_SHORT_NAME) . '</ShortName>
+  <Description>' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_DESCRIPTION) . '</Description>';
 
-      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT)) {
-        $result .= '  <Contact>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT) . '</Contact>' . "\n";
+      if (osc_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT)) {
+        $result .= '  <Contact>' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_CONTACT) . '</Contact>' . "\n";
       }
 
-      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS)) {
-        $result .= '  <Tags>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS) . '</Tags>' . "\n";
+      if (osc_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS)) {
+        $result .= '  <Tags>' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_TAGS) . '</Tags>' . "\n";
       }
 
-      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION)) {
-        $result .= '  <Attribution>' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION) . '</Attribution>' . "\n";
+      if (osc_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION)) {
+        $result .= '  <Attribution>' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ATTRIBUTION) . '</Attribution>' . "\n";
       }
 
       if (MODULE_HEADER_TAGS_OPENSEARCH_SITE_ADULT_CONTENT == 'True') {
         $result .= '  <AdultContent>True</AdultContent>' . "\n";
       }
 
-      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON)) {
-        $result .= '  <Image height="16" width="16" type="image/x-icon">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON) . '</Image>' . "\n";
+      if (osc_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON)) {
+        $result .= '  <Image height="16" width="16" type="image/x-icon">' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_ICON) . '</Image>' . "\n";
       }
 
-      if (tep_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE)) {
-        $result .= '  <Image height="64" width="64" type="image/png">' . tep_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE) . '</Image>' . "\n";
+      if (osc_not_null(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE)) {
+        $result .= '  <Image height="64" width="64" type="image/png">' . osc_output_string(MODULE_HEADER_TAGS_OPENSEARCH_SITE_IMAGE) . '</Image>' . "\n";
       }
 
       $result .= '  <InputEncoding>UTF-8</InputEncoding>
-  <Url type="text/html" method="get" template="' . tep_href_link('search', 'q={searchTerms}', 'NONSSL', false) . '" />
+  <Url type="text/html" method="get" template="' . osc_href_link('search', 'q={searchTerms}', 'NONSSL', false) . '" />
 </OpenSearchDescription>';
 
       echo $result;
