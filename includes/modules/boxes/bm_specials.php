b8c2d628e40e73f152f20b75ea18ac4e9afba057
GBurton <oscshops@gmail.com>
2013-02-27 23:11:12 +0000

    Update specials box

--- includes/modules/boxes/bm_specials.php
+++ includes/modules/boxes/bm_specials.php
@@ -35,10 +35,10 @@
 
       if ( $OSCOM_APP->getCode() != 'products' ) {
         if ($random_product = osc_random_select("select p.products_id, pd.products_name, p.products_price, p.products_tax_class_id, p.products_image, s.specials_new_products_price from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_SPECIALS . " s where p.products_status = '1' and p.products_id = s.products_id and pd.products_id = s.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' and s.status = '1' order by s.specials_date_added desc limit " . MAX_RANDOM_SELECT_SPECIALS)) {
-          $data = '<div class="ui-widget infoBoxContainer">' .
-                  '  <div class="ui-widget-header infoBoxHeading"><a href="' . osc_href_link('products', 'specials') . '">' . MODULE_BOXES_SPECIALS_BOX_TITLE . '</a></div>' .
-                  '  <div class="ui-widget-content infoBoxContents" style="text-align: center;"><a href="' . osc_href_link('products', 'id=' . $random_product["products_id"]) . '">' . osc_image(DIR_WS_IMAGES . $random_product['products_image'], $random_product['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br /><a href="' . osc_href_link('products', 'id=' . $random_product['products_id']) . '">' . $random_product['products_name'] . '</a><br /><del>' . $currencies->display_price($random_product['products_price'], osc_get_tax_rate($random_product['products_tax_class_id'])) . '</del><br /><span class="productSpecialPrice">' . $currencies->display_price($random_product['specials_new_products_price'], osc_get_tax_rate($random_product['products_tax_class_id'])) . '</span></div>' .
-                  '</div>';
+          $data = '<li class="nav-header"><a href="' . osc_href_link('products', 'specials') . '">' . MODULE_BOXES_SPECIALS_BOX_TITLE . '</a></li>' .
+                  '<li style="text-align: center;"><a href="' . osc_href_link('products', 'id=' . $random_product["products_id"]) . '">' . osc_image(DIR_WS_IMAGES . $random_product['products_image'], $random_product['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a></li>' .
+                  '<li style="text-align: center;"><a href="' . osc_href_link('products', 'id=' . $random_product['products_id']) . '">' . $random_product['products_name'] . '</a></li>' .
+                  '<li style="text-align: center;"><del>' . $currencies->display_price($random_product['products_price'], osc_get_tax_rate($random_product['products_tax_class_id'])) . '</del> <span class="productSpecialPrice">' . $currencies->display_price($random_product['specials_new_products_price'], osc_get_tax_rate($random_product['products_tax_class_id'])) . '</span></li>';
 
           $oscTemplate->addBlock($data, $this->group);
         }
