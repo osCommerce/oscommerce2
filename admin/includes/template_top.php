bdc1c1b3a03d4d500f9485b8d4a7a83940b96c78
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-03-02 14:34:00 +0100

    cleanup

--- admin/includes/template_top.php
+++ admin/includes/template_top.php
@@ -12,38 +12,57 @@
 ?>
 <!doctype html>
 <html <?php echo HTML_PARAMS; ?>>
+
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
+
 <meta name="robots" content="noindex,nofollow">
+
 <title><?php echo TITLE; ?></title>
+
 <base href="<?php echo HTTP_SERVER . DIR_WS_ADMIN; ?>" />
+
 <!--[if IE]><script src="<?php echo osc_catalog_href_link('ext/flot/excanvas.min.js'); ?>"></script><![endif]-->
+
 <link rel="stylesheet" type="text/css" href="<?php echo osc_catalog_href_link('ext/bootstrap/css/bootstrap.min.css'); ?>" />
+
 <!-- this is until boot strap is 100% -->
 <link rel="stylesheet" type="text/css" href="<?php echo osc_catalog_href_link('ext/jquery/ui/redmond/jquery-ui-1.8.23.css'); ?>">
+
 <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+
 <script src="includes/general.js"></script>
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="<?php echo osc_catalog_href_link('ext/assets/js/html5shiv.js'); ?>"></script>
-    <![endif]-->
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+  <script src="<?php echo osc_catalog_href_link('ext/assets/js/html5shiv.js'); ?>"></script>
+<![endif]-->
+
 <script src="<?php echo osc_catalog_href_link('ext/jquery/jquery-1.9.1.min.js'); ?>"></script>
+
 <script src="<?php echo osc_catalog_href_link('ext/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
 <!-- CDN is only for UI compatibility with jquery-1.9.1 untill boot strap is 100% integrated -->
 <script src="//code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
 
 <?php
   if (osc_not_null(JQUERY_DATEPICKER_I18N_CODE)) {
 ?>
+
 <script src="<?php echo osc_catalog_href_link('ext/jquery/ui/i18n/jquery.ui.datepicker-' . JQUERY_DATEPICKER_I18N_CODE . '.js'); ?>"></script>
-<script type="text/javascript">
+<script>
 $.datepicker.setDefaults($.datepicker.regional['<?php echo JQUERY_DATEPICKER_I18N_CODE; ?>']);
 </script>
+
 <?php
   }
 ?>
+
 <script src="<?php echo osc_catalog_href_link('ext/flot/jquery.flot.js'); ?>"></script>
+
 </head>
+
 <body>
 
 <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
