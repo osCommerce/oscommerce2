41dfcd09600f109f411bfdca115e9f1df1c80449
GBurton <oscshops@gmail.com>
2013-05-31 09:29:11 +0100

    Twitter Cards for Products
    Adds Twitter Card Meta Tags to all products, note that main product
    image (thumbnail) must be at least 160px by 160px.

---
+++ catalog/includes/languages/english/modules/header_tags/ht_twitter_product_card.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_TITLE', 'Twitter Product Card');
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_DESCRIPTION', 'Add Twitter Product Card tags to your product information pages.');
+
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_TEXT_PRE_ORDER', 'Pre-Order');
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_TEXT_IN_STOCK', 'In Stock');
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_TEXT_OUT_OF_STOCK', 'Out of Stock');
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_TEXT_BUY_NOW', 'BUY NOW');
+  define('MODULE_HEADER_TAGS_TWITTER_PRODUCT_CARD_TEXT_CONTACT_US', 'CONTACT US');
+?>
