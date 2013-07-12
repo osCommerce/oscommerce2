08c6d9beae8cc29c2e25895c20d303c150b8e659
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-07-12 17:56:29 +0200

    Add new partner news dashboard module

---
+++ catalog/admin/includes/languages/english/modules/dashboard/d_partner_news.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_ADMIN_DASHBOARD_PARTNER_NEWS_TITLE', 'Partner News');
+define('MODULE_ADMIN_DASHBOARD_PARTNER_NEWS_DESCRIPTION', 'Show the lastest osCommerce Partner News');
+?>
