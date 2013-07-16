45d22745e047166bef96a2a1b1d2410a449a61f1
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-07-16 21:06:05 +0200

    Also check for , (comma) and - (minus) in the session ID (bug #601)

--- catalog/admin/includes/functions/sessions.php
+++ catalog/admin/includes/functions/sessions.php
@@ -74,19 +74,19 @@
     $sane_session_id = true;
 
     if (isset($HTTP_GET_VARS[tep_session_name()])) {
-      if (preg_match('/^[a-zA-Z0-9]+$/', $HTTP_GET_VARS[tep_session_name()]) == false) {
+      if (preg_match('/^[a-zA-Z0-9,-]+$/', $HTTP_GET_VARS[tep_session_name()]) == false) {
         unset($HTTP_GET_VARS[tep_session_name()]);
 
         $sane_session_id = false;
       }
     } elseif (isset($HTTP_POST_VARS[tep_session_name()])) {
-      if (preg_match('/^[a-zA-Z0-9]+$/', $HTTP_POST_VARS[tep_session_name()]) == false) {
+      if (preg_match('/^[a-zA-Z0-9,-]+$/', $HTTP_POST_VARS[tep_session_name()]) == false) {
         unset($HTTP_POST_VARS[tep_session_name()]);
 
         $sane_session_id = false;
       }
     } elseif (isset($HTTP_COOKIE_VARS[tep_session_name()])) {
-      if (preg_match('/^[a-zA-Z0-9]+$/', $HTTP_COOKIE_VARS[tep_session_name()]) == false) {
+      if (preg_match('/^[a-zA-Z0-9,-]+$/', $HTTP_COOKIE_VARS[tep_session_name()]) == false) {
         $session_data = session_get_cookie_params();
 
         setcookie(tep_session_name(), '', time()-42000, $session_data['path'], $session_data['domain']);
