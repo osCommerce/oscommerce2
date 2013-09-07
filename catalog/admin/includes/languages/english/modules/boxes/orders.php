bc885863bf2ae4b1ee0b2e16c476d9c4697c30bf
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-09-07 15:13:47 +0200

    Admin Menu: * Add language files for boxes (currently only for new boxes) * Add Orders box and remove the orders link from the customers box

---
+++ catalog/admin/includes/languages/english/modules/boxes/orders.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULES_ADMIN_MENU_ORDERS_HEADING', 'Orders');
+  define('MODULES_ADMIN_MENU_ORDERS_ORDERS', 'Orders');
+?>
