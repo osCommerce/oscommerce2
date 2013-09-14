8c57b6a9fe0eec2a452ba6f3f3dc719922980198
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-09-14 16:59:47 +0200

    new version check extended security check module that warns if a new version check was not performed in 30 days

---
+++ catalog/admin/includes/languages/english/modules/security_check/extended/version_check.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SECURITY_CHECK_EXTENDED_VERSION_CHECK_TITLE', 'Version Check');
+define('MODULE_SECURITY_CHECK_EXTENDED_VERSION_CHECK_ERROR', 'A new version check was performed more than 30 days ago. Please perform the check to see if a new version is available.');
+?>
