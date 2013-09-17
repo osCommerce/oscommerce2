8cfcd271e83ab54b8c2836674c40c7a2b496cdac
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-09-17 03:00:43 +0200

    Update copyright year

--- catalog/includes/modules/header_tags/ht_canonical.php
+++ catalog/includes/modules/header_tags/ht_canonical.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2012 osCommerce
+  Copyright (c) 2013 osCommerce
 
   Released under the GNU General Public License
 */
