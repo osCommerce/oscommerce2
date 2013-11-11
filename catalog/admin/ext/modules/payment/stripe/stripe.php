9ddedc7b1c0b0fa9678d88e4156a1da130544599
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-11-11 11:57:51 +0100

    Add new Stripe payment module

---
+++ catalog/admin/ext/modules/payment/stripe/stripe.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  chdir('../../../../');
+  require('includes/application_top.php');
+  require('../includes/languages/' . $language . '/modules/payment/stripe.php');
+  require('../includes/modules/payment/stripe.php');
+
+  if (defined('MODULE_PAYMENT_STRIPE_STATUS')) {
+    $stripe = new stripe();
+
+    $params = array();
+
+    $stripe_result = json_decode($stripe->sendTransactionToGateway('https://api.stripe.com/v1/charges', $params), true);
+
+    if ( is_array($stripe_result) && !empty($stripe_result) && isset($stripe_result['error']) ) {
+      echo '<h1 id="sctresult">' . MODULE_PAYMENT_STRIPE_DIALOG_CONNECTION_SUCCESS_TITLE . '</h1>' .
+           '<p>' . MODULE_PAYMENT_STRIPE_DIALOG_CONNECTION_SUCCESS_TEXT . '</p>';
+    } else {
+      echo '<h1 id="sctresult">' . MODULE_PAYMENT_STRIPE_DIALOG_CONNECTION_ERROR_TITLE . '</h1>' .
+           '<p>' . MODULE_PAYMENT_STRIPE_DIALOG_CONNECTION_ERROR_TEXT . '</p>';
+    }
+  } else {
+    echo '<h1 id="sctresult">' . MODULE_PAYMENT_STRIPE_DIALOG_CONNECTION_TITLE_ERROR . '</h1>' .
+         '<p>' . MODULE_PAYMENT_STRIPE_DIALOG_CONNECTION_NOT_INSTALLED . '</p>';
+  }
+?>
