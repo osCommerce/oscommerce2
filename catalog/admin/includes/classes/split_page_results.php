9d40afefc671f40290e2efe17e4b86f2be2836b1
omarkhcool <omarkh@yahoo.com>
2014-02-14 15:33:06 -0800

    Update split_page_results.php

--- catalog/admin/includes/classes/split_page_results.php
+++ catalog/admin/includes/classes/split_page_results.php
@@ -34,8 +34,12 @@
       if ($current_page_number > $num_pages) {
         $current_page_number = $num_pages;
       }
-      $offset = ($max_rows_per_page * ($current_page_number - 1));
-      $sql_query .= " limit " . max($offset, 0) . ", " . $max_rows_per_page;
+    $offset = ($max_rows_per_page * ($current_page_number - 1));if ($offset < 0)      
+        {
+        $offset = 0;
+        }
+        $sql_query .= " limit " . $offset . ", " . $max_rows_per_page;
+        
     }
 
     function display_links($query_numrows, $max_rows_per_page, $max_page_links, $current_page_number, $parameters = '', $page_name = 'page') {
