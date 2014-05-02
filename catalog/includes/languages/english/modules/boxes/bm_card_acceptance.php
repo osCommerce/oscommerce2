5a77d077fd1c4c6d61b9398553ed98a69b3bcedf
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-05-02 18:45:38 +0200

    Introduce new Card Acceptance box

---
+++ catalog/includes/languages/english/modules/boxes/bm_card_acceptance.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_CARD_ACCEPTANCE_TITLE', 'Card Acceptance');
+  define('MODULE_BOXES_CARD_ACCEPTANCE_DESCRIPTION', 'Show payment card acceptance logos');
+
+  define('MODULE_BOXES_CARD_ACCEPTANCE_SHOWN_CARDS', 'Shown Cards');
+  define('MODULE_BOXES_CARD_ACCEPTANCE_NEW_CARDS', 'New Cards');
+  define('MODULE_BOXES_CARD_ACCEPTANCE_DRAG_HERE', 'Drag Here');
+
+  define('MODULE_BOXES_CARD_ACCEPTANCE_BOX_TITLE', 'We Accept');
+?>
