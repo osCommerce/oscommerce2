92e5078e4b297276f8252af17fad2edab5a2656e
gburton <oscshops@gmail.com>
2014-05-28 10:15:56 +0100

    Dashboard: Latest News Link

--- catalog/admin/includes/languages/english/modules/dashboard/d_latest_news.php
+++ catalog/admin/includes/languages/english/modules/dashboard/d_latest_news.php
@@ -10,12 +10,14 @@
   Released under the GNU General Public License
 */
 
-define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_TITLE', 'Lastest News');
-define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_DESCRIPTION', 'Show the lastest osCommerce News');
+define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_TITLE', 'Latest News');
+define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_DESCRIPTION', 'Show the latest osCommerce News');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_DATE', 'Date');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_FEED_ERROR', 'Could not connect to the osCommerce News feed. The next attempt will be performed within 24 hours.');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_NEWSLETTER', 'Sign-Up for the osCommerce Newsletter');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_FACEBOOK', 'Become an osCommerce Fan on Facebook');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_TWITTER', 'Follow osCommerce on Twitter');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_RSS', 'Subscribe to the osCommerce News RSS Feed');
-?>
+
+define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_NEWS', 'Read the latest osCommerce News');
+
