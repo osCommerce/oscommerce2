775cb62a08638a8bb1ccb4b82bdfdbacdb49c6cf
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-06-04 02:30:56 +0200

    add documentation link

--- catalog/includes/languages/english/modules/payment/rbsworldpay_hosted.php
+++ catalog/includes/languages/english/modules/payment/rbsworldpay_hosted.php
@@ -12,7 +12,7 @@
 
   define('MODULE_PAYMENT_RBSWORLDPAY_HOSTED_TEXT_TITLE', 'WorldPay Hosted Payment Pages');
   define('MODULE_PAYMENT_RBSWORLDPAY_HOSTED_TEXT_PUBLIC_TITLE', 'Credit Card');
-  define('MODULE_PAYMENT_RBSWORLDPAY_HOSTED_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="http://www.worldpay.com" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit WorldPay Website</a>');
+  define('MODULE_PAYMENT_RBSWORLDPAY_HOSTED_TEXT_DESCRIPTION', '<img src="images/icon_info.gif" border="0" />&nbsp;<a href="http://library.oscommerce.com/Package&en&worldpay&oscom23&hosted" target="_blank" style="text-decoration: underline; font-weight: bold;">View Online Documentation</a><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="http://www.worldpay.com" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit WorldPay Website</a>');
 
   define('MODULE_PAYMENT_RBSWORLDPAY_HOSTED_ERROR_ADMIN_CONFIGURATION', 'This module will not load until the Installation ID parameter has been configured. Please edit and configure the settings of this module.');
 
