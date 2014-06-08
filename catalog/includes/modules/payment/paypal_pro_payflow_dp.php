079c81489514d94f728477ce3be4dd955c3dcdbd
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-06-08 18:52:27 +0200

    Inject jQuery if it isn't available from the template (v2.2 compatibility)

--- catalog/includes/modules/payment/paypal_pro_payflow_dp.php
+++ catalog/includes/modules/payment/paypal_pro_payflow_dp.php
@@ -16,7 +16,7 @@
     function paypal_pro_payflow_dp() {
       global $HTTP_GET_VARS, $PHP_SELF, $order;
 
-      $this->signature = 'paypal|paypal_pro_payflow_dp|3.0|2.3';
+      $this->signature = 'paypal|paypal_pro_payflow_dp|3.1|2.3';
 
       $this->code = 'paypal_pro_payflow_dp';
       $this->title = MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_DP_TEXT_TITLE;
@@ -61,7 +61,7 @@
         }
       }
 
-      if ( defined('FILENAME_MODULES') && ($PHP_SELF == FILENAME_MODULES) && isset($HTTP_GET_VARS['action']) && ($HTTP_GET_VARS['action'] == 'install') && isset($HTTP_GET_VARS['subaction']) && ($HTTP_GET_VARS['subaction'] == 'conntest') ) {
+      if ( defined('FILENAME_MODULES') && (basename($PHP_SELF) == FILENAME_MODULES) && isset($HTTP_GET_VARS['action']) && ($HTTP_GET_VARS['action'] == 'install') && isset($HTTP_GET_VARS['subaction']) && ($HTTP_GET_VARS['subaction'] == 'conntest') ) {
         echo $this->getTestConnectionResult();
         exit;
       }
@@ -555,7 +555,15 @@
       $test_url = tep_href_link(FILENAME_MODULES, 'set=payment&module=' . $this->code . '&action=install&subaction=conntest');
 
       $js = <<<EOD
-<script type="text/javascript">
+<script>
+if ( typeof jQuery == 'undefined' ) {
+  document.write('<scr' + 'ipt src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></scr' + 'ipt>');
+  document.write('<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/redmond/jquery-ui.css" />');
+  document.write('<scr' + 'ipt src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></scr' + 'ipt>');
+}
+</script>
+
+<script>
 $(function() {
   $('#tcdprogressbar').progressbar({
     value: false
@@ -660,7 +668,13 @@ EOD;
       }
 
       $js = <<<EOD
-<script type="text/javascript">
+<script>
+if ( typeof jQuery == 'undefined' ) {
+  document.write('<scr' + 'ipt src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></scr' + 'ipt>');
+}
+</script>
+
+<script>
 $(function() {
   if ( typeof($('#paypal_table_new_card').parent().closest('table').attr('width')) == 'undefined' ) {
     $('#paypal_table_new_card').parent().closest('table').attr('width', '100%');
