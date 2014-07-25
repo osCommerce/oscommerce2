0586c8be69481fb8cd3db05bd72079d0bf614415
gburton <oscshops@gmail.com>
2014-07-25 11:25:58 +0100

    Bootstrap "product" modules

--- catalog/includes/modules/upcoming_products.php
+++ catalog/includes/modules/upcoming_products.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2010 osCommerce
+  Copyright (c) 2014 osCommerce
 
   Released under the GNU General Public License
 */
@@ -13,28 +13,28 @@
   $expected_query = tep_db_query("select p.products_id, pd.products_name, products_date_available as date_expected from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where to_days(products_date_available) >= to_days(now()) and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' order by " . EXPECTED_PRODUCTS_FIELD . " " . EXPECTED_PRODUCTS_SORT . " limit " . MAX_DISPLAY_UPCOMING_PRODUCTS);
   if (tep_db_num_rows($expected_query) > 0) {
 ?>
-
-  <div class="ui-widget infoBoxContainer">
-    <div class="ui-widget-header ui-corner-top infoBoxHeading">
-      <span><?php echo TABLE_HEADING_UPCOMING_PRODUCTS; ?></span>
-      <span style="float: right;"><?php echo TABLE_HEADING_DATE_EXPECTED; ?></span>
-    </div>
-
-    <div class="ui-widget-content ui-corner-bottom">
-      <table border="0" width="100%" cellspacing="0" cellpadding="2" class="productListTable">
-<?php
-    while ($expected = tep_db_fetch_array($expected_query)) {
-      echo '        <tr>' . "\n" .
-           '          <td><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $expected['products_id']) . '">' . $expected['products_name'] . '</a></td>' . "\n" .
-           '          <td align="right">' . tep_date_short($expected['date_expected']) . '</td>' . "\n" .
-           '        </tr>' . "\n";
-    }
-?>
-
-      </table>
-    </div>
+  <div class="clearfix"></div>
+  <div class="table-responsive">
+    <table class="table table-condensed">
+      <thead>
+        <tr>
+          <th><?php echo TABLE_HEADING_UPCOMING_PRODUCTS; ?></th>
+          <th class="text-right"><?php echo TABLE_HEADING_DATE_EXPECTED; ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        while ($expected = tep_db_fetch_array($expected_query)) {
+          echo '        <tr>' . "\n" .
+               '          <td><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $expected['products_id']) . '">' . $expected['products_name'] . '</a></td>' . "\n" .
+               '          <td class="text-right">' . tep_date_short($expected['date_expected']) . '</td>' . "\n" .
+               '        </tr>' . "\n";
+        }
+        ?>
+      </tbody>
+    </table>
   </div>
-
+  
 <?php
   }
 ?>
