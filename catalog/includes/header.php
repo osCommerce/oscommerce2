a1cffb5d8c2566f60e30c69ec865a9f80d2f0951
gburton <oscshops@gmail.com>
2014-08-02 11:28:18 +0100

    Clean Up Header

--- catalog/includes/header.php
+++ catalog/includes/header.php
@@ -21,11 +21,11 @@
     <div class="btn-group">
 <?php
   echo tep_draw_button(HEADER_TITLE_CART_CONTENTS . ($cart->count_contents() > 0 ? ' (' . $cart->count_contents() . ')' : ''), 'glyphicon glyphicon-shopping-cart', tep_href_link(FILENAME_SHOPPING_CART)) .
-       tep_draw_button(HEADER_TITLE_CHECKOUT, 'glyphicon glyphicon-chevron-right', tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL')) .
+       tep_draw_button(HEADER_TITLE_CHECKOUT, 'glyphicon glyphicon-credit-card', tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL')) .
        tep_draw_button(HEADER_TITLE_MY_ACCOUNT, 'glyphicon glyphicon-user', tep_href_link(FILENAME_ACCOUNT, '', 'SSL'));
 
   if (tep_session_is_registered('customer_id')) {
-    echo tep_draw_button(HEADER_TITLE_LOGOFF, null, tep_href_link(FILENAME_LOGOFF, '', 'SSL'));
+    echo tep_draw_button(HEADER_TITLE_LOGOFF, 'glyphicon glyphicon-log-out', tep_href_link(FILENAME_LOGOFF, '', 'SSL'));
   }
 ?>
     </div>
@@ -37,21 +37,25 @@
 <?php
   if (isset($HTTP_GET_VARS['error_message']) && tep_not_null($HTTP_GET_VARS['error_message'])) {
 ?>
-<table border="0" width="100%" cellspacing="0" cellpadding="2">
-  <tr class="headerError">
-    <td class="headerError"><?php echo htmlspecialchars(stripslashes(urldecode($HTTP_GET_VARS['error_message']))); ?></td>
-  </tr>
-</table>
+<div class="clearfix"></div>
+<div class="col-xs-12">
+  <div class="alert alert-danger">
+    <a href="#" class="close glyphicon glyphicon-remove" data-dismiss="alert"></a>
+    <?php echo htmlspecialchars(stripslashes(urldecode($HTTP_GET_VARS['error_message']))); ?>
+  </div>
+</div>
 <?php
   }
 
   if (isset($HTTP_GET_VARS['info_message']) && tep_not_null($HTTP_GET_VARS['info_message'])) {
 ?>
-<table border="0" width="100%" cellspacing="0" cellpadding="2">
-  <tr class="headerInfo">
-    <td class="headerInfo"><?php echo htmlspecialchars(stripslashes(urldecode($HTTP_GET_VARS['info_message']))); ?></td>
-  </tr>
-</table>
+<div class="clearfix"></div>
+<div class="col-xs-12">
+  <div class="alert alert-info">
+    <a href="#" class="close glyphicon glyphicon-remove" data-dismiss="alert"></a>
+    <?php echo htmlspecialchars(stripslashes(urldecode($HTTP_GET_VARS['info_message']))); ?>
+  </div>
+</div>
 <?php
   }
 ?>
