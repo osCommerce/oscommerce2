7849b8dbcfe1361cd7e540b08db605e2cd736758
acidvertigo <ldanzelmo@gmail.com>
2014-08-08 14:13:06 +0200

    Change while loop with foreach

--- catalog/includes/modules/boxes/bm_languages.php
+++ catalog/includes/modules/boxes/bm_languages.php
@@ -41,8 +41,7 @@
 
         if (count($lng->catalog_languages) > 1) {
           $languages_string = '';
-          reset($lng->catalog_languages);
-          while (list($key, $value) = each($lng->catalog_languages)) {
+          foreach($lng->catalog_languages as $key => $value) {
             $languages_string .= ' <a href="' . tep_href_link($PHP_SELF, tep_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . tep_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a> ';
           }
 
