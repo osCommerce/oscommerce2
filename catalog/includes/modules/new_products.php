3450b64e86e8590e3833cb596c363e1c0aacf864
acidvertigo <ldanzelmo@gmail.com>
2014-08-13 15:56:04 +0200

    fix Typo

--- catalog/includes/modules/new_products.php
+++ catalog/includes/modules/new_products.php
@@ -31,7 +31,7 @@
     <div class="col-sm-6 col-md-4">
       <div class="thumbnail">
         <a href="<?php echo tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $new_products['products_image'], $new_products['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT); ?></a>
-        <div class="caption">';
+        <div class="caption">
           <p class="text-center"><a href="<?php echo tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . $new_products['products_name']; ?></a></p>
           <p class="text-center"><?php echo $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id'])); ?></p>
         </div>
