dbd3365a94af5ad0c338ae34c46dd7444098e52d
acidvertigo <ldanzelmo@gmail.com>
2014-08-13 16:40:56 +0200

    Separate html tags from php

--- catalog/includes/modules/product_listing.php
+++ catalog/includes/modules/product_listing.php
@@ -29,13 +29,14 @@
 
 <?php
   }
-
-  $prod_list_contents = '<div class="">' .
-                        '  <div class="infoBoxHeading">' .
-                        '    <table border="0" width="100%" cellspacing="0" cellpadding="2" class="table">' .
-                        '      <tr>';
-
-  for ($col=0, $n=sizeof($column_list); $col<$n; $col++) {
+  $prod_list_contents = '';
+?>
+  <div class="ui-widget infoBoxContainer">
+    <div class="ui-widget-header ui-corner-top infoBoxHeading">
+      <table border="0" width="100%" cellspacing="0" cellpadding="2" class="productListingHeader">
+        <tr>
+<?php
+  foreach (array_keys($column_list) as $col) {
     $lc_align = '';
 
     switch ($column_list[$col]) {
@@ -76,75 +77,74 @@
     if ( ($column_list[$col] != 'PRODUCT_LIST_BUY_NOW') && ($column_list[$col] != 'PRODUCT_LIST_IMAGE') ) {
       $lc_text = tep_create_sort_heading($_GET['sort'], $col+1, $lc_text);
     }
-
-    $prod_list_contents .= '        <td' . (tep_not_null($lc_align) ? ' align="' . $lc_align . '"' : '') . '>' . $lc_text . '</td>';
-  }
-
-  $prod_list_contents .= '      </tr>' .
-                         '    </table>' .
-                         '  </div>';
-
+?>
+          <td<?php echo (tep_not_null($lc_align) ? ' align="' . $lc_align . '"' : '') . '>' . $lc_text; ?></td>
+<?php } ?>
+        </tr>
+      </table>
+    </div>
+<?php
   if ($listing_split->number_of_rows > 0) {
     $rows = 0;
     $listing_query = tep_db_query($listing_split->sql_query);
-
-    $prod_list_contents .= '  <div class="productListTable">' .
-                           '    <table border="0" width="100%" cellspacing="2" cellpadding="2" class="table">';
-
+?>
+      <div class="ui-widget-content ui-corner-bottom productListTable">
+        <table border="0" width="100%" cellspacing="0" cellpadding="2" class="productListingData">
+<?php
     while ($listing = tep_db_fetch_array($listing_query)) {
       $rows++;
-
-      $prod_list_contents .= '      <tr>';
-
-      for ($col=0, $n=sizeof($column_list); $col<$n; $col++) {
+?>
+            <tr>
+<?php
+      foreach (array_keys($column_list) as $col) {
         switch ($column_list[$col]) {
-          case 'PRODUCT_LIST_MODEL':
-            $prod_list_contents .= '        <td>' . $listing['products_model'] . '</td>';
-            break;
+          case 'PRODUCT_LIST_MODEL': ?>
+            <td><?php echo $listing['products_model']; ?></td>
+          <?php  break;  
           case 'PRODUCT_LIST_NAME':
-            if (isset($_GET['manufacturers_id']) && tep_not_null($_GET['manufacturers_id'])) {
-              $prod_list_contents .= '        <td><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing['products_id']) . '">' . $listing['products_name'] . '</a></td>';
-            } else {
-              $prod_list_contents .= '        <td><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, ($cPath ? 'cPath=' . $cPath . '&' : '') . 'products_id=' . $listing['products_id']) . '">' . $listing['products_name'] . '</a></td>';
-            }
+            if (isset($_GET['manufacturers_id']) && tep_not_null($_GET['manufacturers_id'])) { ?>
+                      <td><a href="<?php echo tep_href_link(FILENAME_PRODUCT_INFO, 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing['products_id']) . '">' . $listing['products_name']; ?></a></td>
+            <?php } else { ?>
+                      <td><a href="<?php echo tep_href_link(FILENAME_PRODUCT_INFO, ($cPath ? 'cPath=' . $cPath . '&' : '') . 'products_id=' . $listing['products_id']) . '">' . $listing['products_name']; ?></a></td>
+            <?php }
             break;
-          case 'PRODUCT_LIST_MANUFACTURER':
-            $prod_list_contents .= '        <td><a href="' . tep_href_link(FILENAME_DEFAULT, 'manufacturers_id=' . $listing['manufacturers_id']) . '">' . $listing['manufacturers_name'] . '</a></td>';
+          case 'PRODUCT_LIST_MANUFACTURER': ?>
+                    <td><a href="<?php echo tep_href_link(FILENAME_DEFAULT, 'manufacturers_id=' . $listing['manufacturers_id']) . '">' . $listing['manufacturers_name']; ?></a></td>
+            <?php break;
+          case 'PRODUCT_LIST_PRICE': 
+            if (tep_not_null($listing['specials_new_products_price'])) { ?>
+                      <td align="right"><del><?php echo  $currencies->display_price($listing['products_price'], tep_get_tax_rate($listing['products_tax_class_id'])) . '</del>&nbsp;&nbsp;<span class="productSpecialPrice">' . $currencies->display_price($listing['specials_new_products_price'], tep_get_tax_rate($listing['products_tax_class_id'])); ?></span></td>
+            <?php } else { ?>
+                      <td align="right"><?php echo $currencies->display_price($listing['products_price'], tep_get_tax_rate($listing['products_tax_class_id'])); ?></td>
+            <?php } 
             break;
-          case 'PRODUCT_LIST_PRICE':
-            if (tep_not_null($listing['specials_new_products_price'])) {
-              $prod_list_contents .= '        <td align="right"><del>' .  $currencies->display_price($listing['products_price'], tep_get_tax_rate($listing['products_tax_class_id'])) . '</del>&nbsp;&nbsp;<span class="productSpecialPrice">' . $currencies->display_price($listing['specials_new_products_price'], tep_get_tax_rate($listing['products_tax_class_id'])) . '</span></td>';
-            } else {
-              $prod_list_contents .= '        <td align="right">' . $currencies->display_price($listing['products_price'], tep_get_tax_rate($listing['products_tax_class_id'])) . '</td>';
-            }
+          case 'PRODUCT_LIST_QUANTITY': ?>
+                    <td align="right"><?php echo $listing['products_quantity']; ?></td>
+          <?php  break;
+          case 'PRODUCT_LIST_WEIGHT': ?>
+                    <td align="right"><?php echo $listing['products_weight']; ?></td>
+          <?php  break; 
+          case 'PRODUCT_LIST_IMAGE': 
+            if (isset($_GET['manufacturers_id'])  && tep_not_null($_GET['manufacturers_id'])) { ?>
+                      <td align="center"><a href="<?php echo tep_href_link(FILENAME_PRODUCT_INFO, 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $listing['products_image'], $listing['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT); ?></a></td>
+            <?php } else { ?>
+                      <td align="center"><a href="<?php echo tep_href_link(FILENAME_PRODUCT_INFO, ($cPath ? 'cPath=' . $cPath . '&' : '') . 'products_id=' . $listing['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $listing['products_image'], $listing['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT); ?></a></td>
+            <?php } 
             break;
-          case 'PRODUCT_LIST_QUANTITY':
-            $prod_list_contents .= '        <td align="right">' . $listing['products_quantity'] . '</td>';
-            break;
-          case 'PRODUCT_LIST_WEIGHT':
-            $prod_list_contents .= '        <td align="right">' . $listing['products_weight'] . '</td>';
-            break;
-          case 'PRODUCT_LIST_IMAGE':
-            if (isset($_GET['manufacturers_id'])  && tep_not_null($_GET['manufacturers_id'])) {
-              $prod_list_contents .= '        <td align="center"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $listing['products_image'], $listing['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a></td>';
-            } else {
-              $prod_list_contents .= '        <td align="center"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, ($cPath ? 'cPath=' . $cPath . '&' : '') . 'products_id=' . $listing['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $listing['products_image'], $listing['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a></td>';
-            }
-            break;
-          case 'PRODUCT_LIST_BUY_NOW':
-            $prod_list_contents .= '        <td align="center">' . tep_draw_button(IMAGE_BUTTON_BUY_NOW, 'glyphicon glyphicon-shopping-cart', tep_href_link($PHP_SELF, tep_get_all_get_params(array('action')) . 'action=buy_now&products_id=' . $listing['products_id'])) . '</td>';
-            break;
+          case 'PRODUCT_LIST_BUY_NOW': ?>
+                    <td align="center"><?php echo tep_draw_button(IMAGE_BUTTON_BUY_NOW, 'glyphicon glyphicon-shopping-cart', tep_href_link($PHP_SELF, tep_get_all_get_params(array('action')) . 'action=buy_now&products_id=' . $listing['products_id'])); ?></td>
+            <?php break;
         }
       }
+?>
+           </tr>
+ <?php   } ?>
 
-      $prod_list_contents .= '      </tr>';
-    }
+        </table>
+      </div>
+    </div>
 
-    $prod_list_contents .= '    </table>' .
-                           '  </div>' .
-                           '</div>';
-
-    echo $prod_list_contents;
+<?php  
   } else {
 ?>
 
