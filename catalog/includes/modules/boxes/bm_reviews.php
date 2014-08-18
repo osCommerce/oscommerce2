069ee2b473e63277957be6db38edbe7159cb9d58
acidvertigo <ldanzelmo@gmail.com>
2014-08-18 12:16:30 +0200

    even better with bootstrap the text is wrapped correctly

--- catalog/includes/modules/boxes/bm_reviews.php
+++ catalog/includes/modules/boxes/bm_reviews.php
@@ -47,7 +47,7 @@
         $rand_review_query = tep_db_query("select substring(reviews_text, 1, 60) as reviews_text from " . TABLE_REVIEWS_DESCRIPTION . " where reviews_id = '" . (int)$random_product['reviews_id'] . "' and languages_id = '" . (int)$_SESSION['languages_id'] . "'");
         $rand_review = tep_db_fetch_array($rand_review_query);
 
-        $rand_review_text = wordwrap(tep_output_string_protected($rand_review['reviews_text']), 25, '<br />');
+        $rand_review_text = tep_output_string_protected($rand_review['reviews_text']);
 
         $reviews_box_contents .= '<div class="text-center"><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS, 'products_id=' . $random_product['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $random_product['products_image'], $random_product['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a></div><div><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS, 'products_id=' . $random_product['products_id']) . '">' . $rand_review_text . '</a>...</div><div class="text-center" title="' .  sprintf(MODULE_BOXES_REVIEWS_BOX_TEXT_OF_5_STARS, $random_product['reviews_rating']) . '">' . tep_draw_stars((int)$random_product['reviews_rating']) . '</div>';
       } elseif (isset($_GET['products_id'])) {
