211e0661d29e093eeda7c4d2c744be2612f36de6
acidvertigo <ldanzelmo@gmail.com>
2014-08-24 13:56:48 +0200

    Change for loop to foreach

--- catalog/includes/functions/database.php
+++ catalog/includes/functions/database.php
@@ -53,15 +53,14 @@
   }
 
   function tep_db_perform($table, $data, $action = 'insert', $parameters = '', $link = 'db_link') {
-    reset($data);
+
     if ($action == 'insert') {
       $query = 'insert into ' . $table . ' (';
-      while (list($columns, ) = each($data)) {
+      foreach (array_keys($data) as $columns ) {
         $query .= $columns . ', ';
       }
       $query = substr($query, 0, -2) . ') values (';
-      reset($data);
-      while (list(, $value) = each($data)) {
+      foreach ($data as $value) {
         switch ((string)$value) {
           case 'now()':
             $query .= 'now(), ';
@@ -77,7 +76,7 @@
       $query = substr($query, 0, -2) . ')';
     } elseif ($action == 'update') {
       $query = 'update ' . $table . ' set ';
-      while (list($columns, $value) = each($data)) {
+      foreach ($data as $columns => $value) {
         switch ((string)$value) {
           case 'now()':
             $query .= $columns . ' = now(), ';
@@ -136,8 +135,7 @@
     if (is_string($string)) {
       return trim(tep_sanitize_string(stripslashes($string)));
     } elseif (is_array($string)) {
-      reset($string);
-      while (list($key, $value) = each($string)) {
+      foreach ($string as $key => $value) {
         $string[$key] = tep_db_prepare_input($value);
       }
       return $string;
