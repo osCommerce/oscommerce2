9be7d00d3a4b3382d3071691b696bd4d21674636
acidvertigo <ldanzelmo@gmail.com>
2014-08-25 15:01:41 +0200

    Simplify count of products and subcategories using the dedicated functions in general.php

--- catalog/index.php
+++ catalog/index.php
@@ -14,19 +14,17 @@
 
 // the following cPath references come from application_top.php
   $category_depth = 'top';
+
   if (isset($cPath) && tep_not_null($cPath)) {
-    $categories_products_query = tep_db_query("select count(*) as total from " . TABLE_PRODUCTS_TO_CATEGORIES . " where categories_id = '" . (int)$current_category_id . "'");
-    $categories_products = tep_db_fetch_array($categories_products_query);
-    if ($categories_products['total'] > 0) {
+  
+    if (tep_count_products_in_category($current_category_id, false) > 0) {
       $category_depth = 'products'; // display products
-    } else {
-      $category_parent_query = tep_db_query("select count(*) as total from " . TABLE_CATEGORIES . " where parent_id = '" . (int)$current_category_id . "'");
-      $category_parent = tep_db_fetch_array($category_parent_query);
-      if ($category_parent['total'] > 0) {
+    } 
+
+    if (tep_has_category_subcategories($current_category_id) == true) {
         $category_depth = 'nested'; // navigate through the categories
       } else {
         $category_depth = 'products'; // category has no products, but display the 'no products' message
-      }
     }
   }
 
