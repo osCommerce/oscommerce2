6ff80735dace89f7d7c3e62624b2f5e5320f7317
acidvertigo <ldanzelmo@gmail.com>
2014-08-28 16:48:40 +0200

    Fix typo

--- catalog/product_info.php
+++ catalog/product_info.php
@@ -184,7 +184,7 @@ $(function() {
         
   while ($products_options = tep_db_fetch_array($products_options_query)) {
   $products_options_array[$products_options['options_id']][] = array('id' => $products_options['products_options_values_id'], 
-                                                                   'text' => $products_options['products_options_values_name'] . ' ' . $products_options['price_prefix'] . $currencies->display_price($products_options['options_values_price'], tep_get_tax_rate($product->getProducts_tax_class_id())));
+                                                                   'text' => $products_options['products_options_values_name'] . ' ' . $products_options['price_prefix'] . $currencies->display_price($products_options['options_values_price'], tep_get_tax_rate$product_info['products_tax_class_id'])));
  }
          
   if (is_string($_GET['products_id']) && isset($_SESSION['cart']->contents[$_GET['products_id']]['attributes'][$products_options])) {
