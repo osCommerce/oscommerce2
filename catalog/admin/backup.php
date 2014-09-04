91fa1b8f0394b45e3561c1875a1014e15548e037
acidvertigo <ldanzelmo@gmail.com>
2014-09-04 18:35:55 +0200

    Change while loop to foreach

--- catalog/admin/backup.php
+++ catalog/admin/backup.php
@@ -80,7 +80,7 @@
             $index[$kname]['columns'][] = $keys['Column_name'];
           }
 
-          while (list($kname, $info) = each($index)) {
+          foreach ( $index as $kname => $info ) {
             $schema .= ',' . "\n";
 
             $columns = implode($info['columns'], ', ');
@@ -105,8 +105,7 @@
             while ($rows = tep_db_fetch_array($rows_query)) {
               $schema = 'insert into ' . $table . ' (' . implode(', ', $table_list) . ') values (';
 
-              reset($table_list);
-              while (list(,$i) = each($table_list)) {
+              foreach ( $table_list as $i ) {
                 if (!isset($rows[$i])) {
                   $schema .= 'NULL, ';
                 } elseif (tep_not_null($rows[$i])) {
