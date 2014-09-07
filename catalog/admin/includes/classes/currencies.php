101f7b3ccd87376820e57d70b3c3a9b40acc9737
acidvertigo <ldanzelmo@gmail.com>
2014-09-07 22:15:41 +0200

    Remove obsolete euro currency conversion
    As it is no more needed in 2014.

--- catalog/admin/includes/classes/currencies.php
+++ catalog/admin/includes/classes/currencies.php
@@ -36,11 +36,6 @@
       if ($calculate_currency_value) {
         $rate = ($currency_value) ? $currency_value : $this->currencies[$currency_type]['value'];
         $format_string = $this->currencies[$currency_type]['symbol_left'] . number_format($number * $rate, $this->currencies[$currency_type]['decimal_places'], $this->currencies[$currency_type]['decimal_point'], $this->currencies[$currency_type]['thousands_point']) . $this->currencies[$currency_type]['symbol_right'];
-// if the selected currency is in the european euro-conversion and the default currency is euro,
-// the currency will displayed in the national currency and euro currency
-        if ( (DEFAULT_CURRENCY == 'EUR') && ($currency_type == 'DEM' || $currency_type == 'BEF' || $currency_type == 'LUF' || $currency_type == 'ESP' || $currency_type == 'FRF' || $currency_type == 'IEP' || $currency_type == 'ITL' || $currency_type == 'NLG' || $currency_type == 'ATS' || $currency_type == 'PTE' || $currency_type == 'FIM' || $currency_type == 'GRD') ) {
-          $format_string .= ' <small>[' . $this->format($number, true, 'EUR') . ']</small>';
-        }
       } else {
         $format_string = $this->currencies[$currency_type]['symbol_left'] . number_format($number, $this->currencies[$currency_type]['decimal_places'], $this->currencies[$currency_type]['decimal_point'], $this->currencies[$currency_type]['thousands_point']) . $this->currencies[$currency_type]['symbol_right'];
       }
