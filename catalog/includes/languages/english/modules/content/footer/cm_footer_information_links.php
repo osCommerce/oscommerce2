f9577dd5f6d6787453c1ad7a26d10c7e4e489f0a
gburton <oscshops@gmail.com>
2014-09-22 11:44:58 +0100

    Modular Content for "Footer" Area

---
+++ catalog/includes/languages/english/modules/content/footer/cm_footer_information_links.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_FOOTER_INFORMATION_TITLE', 'Information Links Block');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_DESCRIPTION', 'Adds Information Links Block to the Footer Area of your site');
+
+  define('MODULE_CONTENT_FOOTER_INFORMATION_HEADING_TITLE', 'Information');
+
+  define('MODULE_CONTENT_FOOTER_INFORMATION_SHIPPING', 'Shipping & Returns');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_PRIVACY', 'Privacy & Cookie Policy');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_CONDITIONS', 'Terms & Conditions');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_CONTACT', 'Contact Us');
+
