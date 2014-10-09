6b6223634303a31dfc71bb51ec8fd954e4d4a5e5
Luca <ldanzelmo@gmail.com>
2014-10-09 12:31:48 +0200

    If display_cart == true redirect to the shopping cart page for both "buy_now" and "add to cart" button
    if not, the page will be redirected to the product page for both buttons.
    
    I think that also "buy now" button should redirect to the product cart if display_cart == 'true'  as this seems to me more intuitive for the customer.

--- catalog/includes/application_top.php
+++ catalog/includes/application_top.php
@@ -265,17 +265,12 @@
       tep_redirect(tep_href_link(FILENAME_COOKIE_USAGE));
     }
 
-    if ( DISPLAY_CART == 'true' ) {
+     if ( DISPLAY_CART == 'true' ) {
       $goto =  FILENAME_SHOPPING_CART;
       $parameters = array('action', 'cPath', 'products_id', 'pid');
     } else {
       $goto = $PHP_SELF;
-
-      if ( $_GET['action'] == 'buy_now') {
-        $parameters = array('action', 'pid', 'products_id');
-      } else {
-        $parameters = array('action', 'pid');
-      }
+      $parameters = array('action', 'pid', ( $_GET['action'] == 'buy_now') ? 'products_id' : '');
     }
 
     switch ( $_GET['action'] ) {
