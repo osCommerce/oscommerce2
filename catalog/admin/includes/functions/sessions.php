4b278b59bcab510de08ec785bfcec899c51cacb2
Luca <ldanzelmo@gmail.com>
2014-10-10 14:12:59 +0200

    Remove check for register globals

--- catalog/admin/includes/functions/sessions.php
+++ catalog/admin/includes/functions/sessions.php
@@ -10,11 +10,6 @@
   Released under the GNU General Public License
 */
 
-  if ( (PHP_VERSION >= 4.3) && ((bool)ini_get('register_globals') == false) ) {
-    @ini_set('session.bug_compat_42', 1);
-    @ini_set('session.bug_compat_warn', 0);
-  }
-
   if (STORE_SESSIONS == 'mysql') {
     function _sess_open($save_path, $session_name) {
       return true;
