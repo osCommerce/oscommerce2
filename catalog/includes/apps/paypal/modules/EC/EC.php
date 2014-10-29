b3cc84ee3342a8211c436754765899e876a122da
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-10-29 17:17:23 +0100

    Remove Payflow EC module files; Finalize Payflow EC migration.

--- catalog/includes/apps/paypal/modules/EC/EC.php
+++ catalog/includes/apps/paypal/modules/EC/EC.php
@@ -15,6 +15,7 @@
     var $_short_title;
     var $_introduction;
     var $_pm_code = 'paypal_express';
+    var $_pm_pf_code = 'paypal_pro_payflow_ec';
     var $_sort_order = 100;
 
     function OSCOM_PayPal_EC() {
@@ -52,8 +53,10 @@
     }
 
     function canMigrate() {
-      $class = $this->_pm_code;
+      return $this->doMigrationCheck($this->_pm_code) || $this->doMigrationCheck($this->_pm_pf_code);
+    }
 
+    function doMigrationCheck($class) {
       if ( file_exists(DIR_FS_CATALOG . 'includes/modules/payment/' . $class . '.php') ) {
         if ( !class_exists($class) ) {
           include(DIR_FS_CATALOG . 'includes/modules/payment/' . $class . '.php');
@@ -74,6 +77,8 @@
     }
 
     function migrate($OSCOM_PayPal) {
+      $is_payflow = false;
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_SERVER') ) {
         $server = (MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_SERVER == 'Live') ? 'LIVE' : 'SANDBOX';
 
@@ -106,6 +111,32 @@
         }
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_SERVER') ) {
+        $is_payflow = true;
+
+        $server = (MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_SERVER == 'Live') ? 'LIVE' : 'SANDBOX';
+
+        if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VENDOR') && defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_USERNAME') && defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PASSWORD') && defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PARTNER') ) {
+          if ( tep_not_null(MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VENDOR) && tep_not_null(MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PASSWORD) && tep_not_null(MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PARTNER) ) {
+            if ( !defined('OSCOM_APP_PAYPAL_PF_' . $server . '_VENDOR') || !tep_not_null(constant('OSCOM_APP_PAYPAL_PF_' . $server . '_VENDOR')) ) {
+              if ( !defined('OSCOM_APP_PAYPAL_PF_' . $server . '_PASSWORD') || !tep_not_null(constant('OSCOM_APP_PAYPAL_PF_' . $server . '_PASSWORD')) ) {
+                if ( !defined('OSCOM_APP_PAYPAL_PF_' . $server . '_PARTNER') || !tep_not_null(constant('OSCOM_APP_PAYPAL_PF_' . $server . '_PARTNER')) ) {
+                  $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_PF_' . $server . '_VENDOR', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VENDOR);
+                  $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_PF_' . $server . '_USER', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_USERNAME);
+                  $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_PF_' . $server . '_PASSWORD', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PASSWORD);
+                  $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_PF_' . $server . '_PARTNER', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PARTNER);
+                }
+              }
+            }
+          }
+
+          $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VENDOR');
+          $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_USERNAME');
+          $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PASSWORD');
+          $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PARTNER');
+        }
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_ACCOUNT_OPTIONAL') ) {
         $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_ACCOUNT_OPTIONAL', (MODULE_PAYMENT_PAYPAL_EXPRESS_ACCOUNT_OPTIONAL == 'True') ? '1' : '0');
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_ACCOUNT_OPTIONAL');
@@ -126,9 +157,19 @@
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_PAGE_STYLE');
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PAGE_STYLE') ) {
+        $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_PAGE_STYLE', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PAGE_STYLE);
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PAGE_STYLE');
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_METHOD') ) {
         $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_TRANSACTION_METHOD', (MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_METHOD == 'Sale') ? '1' : '0');
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_METHOD');
+      }
+
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_METHOD') ) {
+        $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_TRANSACTION_METHOD', (MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_METHOD == 'Sale') ? '1' : '0');
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_METHOD');
       }
 
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_ORDER_STATUS_ID') ) {
@@ -136,9 +177,19 @@
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_ORDER_STATUS_ID');
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_ORDER_STATUS_ID') ) {
+        $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_ORDER_STATUS_ID', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_ORDER_STATUS_ID);
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_ORDER_STATUS_ID');
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_ZONE') ) {
         $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_ZONE', MODULE_PAYMENT_PAYPAL_EXPRESS_ZONE);
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_ZONE');
+      }
+
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_ZONE') ) {
+        $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_ZONE', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_ZONE);
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_ZONE');
       }
 
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_SORT_ORDER') ) {
@@ -146,8 +197,17 @@
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_SORT_ORDER');
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_SORT_ORDER') ) {
+        $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_SORT_ORDER', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_SORT_ORDER, 'Sort Order', 'Sort order of display (lowest to highest).');
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_SORT_ORDER');
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTIONS_ORDER_STATUS_ID') ) {
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTIONS_ORDER_STATUS_ID');
+      }
+
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTIONS_ORDER_STATUS_ID') ) {
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTIONS_ORDER_STATUS_ID');
       }
 
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_STATUS') ) {
@@ -165,8 +225,27 @@
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_STATUS');
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_STATUS') ) {
+        $status = '-1';
+
+        if ( (MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_STATUS == 'True') && defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_SERVER') ) {
+          if ( MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_SERVER == 'Live' ) {
+            $status = '1';
+          } else {
+            $status = '0';
+          }
+        }
+
+        $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_EC_STATUS', $status);
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_STATUS');
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_SERVER') ) {
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_TRANSACTION_SERVER');
+      }
+
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_SERVER') ) {
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_TRANSACTION_SERVER');
       }
 
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_VERIFY_SSL') ) {
@@ -177,6 +256,14 @@
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_VERIFY_SSL');
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VERIFY_SSL') ) {
+        if ( !defined('OSCOM_APP_PAYPAL_VERIFY_SSL') ) {
+          $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_VERIFY_SSL', (MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VERIFY_SSL == 'True') ? '1' : '0');
+        }
+
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_VERIFY_SSL');
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_PROXY') ) {
         if ( !defined('OSCOM_APP_PAYPAL_PROXY') ) {
           $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_PROXY', MODULE_PAYMENT_PAYPAL_EXPRESS_PROXY);
@@ -185,8 +272,20 @@
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_PROXY');
       }
 
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PROXY') ) {
+        if ( !defined('OSCOM_APP_PAYPAL_PROXY') ) {
+          $OSCOM_PayPal->saveParameter('OSCOM_APP_PAYPAL_PROXY', MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PROXY);
+        }
+
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_PROXY');
+      }
+
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_DEBUG_EMAIL') ) {
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_DEBUG_EMAIL');
+      }
+
+      if ( defined('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_DEBUG_EMAIL') ) {
+        $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_PRO_PAYFLOW_EC_DEBUG_EMAIL');
       }
 
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_CHECKOUT_FLOW') ) {
@@ -195,6 +294,17 @@
 
       if ( defined('MODULE_PAYMENT_PAYPAL_EXPRESS_DISABLE_IE_COMPAT') ) {
         $OSCOM_PayPal->deleteParameter('MODULE_PAYMENT_PAYPAL_EXPRESS_DISABLE_IE_COMPAT');
+      }
+
+      if ( $is_payflow === true ) {
+        $installed = explode(';', MODULE_PAYMENT_INSTALLED);
+        $installed_pos = array_search($this->_pm_pf_code . '.php', $installed);
+
+        if ( $installed_pos !== false ) {
+          unset($installed[$installed_pos]);
+
+          $OSCOM_PayPal->saveParameter('MODULE_PAYMENT_INSTALLED', implode(';', $installed));
+        }
       }
     }
   }
