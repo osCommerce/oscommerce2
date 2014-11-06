7d799dd3087620ffd12f546922d7054e1c65c48c
Luca <ldanzelmo@gmail.com>
2014-11-06 14:16:51 +0100

    fix typo

--- catalog/admin/includes/classes/language.php
+++ catalog/admin/includes/classes/language.php
@@ -15,7 +15,8 @@
 
   class language {
     var $languages, $catalog_languages, $browser_languages, $language;
-
+    
+    function language($lng = '') {
     $this->languages = array('af' => 'af|afrikaans',
                                'ar' => 'ar([-_][[:alpha:]]{2})?|arabic',
                                'be' => 'be|belarusian',
