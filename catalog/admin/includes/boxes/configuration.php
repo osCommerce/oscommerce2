d46bee54d5da088b8f84ebdec34fad26f557cd6a
acidvertigo <ldanzelmo@gmail.com>
2014-11-22 16:43:23 +0100

    Remove FILENAME_configuration

--- catalog/admin/includes/boxes/configuration.php
+++ catalog/admin/includes/boxes/configuration.php
@@ -29,9 +29,9 @@
   $configuration_groups_query = tep_db_query("select configuration_group_id as cgID, configuration_group_title as cgTitle from " . TABLE_CONFIGURATION_GROUP . " where visible = '1' order by sort_order");
   while ($configuration_groups = tep_db_fetch_array($configuration_groups_query)) {
     $cl_box_groups[sizeof($cl_box_groups)-1]['apps'][] = array(
-      'code' => FILENAME_CONFIGURATION,
+      'code' => 'configuration.php',
       'title' => $configuration_groups['cgTitle'],
-      'link' => tep_href_link(FILENAME_CONFIGURATION, 'gID=' . $configuration_groups['cgID'])
+      'link' => tep_href_link('configuration.php', 'gID=' . $configuration_groups['cgID'])
     );
   }
 ?>
--- catalog/admin/includes/filenames.php
+++ catalog/admin/includes/filenames.php
@@ -11,7 +11,6 @@
 */
 
 // define the filenames used in the project
-  define('FILENAME_CONFIGURATION', 'configuration.php');
   define('FILENAME_COUNTRIES', 'countries.php');
   define('FILENAME_CURRENCIES', 'currencies.php');
   define('FILENAME_CUSTOMERS', 'customers.php');
