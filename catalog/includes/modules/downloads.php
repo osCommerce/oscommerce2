67b0bc3f2e3b2c0742cb87d03df2ca708f56e1e2
acidvertigo <ldanzelmo@gmail.com>
2014-11-22 14:15:46 +0100

    REMOVE FILENAME_DOWNLOAD

--- catalog/includes/modules/downloads.php
+++ catalog/includes/modules/downloads.php
@@ -44,7 +44,7 @@
 // - No expiry date is enforced (maxdays == 0), OR
 // - The expiry date is not reached
       if ( ($downloads['download_count'] > 0) && (file_exists(DIR_FS_DOWNLOAD . $downloads['orders_products_filename'])) && ( ($downloads['download_maxdays'] == 0) || ($download_timestamp > time())) ) {
-        echo '        <td><a href="' . tep_href_link(FILENAME_DOWNLOAD, 'order=' . $last_order . '&id=' . $downloads['orders_products_download_id']) . '">' . $downloads['products_name'] . '</a></td>' . "\n";
+        echo '        <td><a href="' . tep_href_link('download.php', 'order=' . $last_order . '&id=' . $downloads['orders_products_download_id']) . '">' . $downloads['products_name'] . '</a></td>' . "\n";
       } else {
         echo '        <td>' . $downloads['products_name'] . '</td>' . "\n";
       }
