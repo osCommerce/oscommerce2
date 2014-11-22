cdf1d64a20415058fd528fd61b03dac87b73efc7
acidvertigo <ldanzelmo@gmail.com>
2014-11-22 16:41:22 +0100

    Remove FILENAME_CATALOG_ACCOUNT_HISTORY_INFO

--- catalog/admin/includes/filenames.php
+++ catalog/admin/includes/filenames.php
@@ -11,7 +11,6 @@
 */
 
 // define the filenames used in the project
-  define('FILENAME_CATALOG_ACCOUNT_HISTORY_INFO', 'account_history_info.php');
   define('FILENAME_CATEGORIES', 'categories.php');
   define('FILENAME_CONFIGURATION', 'configuration.php');
   define('FILENAME_COUNTRIES', 'countries.php');
