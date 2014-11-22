55de96820bf139b82ec161eb6677401c74e77c65
acidvertigo <ldanzelmo@gmail.com>
2014-11-22 12:25:17 +0100

    REmove filename_privacy

--- catalog/includes/modules/boxes/bm_information.php
+++ catalog/includes/modules/boxes/bm_information.php
@@ -38,7 +38,7 @@
               '  <div class="panel-body">' .
               '    <ul class="list-unstyled">' .
               '      <li><a href="' . tep_href_link(FILENAME_SHIPPING) . '">' . MODULE_BOXES_INFORMATION_BOX_SHIPPING . '</a></li>' .
-              '      <li><a href="' . tep_href_link(FILENAME_PRIVACY) . '">' . MODULE_BOXES_INFORMATION_BOX_PRIVACY . '</a></li>' .
+              '      <li><a href="' . tep_href_link('privacy.php') . '">' . MODULE_BOXES_INFORMATION_BOX_PRIVACY . '</a></li>' .
               '      <li><a href="' . tep_href_link('conditions.php') . '">' . MODULE_BOXES_INFORMATION_BOX_CONDITIONS . '</a></li>' .
               '      <li><a href="' . tep_href_link('contact_us.php') . '">' . MODULE_BOXES_INFORMATION_BOX_CONTACT . '</a></li>' .
               '    </ul>' .
