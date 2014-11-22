e7dadc296d3cee259835678aed77969ffc7f95fb
acidvertigo <ldanzelmo@gmail.com>
2014-11-22 12:09:43 +0100

    delete custom configuration

--- catalog/admin/includes/configure.php
+++ catalog/admin/includes/configure.php
@@ -1,21 +1,35 @@
 <?php
-  define('HTTP_SERVER', 'http://localhost');
-  define('HTTPS_SERVER', 'http://localhost');
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+// define our webserver variables
+// FS = Filesystem (physical)
+// WS = Webserver (virtual)
+  define('HTTP_SERVER', ''); // eg, http://localhost or - https://localhost should not be NULL for productive servers
+  define('HTTPS_SERVER', '');
   define('ENABLE_SSL', false);
   define('HTTP_COOKIE_DOMAIN', '');
   define('HTTPS_COOKIE_DOMAIN', '');
-  define('HTTP_COOKIE_PATH', '/osc/remo/oscommerce2/catalog/admin');
-  define('HTTPS_COOKIE_PATH', '/osc/remo/oscommerce2/catalog/admin');
-  define('HTTP_CATALOG_SERVER', 'http://localhost');
-  define('HTTPS_CATALOG_SERVER', 'http://localhost');
-  define('ENABLE_SSL_CATALOG', 'false');
-  define('DIR_FS_DOCUMENT_ROOT', 'E:/xampp/htdocs/osc/remo/oscommerce2/catalog/');
-  define('DIR_WS_ADMIN', '/osc/remo/oscommerce2/catalog/admin/');
-  define('DIR_WS_HTTPS_ADMIN', '/osc/remo/oscommerce2/catalog/admin/');
-  define('DIR_FS_ADMIN', 'E:/xampp/htdocs/osc/remo/oscommerce2/catalog/admin/');
-  define('DIR_WS_CATALOG', '/osc/remo/oscommerce2/catalog/');
-  define('DIR_WS_HTTPS_CATALOG', '/osc/remo/oscommerce2/catalog/');
-  define('DIR_FS_CATALOG', 'E:/xampp/htdocs/osc/remo/oscommerce2/catalog/');
+  define('HTTP_COOKIE_PATH', '');
+  define('HTTPS_COOKIE_PATH', '');
+  define('HTTP_CATALOG_SERVER', '');
+  define('HTTPS_CATALOG_SERVER', '');
+  define('ENABLE_SSL_CATALOG', 'false'); // secure webserver for catalog module
+  define('DIR_FS_DOCUMENT_ROOT', $DOCUMENT_ROOT); // where your pages are located on the server. if $DOCUMENT_ROOT doesnt suit you, replace with your local path. (eg, /usr/local/apache/htdocs)
+  define('DIR_WS_ADMIN', '/admin/');
+  define('DIR_WS_HTTPS_ADMIN', '/admin/');
+  define('DIR_FS_ADMIN', DIR_FS_DOCUMENT_ROOT . DIR_WS_ADMIN);
+  define('DIR_WS_CATALOG', '/catalog/');
+  define('DIR_WS_HTTPS_CATALOG', '/catalog/');
+  define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG);
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -33,11 +47,11 @@
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
 
-  define('DB_SERVER', 'localhost');
-  define('DB_SERVER_USERNAME', 'root');
+// define our database connection
+  define('DB_SERVER', '');
+  define('DB_SERVER_USERNAME', 'mysql');
   define('DB_SERVER_PASSWORD', '');
-  define('DB_DATABASE', 'shop');
+  define('DB_DATABASE', 'osCommerce');
   define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', 'mysql');
-  define('CFG_TIME_ZONE', 'Europe/Berlin');
+  define('STORE_SESSIONS', '');
 ?>
