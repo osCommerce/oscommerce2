4e3ca795f8b39e9958f05a0d42d343b3c178e54b
acidvertigo <ldanzelmo@gmail.com>
2014-11-22 12:25:44 +0100

    Code clean

--- catalog/privacy.php
+++ catalog/privacy.php
@@ -12,7 +12,7 @@
 
   require('includes/application_top.php');
 
-  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . 'privacy.php');
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/privacy.php');
 
   $breadcrumb->add(NAVBAR_TITLE, tep_href_link('privacy.php'));
 
