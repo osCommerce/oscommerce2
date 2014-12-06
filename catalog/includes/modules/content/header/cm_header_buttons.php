bb21786e0f5f4a0fde6c2f80c9d0ad82cd3e9872
Luca <ldanzelmo@gmail.com>
2014-12-06 14:36:31 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/header/cm_header_buttons.php
+++ catalog/includes/modules/content/header/cm_header_buttons.php
@@ -20,7 +20,7 @@
 
     function cm_header_buttons() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_HEADER_BUTTONS_TITLE;
       $this->description = MODULE_CONTENT_HEADER_BUTTONS_DESCRIPTION;
