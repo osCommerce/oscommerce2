e4a44455622c9de00dc1b3619aa7beea0e64b328
Luca <ldanzelmo@gmail.com>
2014-12-06 14:39:53 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/checkout_success/cm_cs_thank_you.php
+++ catalog/includes/modules/content/checkout_success/cm_cs_thank_you.php
@@ -20,7 +20,7 @@
 
     function cm_cs_thank_you() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_CHECKOUT_SUCCESS_THANK_YOU_TITLE;
       $this->description = MODULE_CONTENT_CHECKOUT_SUCCESS_THANK_YOU_DESCRIPTION;
