909bebbdd9274151f6e0d874bf7baf45cb37b727
Luca <ldanzelmo@gmail.com>
2014-12-06 14:40:42 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/account/cm_account_sage_pay_cards.php
+++ catalog/includes/modules/content/account/cm_account_sage_pay_cards.php
@@ -20,7 +20,7 @@
 
     function cm_account_sage_pay_cards() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_TITLE;
       $this->description = MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_DESCRIPTION;
