4ba9d8de5a501b686d66d9ae5a7c6a036c04eb11
Luca <ldanzelmo@gmail.com>
2014-12-06 14:41:05 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/account/cm_account_set_password.php
+++ catalog/includes/modules/content/account/cm_account_set_password.php
@@ -20,7 +20,7 @@
 
     function cm_account_set_password() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_ACCOUNT_SET_PASSWORD_TITLE;
       $this->description = MODULE_CONTENT_ACCOUNT_SET_PASSWORD_DESCRIPTION;
