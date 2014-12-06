17e379254c03c0c5a0e16b37df13696aaece316a
Luca <ldanzelmo@gmail.com>
2014-12-06 14:41:33 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/account/cm_account_stripe_cards.php
+++ catalog/includes/modules/content/account/cm_account_stripe_cards.php
@@ -20,7 +20,7 @@
 
     function cm_account_stripe_cards() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_ACCOUNT_STRIPE_CARDS_TITLE;
       $this->description = MODULE_CONTENT_ACCOUNT_STRIPE_CARDS_DESCRIPTION;
