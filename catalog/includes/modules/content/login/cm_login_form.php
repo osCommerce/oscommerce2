f917f06f20b92c00616606abc8fe49a1b48722a7
Luca <ldanzelmo@gmail.com>
2014-12-06 14:38:03 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/login/cm_login_form.php
+++ catalog/includes/modules/content/login/cm_login_form.php
@@ -20,7 +20,7 @@
 
     function cm_login_form() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_LOGIN_FORM_TITLE;
       $this->description = MODULE_CONTENT_LOGIN_FORM_DESCRIPTION;
