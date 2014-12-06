fc6b5ff395a61174f96e994e5689128e1dd95fdb
Luca <ldanzelmo@gmail.com>
2014-12-06 14:39:31 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/checkout_success/cm_cs_redirect_old_order.php
+++ catalog/includes/modules/content/checkout_success/cm_cs_redirect_old_order.php
@@ -20,7 +20,7 @@
 
     function cm_cs_redirect_old_order() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_CHECKOUT_SUCCESS_REDIRECT_OLD_ORDER_TITLE;
       $this->description = MODULE_CONTENT_CHECKOUT_SUCCESS_REDIRECT_OLD_ORDER_DESCRIPTION;
