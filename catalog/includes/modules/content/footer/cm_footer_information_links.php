8983aa28c57074015e6a7e98eac292fbc7a63114
Luca <ldanzelmo@gmail.com>
2014-12-06 14:34:25 +0100

    Update to __DIR__ constant

--- catalog/includes/modules/content/footer/cm_footer_information_links.php
+++ catalog/includes/modules/content/footer/cm_footer_information_links.php
@@ -20,7 +20,7 @@
 
     function cm_footer_information_links() {
       $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+      $this->group = basename(__DIR__);
 
       $this->title = MODULE_CONTENT_FOOTER_INFORMATION_TITLE;
       $this->description = MODULE_CONTENT_FOOTER_INFORMATION_DESCRIPTION;
