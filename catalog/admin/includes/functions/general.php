df54db7d4c725f3833ba879627e26880e2db4fd6
Luca <ldanzelmo@gmail.com>
2014-12-20 00:16:32 +0100

    Add conditional check for $ip_addresses[] = $_SERVER['REMOTE_ADDR'];

--- catalog/admin/includes/functions/general.php
+++ catalog/admin/includes/functions/general.php
@@ -1410,7 +1410,9 @@
       $ip_addresses[] = $_SERVER['HTTP_PROXY_USER'];
     }
 
-    $ip_addresses[] = $_SERVER['REMOTE_ADDR'];
+    if ( isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR']) ) {
+        $ip_addresses[] = $_SERVER['REMOTE_ADDR'];
+    }
 
     foreach ( $ip_addresses as $ip ) {
       if (!empty($ip) && tep_validate_ip_address($ip)) {
