20e9e1e5655c342c8150d91a6abdd415c2b6c874
Luca <ldanzelmo@gmail.com>
2015-01-10 22:45:17 +0100

    Remove unused variable

--- catalog/index.php
+++ catalog/index.php
@@ -64,8 +64,6 @@
       $categories_query = tep_db_query("select c.categories_id, cd.categories_name, c.categories_image, c.parent_id from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.parent_id = '" . (int)$current_category_id . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$_SESSION['languages_id'] . "' order by sort_order, cd.categories_name");
     }
 
-    $number_of_categories = tep_db_num_rows($categories_query);
-
     while ($categories = tep_db_fetch_array($categories_query)) {
       $cPath_new = tep_get_path($categories['categories_id']);
       echo '<div class="col-xs-6 col-sm-4">';
