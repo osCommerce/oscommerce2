60906ff0f079eeea0a65235bc030ced0fe7c4d39
gburton <oscshops@gmail.com>
2015-03-20 15:25:05 +0000

    Update Header Boxes

--- catalog/includes/modules/content/header/cm_header_logo.php
+++ catalog/includes/modules/content/header/cm_header_logo.php
@@ -24,6 +24,7 @@
 
       $this->title = MODULE_CONTENT_HEADER_LOGO_TITLE;
       $this->description = MODULE_CONTENT_HEADER_LOGO_DESCRIPTION;
+      if (defined('MODULE_CONTENT_BOOTSTRAP_ROW_DESCRIPTION')) $this->description .= '<div class="secWarning">' . MODULE_CONTENT_BOOTSTRAP_ROW_DESCRIPTION . '</div>';
 
       if ( defined('MODULE_CONTENT_HEADER_LOGO_STATUS') ) {
         $this->sort_order = MODULE_CONTENT_HEADER_LOGO_SORT_ORDER;
@@ -34,29 +35,7 @@
     function execute() {
       global $oscTemplate;
       
-      switch (MODULE_CONTENT_HEADER_LOGO_CONTENT_WIDTH) {
-        case "100%":
-        $content_width = 12;
-        break;
-        case "75%":
-        $content_width = 9;
-        break;
-        case "66%":
-        $content_width = 8;
-        break;
-        case "50%":
-        $content_width = 6;
-        break;
-        case "33%":
-        $content_width = 4;
-        break;
-        case "25%":
-        $content_width = 3;
-        break;
-        case "20%":
-        default:
-        $content_width = 2;
-      }
+      $content_width = (int)MODULE_CONTENT_HEADER_LOGO_CONTENT_WIDTH;
       
       ob_start();
       include(DIR_WS_MODULES . 'content/' . $this->group . '/templates/logo.php');
@@ -74,13 +53,13 @@
     }
 
     function install() {
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Header Logo Module', 'MODULE_CONTENT_HEADER_LOGO_STATUS', 'True', 'Do you want to enable the Logo content module?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'MODULE_CONTENT_HEADER_LOGO_CONTENT_WIDTH', '50%', 'What width container should the content be shown in?', '6', '1', 'tep_cfg_select_option(array(\'20%\', \'25%\', \'33%\', \'50%\', \'66%\', \'75%\', \'100%\'), ', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_HEADER_LOGO_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Header Logo Module', 'MODULE_CONTENT_HEADER_LOGO_STATUS', 'True', 'Do you want to enable the Logo content module?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'MODULE_CONTENT_HEADER_LOGO_CONTENT_WIDTH', '4', 'What width container should the content be shown in?', '6', '1', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_HEADER_LOGO_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
     function remove() {
-      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->keys()) . "')");
+      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
     }
 
     function keys() {
