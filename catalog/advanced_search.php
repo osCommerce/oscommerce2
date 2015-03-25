5c25892a7ec53ba0721d6908eb209c715a15f5b0
gburton <oscshops@gmail.com>
2015-03-25 10:55:40 +0000

    Update Advanced Search Page and Results Page

--- catalog/advanced_search.php
+++ catalog/advanced_search.php
@@ -12,9 +12,9 @@
 
   require('includes/application_top.php');
 
-  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_ADVANCED_SEARCH);
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/advanced_search.php');
 
-  $breadcrumb->add(NAVBAR_TITLE_1, tep_href_link(FILENAME_ADVANCED_SEARCH));
+  $breadcrumb->add(NAVBAR_TITLE_1, tep_href_link('advanced_search.php'));
 
   require(DIR_WS_INCLUDES . 'template_top.php');
 ?>
@@ -29,7 +29,7 @@
   }
 ?>
 
-<?php echo tep_draw_form('advanced_search', tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', 'NONSSL', false), 'get', 'class="form-horizontal" role="form"') . tep_hide_session_id(); ?>
+<?php echo tep_draw_form('advanced_search', tep_href_link('advanced_search_result.php', '', 'NONSSL', false), 'get', 'class="form-horizontal" role="form"') . tep_hide_session_id(); ?>
 
 <div class="contentContainer">
 
@@ -38,8 +38,8 @@
     <p class="inputRequirement text-right"><?php echo FORM_REQUIRED_INFORMATION; ?></p>
 
     <div class="form-group has-feedback">
-      <label for="inputKeywords" class="control-label col-xs-3"><?php echo HEADING_SEARCH_CRITERIA; ?></label>
-      <div class="col-xs-9">
+      <label for="inputKeywords" class="control-label col-sm-3"><?php echo HEADING_SEARCH_CRITERIA; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_input_field('keywords', '', 'required aria-required="true" id="inputKeywords" placeholder="' . TEXT_SEARCH_PLACEHOLDER . '"');
         echo FORM_REQUIRED_INPUT;
@@ -51,8 +51,8 @@
     <br />
 
     <div class="row">
-      <div class="col-sm-6 text-right pull-right"><?php echo tep_draw_button(IMAGE_BUTTON_SEARCH, 'glyphicon glyphicon-search', null, 'primary', null, 'btn-success'); ?></div>
-      <div class="col-sm-6"><a data-toggle="modal" data-target="#helpSearch" class="btn btn-link"><?php echo TEXT_SEARCH_HELP_LINK; ?></a></div>
+      <div class="col-xs-6 text-right pull-right"><?php echo tep_draw_button(IMAGE_BUTTON_SEARCH, 'glyphicon glyphicon-search', null, 'primary', null, 'btn-success'); ?></div>
+      <div class="col-xs-6"><a data-toggle="modal" data-target="#helpSearch" class="btn btn-link"><?php echo TEXT_SEARCH_HELP_LINK; ?></a></div>
     </div>
 
     <div class="modal fade" id="helpSearch" tabindex="-1" role="dialog" aria-labelledby="helpSearchLabel" aria-hidden="true">
@@ -74,16 +74,16 @@
     <hr>
 
     <div class="form-group">
-      <label for="entryCategories" class="control-label col-xs-3"><?php echo ENTRY_CATEGORIES; ?></label>
-      <div class="col-xs-9">
+      <label for="entryCategories" class="control-label col-sm-3"><?php echo ENTRY_CATEGORIES; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_pull_down_menu('categories_id', tep_get_categories(array(array('id' => '', 'text' => TEXT_ALL_CATEGORIES))), NULL, 'id="entryCategories"');
         ?>
       </div>
     </div>
     <div class="form-group">
-      <label for="entryIncludeSubs" class="control-label col-xs-3"><?php echo ENTRY_INCLUDE_SUBCATEGORIES; ?></label>
-      <div class="col-xs-9">
+      <label for="entryIncludeSubs" class="control-label col-sm-3"><?php echo ENTRY_INCLUDE_SUBCATEGORIES; ?></label>
+      <div class="col-sm-9">
         <div class="checkbox">
           <label>
             <?php
@@ -94,40 +94,40 @@
       </div>
     </div>
     <div class="form-group">
-      <label for="entryManufacturers" class="control-label col-xs-3"><?php echo ENTRY_MANUFACTURERS; ?></label>
-      <div class="col-xs-9">
+      <label for="entryManufacturers" class="control-label col-sm-3"><?php echo ENTRY_MANUFACTURERS; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_pull_down_menu('manufacturers_id', tep_get_manufacturers(array(array('id' => '', 'text' => TEXT_ALL_MANUFACTURERS))), NULL, 'id="entryManufacturers"');
         ?>
       </div>
     </div>
     <div class="form-group">
-      <label for="PriceFrom" class="control-label col-xs-3"><?php echo ENTRY_PRICE_FROM; ?></label>
-      <div class="col-xs-9">
+      <label for="PriceFrom" class="control-label col-sm-3"><?php echo ENTRY_PRICE_FROM; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_input_field('pfrom', '', 'id="PriceFrom" placeholder="' . ENTRY_PRICE_FROM . '"');
         ?>
       </div>
     </div>
     <div class="form-group">
-      <label for="PriceTo" class="control-label col-xs-3"><?php echo ENTRY_PRICE_TO; ?></label>
-      <div class="col-xs-9">
+      <label for="PriceTo" class="control-label col-sm-3"><?php echo ENTRY_PRICE_TO; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_input_field('pto', '', 'id="PriceTo" placeholder="' . ENTRY_PRICE_TO . '"');
         ?>
       </div>
     </div>
     <div class="form-group">
-      <label for="dfrom" class="control-label col-xs-3"><?php echo ENTRY_DATE_FROM; ?></label>
-      <div class="col-xs-9">
+      <label for="dfrom" class="control-label col-sm-3"><?php echo ENTRY_DATE_FROM; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_input_field('dfrom', '', 'id="dfrom" placeholder="' . ENTRY_DATE_FROM . '"');
         ?>
       </div>
     </div>
     <div class="form-group">
-      <label for="dto" class="control-label col-xs-3"><?php echo ENTRY_DATE_TO; ?></label>
-      <div class="col-xs-9">
+      <label for="dto" class="control-label col-sm-3"><?php echo ENTRY_DATE_TO; ?></label>
+      <div class="col-sm-9">
         <?php
         echo tep_draw_input_field('dto', '', 'id="dto" placeholder="' . ENTRY_DATE_TO . '"');
         ?>
