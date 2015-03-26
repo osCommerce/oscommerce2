efe70d043f60ef0a503f183a63324ba09edb9f6d
gburton <oscshops@gmail.com>
2015-03-26 17:31:26 +0000

    Pretty Search

--- catalog/includes/languages/english/modules/boxes/bm_search.php
+++ catalog/includes/languages/english/modules/boxes/bm_search.php
@@ -13,6 +13,5 @@
   define('MODULE_BOXES_SEARCH_TITLE', 'Search');
   define('MODULE_BOXES_SEARCH_DESCRIPTION', 'Show search field');
   define('MODULE_BOXES_SEARCH_BOX_TITLE', 'Quick Find');
-  define('MODULE_BOXES_SEARCH_BOX_TEXT', 'Use keywords to find the product you are looking for.');
   define('MODULE_BOXES_SEARCH_BOX_ADVANCED_SEARCH', 'Advanced Search');
-?>
+
