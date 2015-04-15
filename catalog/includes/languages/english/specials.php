bdb97f6509e72a50361e2aea4c6ba6fbcccfcaa9
Eliot Rayner <eliot@ersd.net>
2015-04-15 15:40:50 +0100

    Update specials.php
    Added missing TABLE_HEADING_ defs

--- catalog/includes/languages/english/specials.php
+++ catalog/includes/languages/english/specials.php
@@ -12,4 +12,12 @@
 
 define('NAVBAR_TITLE', 'Specials');
 define('HEADING_TITLE', 'Get Them While They\'re Hot!');
+define('TABLE_HEADING_IMAGE', '');
+define('TABLE_HEADING_MODEL', 'Model');
+define('TABLE_HEADING_PRODUCTS', 'Product Name');
+define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
+define('TABLE_HEADING_QUANTITY', 'Quantity');
+define('TABLE_HEADING_PRICE', 'Price');
+define('TABLE_HEADING_WEIGHT', 'Weight');
+define('TABLE_HEADING_BUY_NOW', 'Buy Now');
 ?>
