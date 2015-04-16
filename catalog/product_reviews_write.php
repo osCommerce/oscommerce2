8171f2e54fe576d09b7ece47780f997cd4783953
Eliot Rayner <eliot@ersd.net>
2015-04-16 13:05:49 +0100

    Update product_reviews_write.php
    $customer_id to (int)$_SESSION['customer_id']

--- catalog/product_reviews_write.php
+++ catalog/product_reviews_write.php
@@ -51,7 +51,7 @@
     }
 
     if ($error == false) {
-      tep_db_query("insert into reviews (products_id, customers_id, customers_name, reviews_rating, date_added) values ('" . (int)$_GET['products_id'] . "', '" . (int)$customer_id . "', '" . tep_db_input($customer['customers_firstname']) . ' ' . tep_db_input($customer['customers_lastname']) . "', '" . tep_db_input($rating) . "', now())");
+      tep_db_query("insert into reviews (products_id, customers_id, customers_name, reviews_rating, date_added) values ('" . (int)$_GET['products_id'] . "', '" . (int)$_SESSION['customer_id'] . "', '" . tep_db_input($customer['customers_firstname']) . ' ' . tep_db_input($customer['customers_lastname']) . "', '" . tep_db_input($rating) . "', now())");
       $insert_id = tep_db_insert_id();
 
       tep_db_query("insert into reviews_description (reviews_id, languages_id, reviews_text) values ('" . (int)$insert_id . "', '" . (int)$_SESSION['languages_id'] . "', '" . tep_db_input($review) . "')");
