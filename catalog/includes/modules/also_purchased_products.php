a1844f7b24f6219b187deda71ddc0e9622385958
Harald Ponce de Leon <hpdl@oscommerce.com>
2015-05-19 09:36:09 +0200

    replace tep_db* with OSC\OM\Db

--- catalog/includes/modules/also_purchased_products.php
+++ catalog/includes/modules/also_purchased_products.php
@@ -11,18 +11,23 @@
 */
 
   if (isset($_GET['products_id'])) {
-    $orders_query = tep_db_query("select p.products_id, p.products_image, pd.products_name from orders_products opa, orders_products opb, orders o, products p LEFT JOIN products_description pd on p.products_id = pd.products_id where opa.products_id = '" . (int)$_GET['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . (int)$_GET['products_id'] . "' and opb.products_id = p.products_id and opb.orders_id = o.orders_id and p.products_status = '1' and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' group by p.products_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
-    $num_products_ordered = tep_db_num_rows($orders_query);
-    if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
+    $Qorders = $OSCOM_Db->prepare('select p.products_id, p.products_image, pd.products_name from :table_orders_products opa, :table_orders_products opb, :table_orders o, :table_products p left join :table_products_description pd on p.products_id = pd.products_id where opa.products_id = :products_id and opa.orders_id = opb.orders_id and opb.products_id != opa.products_id and opb.products_id = p.products_id and opb.orders_id = o.orders_id and p.products_status = 1 and pd.language_id = :language_id group by p.products_id order by o.date_purchased desc limit :limit');
+    $Qorders->bindInt(':products_id', $_GET['products_id']);
+    $Qorders->bindInt(':language_id', $_SESSION['languages_id']);
+    $Qorders->bindInt(':limit', MAX_DISPLAY_ALSO_PURCHASED);
+    $Qorders->execute();
 
+    $orders = $Qorders->fetchAll();
+
+    if (count($orders) >= MIN_DISPLAY_ALSO_PURCHASED) {
       $also_pur_prods_content = NULL;
 
-      while ($orders = tep_db_fetch_array($orders_query)) {
+      foreach ($orders as $o) {
         $also_pur_prods_content .= '<div class="col-sm-6 col-md-4">';
         $also_pur_prods_content .= '  <div class="thumbnail">';
-        $also_pur_prods_content .= '    <a href="' . tep_href_link('product_info.php', 'products_id=' . $orders['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $orders['products_image'], $orders['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a>';
+        $also_pur_prods_content .= '    <a href="' . tep_href_link('product_info.php', 'products_id=' . $o['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $o['products_image'], $o['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a>';
         $also_pur_prods_content .= '    <div class="caption">';
-        $also_pur_prods_content .= '      <p class="text-center"><a href="' . tep_href_link('product_info.php', 'products_id=' . $orders['products_id']) . '">' . $orders['products_name'] . '</a></p>';
+        $also_pur_prods_content .= '      <p class="text-center"><a href="' . tep_href_link('product_info.php', 'products_id=' . $o['products_id']) . '">' . $o['products_name'] . '</a></p>';
         $also_pur_prods_content .= '    </div>';
         $also_pur_prods_content .= '  </div>';
         $also_pur_prods_content .= '</div>';
