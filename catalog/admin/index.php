002098ec015a8687295c93d28d3e40c500b67d79
Harald Ponce de Leon <hpdl@oscommerce.com>
2015-07-16 19:32:19 +0200

    introduce basic 404 error page

--- catalog/admin/index.php
+++ catalog/admin/index.php
@@ -13,11 +13,11 @@ require('includes/application_top.php');
 if (OSCOM::isRPC() === false) {
     $page_file = OSCOM::getSitePageFile();
 
-    if (!empty($page_file) && file_exists($page_file)) {
-        include($page_file);
-    } else {
-        http_response_code(404);
+    if (empty($page_file) || !file_exists($page_file)) {
+        $page_file = DIR_FS_CATALOG . 'includes/error_documents/404.php';
     }
+
+    include($page_file);
 }
 
 require('includes/application_bottom.php');
