89ed4ad43f5702676503c0dcf333676f250f20ce
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-06-24 00:36:33 +0200

    update jquery version

--- catalog/admin/includes/template_top.php
+++ catalog/admin/includes/template_top.php
@@ -20,7 +20,7 @@
 <base href="<?php echo ($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_ADMIN : HTTP_SERVER . DIR_WS_ADMIN; ?>" />
 <!--[if IE]><script type="text/javascript" src="<?php echo tep_catalog_href_link('ext/flot/excanvas.min.js', '', 'SSL'); ?>"></script><![endif]-->
 <link rel="stylesheet" type="text/css" href="<?php echo tep_catalog_href_link('ext/jquery/ui/redmond/jquery-ui-1.11.4.min.css', '', 'SSL'); ?>">
-<script type="text/javascript" src="<?php echo tep_catalog_href_link('ext/jquery/jquery-2.1.4.min.js', '', 'SSL'); ?>"></script>
+<script type="text/javascript" src="<?php echo tep_catalog_href_link('ext/jquery/jquery-2.2.3.min.js', '', 'SSL'); ?>"></script>
 <script type="text/javascript" src="<?php echo tep_catalog_href_link('ext/jquery/ui/jquery-ui-1.11.4.min.js', '', 'SSL'); ?>"></script>
 
 <?php
