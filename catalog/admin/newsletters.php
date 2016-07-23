64ab381685ddb92c9667f416895d7cff14899a5b
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-07-23 16:13:16 +0200

    replace legacy database layer

--- catalog/admin/newsletters.php
+++ catalog/admin/newsletters.php
@@ -15,25 +15,29 @@
 
   require('includes/application_top.php');
 
+  if (!isset($_GET['page']) || !is_numeric($_GET['page'])) {
+    $_GET['page'] = 1;
+  }
+
   $action = (isset($_GET['action']) ? $_GET['action'] : '');
 
   if (tep_not_null($action)) {
     switch ($action) {
       case 'lock':
       case 'unlock':
-        $newsletter_id = tep_db_prepare_input($_GET['nID']);
+        $newsletter_id = HTML::sanitize($_GET['nID']);
         $status = (($action == 'lock') ? '1' : '0');
 
-        tep_db_query("update " . TABLE_NEWSLETTERS . " set locked = '" . $status . "' where newsletters_id = '" . (int)$newsletter_id . "'");
+        $OSCOM_Db->save('newsletters', ['locked' => $status], ['newsletters_id' => (int)$newsletter_id]);
 
         OSCOM::redirect(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $_GET['nID']);
         break;
       case 'insert':
       case 'update':
-        if (isset($_POST['newsletter_id'])) $newsletter_id = tep_db_prepare_input($_POST['newsletter_id']);
-        $newsletter_module = tep_db_prepare_input($_POST['module']);
-        $title = tep_db_prepare_input($_POST['title']);
-        $content = tep_db_prepare_input($_POST['content']);
+        if (isset($_POST['newsletter_id'])) $newsletter_id = HTML::sanitize($_POST['newsletter_id']);
+        $newsletter_module = HTML::sanitize($_POST['module']);
+        $title = HTML::sanitize($_POST['title']);
+        $content = HTML::sanitize($_POST['content']);
 
         $newsletter_error = false;
         if (empty($title)) {
@@ -56,21 +60,21 @@
             $sql_data_array['status'] = '0';
             $sql_data_array['locked'] = '0';
 
-            tep_db_perform(TABLE_NEWSLETTERS, $sql_data_array);
-            $newsletter_id = tep_db_insert_id();
+            $OSCOM_Db->save('newsletters', $sql_data_array);
+            $newsletter_id = $OSCOM_Db->lastInsertId();
           } elseif ($action == 'update') {
-            tep_db_perform(TABLE_NEWSLETTERS, $sql_data_array, 'update', "newsletters_id = '" . (int)$newsletter_id . "'");
+            $OSCOM_Db->save('newsletters', $sql_data_array, ['newsletters_id' => (int)$newsletter_id]);
           }
 
-          OSCOM::redirect(FILENAME_NEWSLETTERS, (isset($_GET['page']) ? 'page=' . $_GET['page'] . '&' : '') . 'nID=' . $newsletter_id);
+          OSCOM::redirect(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $newsletter_id);
         } else {
           $action = 'new';
         }
         break;
       case 'deleteconfirm':
-        $newsletter_id = tep_db_prepare_input($_GET['nID']);
+        $newsletter_id = HTML::sanitize($_GET['nID']);
 
-        tep_db_query("delete from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$newsletter_id . "'");
+        $OSCOM_Db->delete('newsletters', ['newsletters_id' => (int)$newsletter_id]);
 
         OSCOM::redirect(FILENAME_NEWSLETTERS, 'page=' . $_GET['page']);
         break;
@@ -78,22 +82,23 @@
       case 'new': if (!isset($_GET['nID'])) break;
       case 'send':
       case 'confirm_send':
-        $newsletter_id = tep_db_prepare_input($_GET['nID']);
+        $newsletter_id = HTML::sanitize($_GET['nID']);
 
-        $check_query = tep_db_query("select locked from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$newsletter_id . "'");
-        $check = tep_db_fetch_array($check_query);
+        $Qcheck = $OSCOM_Db->get('newsletters', 'locked', ['newsletters_id' => (int)$newsletter_id]);
 
-        if ($check['locked'] < 1) {
-          switch ($action) {
-            case 'delete': $error = ERROR_REMOVE_UNLOCKED_NEWSLETTER; break;
-            case 'new': $error = ERROR_EDIT_UNLOCKED_NEWSLETTER; break;
-            case 'send': $error = ERROR_SEND_UNLOCKED_NEWSLETTER; break;
-            case 'confirm_send': $error = ERROR_SEND_UNLOCKED_NEWSLETTER; break;
+        if ($Qcheck->fetch() !== false) {
+          if ($Qcheck->valueInt('locked') < 1) {
+            switch ($action) {
+              case 'delete': $error = ERROR_REMOVE_UNLOCKED_NEWSLETTER; break;
+              case 'new': $error = ERROR_EDIT_UNLOCKED_NEWSLETTER; break;
+              case 'send': $error = ERROR_SEND_UNLOCKED_NEWSLETTER; break;
+              case 'confirm_send': $error = ERROR_SEND_UNLOCKED_NEWSLETTER; break;
+            }
+
+            $messageStack->add_session($error, 'error');
+
+            OSCOM::redirect(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $_GET['nID']);
           }
-
-          $messageStack->add_session($error, 'error');
-
-          OSCOM::redirect(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $_GET['nID']);
         }
         break;
     }
@@ -123,12 +128,17 @@
     if (isset($_GET['nID'])) {
       $form_action = 'update';
 
-      $nID = tep_db_prepare_input($_GET['nID']);
+      $nID = HTML::sanitize($_GET['nID']);
 
-      $newsletter_query = tep_db_query("select title, content, module from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$nID . "'");
-      $newsletter = tep_db_fetch_array($newsletter_query);
+      $Qnewsletter = $OSCOM_Db->get('newsletters', [
+        'title',
+        'content',
+        'module'
+      ], [
+        'newsletters_id' => (int)$nID
+      ]);
 
-      $nInfo->objectInfo($newsletter);
+      $nInfo->objectInfo($Qnewsletter->toArray());
     } elseif ($_POST) {
       $nInfo->objectInfo($_POST);
     }
@@ -151,7 +161,7 @@
       $modules_array[] = array('id' => substr($directory_array[$i], 0, strrpos($directory_array[$i], '.')), 'text' => substr($directory_array[$i], 0, strrpos($directory_array[$i], '.')));
     }
 ?>
-      <tr><?php echo HTML::form('newsletter', OSCOM::link(FILENAME_NEWSLETTERS, (isset($_GET['page']) ? 'page=' . $_GET['page'] . '&' : '') . 'action=' . $form_action)); if ($form_action == 'update') echo HTML::hiddenField('newsletter_id', $nID); ?>
+      <tr><?php echo HTML::form('newsletter', OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&action=' . $form_action)); if ($form_action == 'update') echo HTML::hiddenField('newsletter_id', $nID); ?>
         <td><table border="0" cellspacing="0" cellpadding="2">
           <tr>
             <td class="main"><?php echo TEXT_NEWSLETTER_MODULE; ?></td>
@@ -176,18 +186,23 @@
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
           <tr>
-            <td class="smallText" align="right"><?php echo HTML::button(IMAGE_SAVE, 'fa fa-save', null, 'primary') . HTML::button(IMAGE_CANCEL, 'fa fa-close', OSCOM::link(FILENAME_NEWSLETTERS, (isset($_GET['page']) ? 'page=' . $_GET['page'] . '&' : '') . (isset($_GET['nID']) ? 'nID=' . $_GET['nID'] : ''))); ?></td>
+            <td class="smallText" align="right"><?php echo HTML::button(IMAGE_SAVE, 'fa fa-save', null, 'primary') . HTML::button(IMAGE_CANCEL, 'fa fa-close', OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&' . (isset($_GET['nID']) ? 'nID=' . $_GET['nID'] : ''))); ?></td>
           </tr>
         </table></td>
       </form></tr>
 <?php
   } elseif ($action == 'preview') {
-    $nID = tep_db_prepare_input($_GET['nID']);
+    $nID = HTML::sanitize($_GET['nID']);
 
-    $newsletter_query = tep_db_query("select title, content, module from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$nID . "'");
-    $newsletter = tep_db_fetch_array($newsletter_query);
+    $Qnewsletter = $OSCOM_Db->get('newsletters', [
+      'title',
+      'content',
+      'module'
+    ], [
+      'newsletters_id' => (int)$nID
+    ]);
 
-    $nInfo = new objectInfo($newsletter);
+    $nInfo = new objectInfo($Qnewsletter->toArray());
 ?>
       <tr>
         <td class="smallText" align="right"><?php echo HTML::button(IMAGE_BACK, 'fa fa-chevron-left', OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $_GET['nID'])); ?></td>
@@ -200,14 +215,19 @@
       </tr>
 <?php
   } elseif ($action == 'send') {
-    $nID = tep_db_prepare_input($_GET['nID']);
+    $nID = HTML::sanitize($_GET['nID']);
 
-    $newsletter_query = tep_db_query("select title, content, module from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$nID . "'");
-    $newsletter = tep_db_fetch_array($newsletter_query);
+    $Qnewsletter = $OSCOM_Db->get('newsletters', [
+      'title',
+      'content',
+      'module'
+    ], [
+      'newsletters_id' => (int)$nID
+    ]);
 
-    $nInfo = new objectInfo($newsletter);
+    $nInfo = new objectInfo($Qnewsletter->toArray());
 
-    include(DIR_WS_LANGUAGES . $language . '/modules/newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
+    include(DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
     include(DIR_WS_MODULES . 'newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
     $module_name = $nInfo->module;
     $module = new $module_name($nInfo->title, $nInfo->content);
@@ -217,14 +237,19 @@
       </tr>
 <?php
   } elseif ($action == 'confirm') {
-    $nID = tep_db_prepare_input($_GET['nID']);
+    $nID = HTML::sanitize($_GET['nID']);
 
-    $newsletter_query = tep_db_query("select title, content, module from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$nID . "'");
-    $newsletter = tep_db_fetch_array($newsletter_query);
+    $Qnewsletter = $OSCOM_Db->get('newsletters', [
+      'title',
+      'content',
+      'module'
+    ], [
+      'newsletters_id' => (int)$nID
+    ]);
 
-    $nInfo = new objectInfo($newsletter);
+    $nInfo = new objectInfo($Qnewsletter->toArray());
 
-    include(DIR_WS_LANGUAGES . $language . '/modules/newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
+    include(DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
     include(DIR_WS_MODULES . 'newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
     $module_name = $nInfo->module;
     $module = new $module_name($nInfo->title, $nInfo->content);
@@ -234,14 +259,20 @@
       </tr>
 <?php
   } elseif ($action == 'confirm_send') {
-    $nID = tep_db_prepare_input($_GET['nID']);
+    $nID = HTML::sanitize($_GET['nID']);
 
-    $newsletter_query = tep_db_query("select newsletters_id, title, content, module from " . TABLE_NEWSLETTERS . " where newsletters_id = '" . (int)$nID . "'");
-    $newsletter = tep_db_fetch_array($newsletter_query);
+    $Qnewsletter = $OSCOM_Db->get('newsletters', [
+      'newsletters_id',
+      'title',
+      'content',
+      'module'
+    ], [
+      'newsletters_id' => (int)$nID
+    ]);
 
-    $nInfo = new objectInfo($newsletter);
+    $nInfo = new objectInfo($Qnewsletter->toArray());
 
-    include(DIR_WS_LANGUAGES . $language . '/modules/newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
+    include(DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
     include(DIR_WS_MODULES . 'newsletters/' . $nInfo->module . substr($PHP_SELF, strrpos($PHP_SELF, '.')));
     $module_name = $nInfo->module;
     $module = new $module_name($nInfo->title, $nInfo->content);
@@ -281,26 +312,27 @@
                 <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</td>
               </tr>
 <?php
-    $newsletters_query_raw = "select newsletters_id, title, length(content) as content_length, module, date_added, date_sent, status, locked from " . TABLE_NEWSLETTERS . " order by date_added desc";
-    $newsletters_split = new splitPageResults($_GET['page'], MAX_DISPLAY_SEARCH_RESULTS, $newsletters_query_raw, $newsletters_query_numrows);
-    $newsletters_query = tep_db_query($newsletters_query_raw);
-    while ($newsletters = tep_db_fetch_array($newsletters_query)) {
-    if ((!isset($_GET['nID']) || (isset($_GET['nID']) && ($_GET['nID'] == $newsletters['newsletters_id']))) && !isset($nInfo) && (substr($action, 0, 3) != 'new')) {
-        $nInfo = new objectInfo($newsletters);
+    $Qnewsletters = $OSCOM_Db->prepare('select SQL_CALC_FOUND_ROWS newsletters_id, title, length(content) as content_length, module, date_added, date_sent, status, locked from :table_newsletters order by date_added desc limit :page_set_offset, :page_set_max_results');
+    $Qnewsletters->setPageSet(MAX_DISPLAY_SEARCH_RESULTS);
+    $Qnewsletters->execute();
+
+    while ($Qnewsletters->fetch()) {
+    if ((!isset($_GET['nID']) || (isset($_GET['nID']) && ((int)$_GET['nID'] === $Qnewsletters->valueInt('newsletters_id')))) && !isset($nInfo) && (substr($action, 0, 3) != 'new')) {
+        $nInfo = new objectInfo($Qnewsletters->toArray());
       }
 
-      if (isset($nInfo) && is_object($nInfo) && ($newsletters['newsletters_id'] == $nInfo->newsletters_id) ) {
+      if (isset($nInfo) && is_object($nInfo) && ($Qnewsletters->valueInt('newsletters_id') === (int)$nInfo->newsletters_id) ) {
         echo '                  <tr id="defaultSelected" class="dataTableRowSelected" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $nInfo->newsletters_id . '&action=preview') . '\'">' . "\n";
       } else {
-        echo '                  <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $newsletters['newsletters_id']) . '\'">' . "\n";
+        echo '                  <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $Qnewsletters->valueInt('newsletters_id')) . '\'">' . "\n";
       }
 ?>
-                <td class="dataTableContent"><?php echo '<a href="' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $newsletters['newsletters_id'] . '&action=preview') . '">' . HTML::image(DIR_WS_ICONS . 'preview.gif', ICON_PREVIEW) . '</a>&nbsp;' . $newsletters['title']; ?></td>
-                <td class="dataTableContent" align="right"><?php echo number_format($newsletters['content_length']) . ' bytes'; ?></td>
-                <td class="dataTableContent" align="right"><?php echo $newsletters['module']; ?></td>
-                <td class="dataTableContent" align="center"><?php if ($newsletters['status'] == '1') { echo HTML::image(DIR_WS_ICONS . 'tick.gif', ICON_TICK); } else { echo HTML::image(DIR_WS_ICONS . 'cross.gif', ICON_CROSS); } ?></td>
-                <td class="dataTableContent" align="center"><?php if ($newsletters['locked'] > 0) { echo HTML::image(DIR_WS_ICONS . 'locked.gif', ICON_LOCKED); } else { echo HTML::image(DIR_WS_ICONS . 'unlocked.gif', ICON_UNLOCKED); } ?></td>
-                <td class="dataTableContent" align="right"><?php if (isset($nInfo) && is_object($nInfo) && ($newsletters['newsletters_id'] == $nInfo->newsletters_id) ) { echo HTML::image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); } else { echo '<a href="' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $newsletters['newsletters_id']) . '">' . HTML::image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
+                <td class="dataTableContent"><?php echo '<a href="' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $Qnewsletters->valueInt('newsletters_id') . '&action=preview') . '">' . HTML::image(DIR_WS_ICONS . 'preview.gif', ICON_PREVIEW) . '</a>&nbsp;' . $Qnewsletters->value('title'); ?></td>
+                <td class="dataTableContent" align="right"><?php echo number_format($Qnewsletters->valueInt('content_length')) . ' bytes'; ?></td>
+                <td class="dataTableContent" align="right"><?php echo $Qnewsletters->value('module'); ?></td>
+                <td class="dataTableContent" align="center"><?php if ($Qnewsletters->valueInt('status') === 1) { echo HTML::image(DIR_WS_ICONS . 'tick.gif', ICON_TICK); } else { echo HTML::image(DIR_WS_ICONS . 'cross.gif', ICON_CROSS); } ?></td>
+                <td class="dataTableContent" align="center"><?php if ($Qnewsletters->valueInt('locked') > 0) { echo HTML::image(DIR_WS_ICONS . 'locked.gif', ICON_LOCKED); } else { echo HTML::image(DIR_WS_ICONS . 'unlocked.gif', ICON_UNLOCKED); } ?></td>
+                <td class="dataTableContent" align="right"><?php if (isset($nInfo) && is_object($nInfo) && ($Qnewsletters->valueInt('newsletters_id') === (int)$nInfo->newsletters_id) ) { echo HTML::image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); } else { echo '<a href="' . OSCOM::link(FILENAME_NEWSLETTERS, 'page=' . $_GET['page'] . '&nID=' . $Qnewsletters->valueInt('newsletters_id')) . '">' . HTML::image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
               </tr>
 <?php
     }
@@ -308,8 +340,8 @@
               <tr>
                 <td colspan="6"><table border="0" width="100%" cellspacing="0" cellpadding="2">
                   <tr>
-                    <td class="smallText" valign="top"><?php echo $newsletters_split->display_count($newsletters_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $_GET['page'], TEXT_DISPLAY_NUMBER_OF_NEWSLETTERS); ?></td>
-                    <td class="smallText" align="right"><?php echo $newsletters_split->display_links($newsletters_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $_GET['page']); ?></td>
+                    <td class="smallText" valign="top"><?php echo $Qnewsletters->getPageSetLabel(TEXT_DISPLAY_NUMBER_OF_NEWSLETTERS); ?></td>
+                    <td class="smallText" align="right"><?php echo $Qnewsletters->getPageSetLinks(); ?></td>
                   </tr>
                   <tr>
                     <td class="smallText" align="right" colspan="2"><?php echo HTML::button(IMAGE_NEW_NEWSLETTER, 'fa fa-plus', OSCOM::link(FILENAME_NEWSLETTERS, 'action=new')); ?></td>
