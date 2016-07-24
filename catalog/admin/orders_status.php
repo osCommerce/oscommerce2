ecb1b1d33da132308ed917b2cf0edf74ac5016cb
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-07-24 03:31:03 +0200

    replace legacy database layer

--- catalog/admin/orders_status.php
+++ catalog/admin/orders_status.php
@@ -15,28 +15,31 @@
 
   require('includes/application_top.php');
 
+  if (!isset($_GET['page']) || !is_numeric($_GET['page'])) {
+    $_GET['page'] = 1;
+  }
+
   $action = (isset($_GET['action']) ? $_GET['action'] : '');
 
   if (tep_not_null($action)) {
     switch ($action) {
       case 'insert':
       case 'save':
-        if (isset($_GET['oID'])) $orders_status_id = tep_db_prepare_input($_GET['oID']);
+        if (isset($_GET['oID'])) $orders_status_id = HTML::sanitize($_GET['oID']);
 
         $languages = tep_get_languages();
         for ($i=0, $n=sizeof($languages); $i<$n; $i++) {
           $orders_status_name_array = $_POST['orders_status_name'];
           $language_id = $languages[$i]['id'];
 
-          $sql_data_array = array('orders_status_name' => tep_db_prepare_input($orders_status_name_array[$language_id]),
+          $sql_data_array = array('orders_status_name' => HTML::sanitize($orders_status_name_array[$language_id]),
                                   'public_flag' => ((isset($_POST['public_flag']) && ($_POST['public_flag'] == '1')) ? '1' : '0'),
                                   'downloads_flag' => ((isset($_POST['downloads_flag']) && ($_POST['downloads_flag'] == '1')) ? '1' : '0'));
 
           if ($action == 'insert') {
             if (empty($orders_status_id)) {
-              $next_id_query = tep_db_query("select max(orders_status_id) as orders_status_id from " . TABLE_ORDERS_STATUS . "");
-              $next_id = tep_db_fetch_array($next_id_query);
-              $orders_status_id = $next_id['orders_status_id'] + 1;
+              $Qnext = $OSCOM_Db->get('orders_status', 'max(orders_status_id) as orders_status_id');
+              $orders_status_id = $Qnext->valueInt('orders_status_id') + 1;
             }
 
             $insert_sql_data = array('orders_status_id' => $orders_status_id,
@@ -44,49 +47,59 @@
 
             $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
 
-            tep_db_perform(TABLE_ORDERS_STATUS, $sql_data_array);
+            $OSCOM_Db->save('orders_status', $sql_data_array);
           } elseif ($action == 'save') {
-            tep_db_perform(TABLE_ORDERS_STATUS, $sql_data_array, 'update', "orders_status_id = '" . (int)$orders_status_id . "' and language_id = '" . (int)$language_id . "'");
+            $OSCOM_Db->save('orders_status',
+              $sql_data_array,
+            [
+              'orders_status_id' => (int)$orders_status_id,
+              'language_id' => (int)$language_id
+            ]);
           }
         }
 
         if (isset($_POST['default']) && ($_POST['default'] == 'on')) {
-          tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '" . tep_db_input($orders_status_id) . "' where configuration_key = 'DEFAULT_ORDERS_STATUS_ID'");
+          $OSCOM_Db->save('configuration', [
+            'configuration_value' => $orders_status_id
+          ], [
+            'configuration_key' => 'DEFAULT_ORDERS_STATUS_ID'
+          ]);
         }
 
         OSCOM::redirect(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page'] . '&oID=' . $orders_status_id);
         break;
       case 'deleteconfirm':
-        $oID = tep_db_prepare_input($_GET['oID']);
+        $oID = HTML::sanitize($_GET['oID']);
 
-        $orders_status_query = tep_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'DEFAULT_ORDERS_STATUS_ID'");
-        $orders_status = tep_db_fetch_array($orders_status_query);
+        $Qstatus = $OSCOM_Db->get('configuration', 'configuration_value', ['configuration_key' => 'DEFAULT_ORDERS_STATUS_ID']);
 
-        if ($orders_status['configuration_value'] == $oID) {
-          tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '' where configuration_key = 'DEFAULT_ORDERS_STATUS_ID'");
+        if ($Qstatus->value('configuration_value') == $oID) {
+          $OSCOM_Db->save('configuration', [
+            'configuration_value' => ''
+          ], [
+            'configuration_key' => 'DEFAULT_ORDERS_STATUS_ID'
+          ]);
         }
 
-        tep_db_query("delete from " . TABLE_ORDERS_STATUS . " where orders_status_id = '" . tep_db_input($oID) . "'");
+        $OSCOM_Db->delete('orders_status', ['orders_status_id' => $oID]);
 
         OSCOM::redirect(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page']);
         break;
       case 'delete':
-        $oID = tep_db_prepare_input($_GET['oID']);
+        $oID = HTML::sanitize($_GET['oID']);
 
-        $status_query = tep_db_query("select count(*) as count from " . TABLE_ORDERS . " where orders_status = '" . (int)$oID . "'");
-        $status = tep_db_fetch_array($status_query);
+        $Qstatus = $OSCOM_Db->get('orders', 'orders_status', ['orders_status' => (int)$oID], null, 1);
 
         $remove_status = true;
         if ($oID == DEFAULT_ORDERS_STATUS_ID) {
           $remove_status = false;
           $messageStack->add(ERROR_REMOVE_DEFAULT_ORDER_STATUS, 'error');
-        } elseif ($status['count'] > 0) {
+        } elseif ($Qstatus->fetch() !== false) {
           $remove_status = false;
           $messageStack->add(ERROR_STATUS_USED_IN_ORDERS, 'error');
         } else {
-          $history_query = tep_db_query("select count(*) as count from " . TABLE_ORDERS_STATUS_HISTORY . " where orders_status_id = '" . (int)$oID . "'");
-          $history = tep_db_fetch_array($history_query);
-          if ($history['count'] > 0) {
+          $Qhistory = $OSCOM_Db->get('orders_status_history', 'orders_status_id', ['orders_status_id' => (int)$oID], null, 1);
+          if ($Qhistory->fetch() !== false) {
             $remove_status = false;
             $messageStack->add(ERROR_STATUS_USED_IN_HISTORY, 'error');
           }
@@ -117,29 +130,31 @@
                 <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</td>
               </tr>
 <?php
-  $orders_status_query_raw = "select * from " . TABLE_ORDERS_STATUS . " where language_id = '" . (int)$languages_id . "' order by orders_status_id";
-  $orders_status_split = new splitPageResults($_GET['page'], MAX_DISPLAY_SEARCH_RESULTS, $orders_status_query_raw, $orders_status_query_numrows);
-  $orders_status_query = tep_db_query($orders_status_query_raw);
-  while ($orders_status = tep_db_fetch_array($orders_status_query)) {
-    if ((!isset($_GET['oID']) || (isset($_GET['oID']) && ($_GET['oID'] == $orders_status['orders_status_id']))) && !isset($oInfo) && (substr($action, 0, 3) != 'new')) {
-      $oInfo = new objectInfo($orders_status);
+  $Qstatus = $OSCOM_Db->prepare('select SQL_CALC_FOUND_ROWS * from :table_orders_status where language_id = :language_id order by orders_status_id limit :page_set_offset, :page_set_max_results');
+  $Qstatus->bindInt(':language_id', $_SESSION['languages_id']);
+  $Qstatus->setPageSet(MAX_DISPLAY_SEARCH_RESULTS);
+  $Qstatus->execute();
+
+  while ($Qstatus->fetch()) {
+    if ((!isset($_GET['oID']) || (isset($_GET['oID']) && ((int)$_GET['oID'] === $Qstatus->valueInt('orders_status_id')))) && !isset($oInfo) && (substr($action, 0, 3) != 'new')) {
+      $oInfo = new objectInfo($Qstatus->toArray());
     }
 
-    if (isset($oInfo) && is_object($oInfo) && ($orders_status['orders_status_id'] == $oInfo->orders_status_id)) {
+    if (isset($oInfo) && is_object($oInfo) && ($Qstatus->valueInt('orders_status_id') === (int)$oInfo->orders_status_id)) {
       echo '                  <tr id="defaultSelected" class="dataTableRowSelected" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . OSCOM::link(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page'] . '&oID=' . $oInfo->orders_status_id . '&action=edit') . '\'">' . "\n";
     } else {
-      echo '                  <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . OSCOM::link(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page'] . '&oID=' . $orders_status['orders_status_id']) . '\'">' . "\n";
+      echo '                  <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . OSCOM::link(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page'] . '&oID=' . $Qstatus->valueInt('orders_status_id')) . '\'">' . "\n";
     }
 
-    if (DEFAULT_ORDERS_STATUS_ID == $orders_status['orders_status_id']) {
-      echo '                <td class="dataTableContent"><strong>' . $orders_status['orders_status_name'] . ' (' . TEXT_DEFAULT . ')</strong></td>' . "\n";
+    if ((int)DEFAULT_ORDERS_STATUS_ID == $Qstatus->valueInt('orders_status_id')) {
+      echo '                <td class="dataTableContent"><strong>' . $Qstatus->value('orders_status_name') . ' (' . TEXT_DEFAULT . ')</strong></td>' . "\n";
     } else {
-      echo '                <td class="dataTableContent">' . $orders_status['orders_status_name'] . '</td>' . "\n";
+      echo '                <td class="dataTableContent">' . $Qstatus->value('orders_status_name') . '</td>' . "\n";
     }
 ?>
-                <td class="dataTableContent" align="center"><?php echo HTML::image(DIR_WS_IMAGES . 'icons/' . (($orders_status['public_flag'] == '1') ? 'tick.gif' : 'cross.gif')); ?></td>
-                <td class="dataTableContent" align="center"><?php echo HTML::image(DIR_WS_IMAGES . 'icons/' . (($orders_status['downloads_flag'] == '1') ? 'tick.gif' : 'cross.gif')); ?></td>
-                <td class="dataTableContent" align="right"><?php if (isset($oInfo) && is_object($oInfo) && ($orders_status['orders_status_id'] == $oInfo->orders_status_id)) { echo HTML::image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); } else { echo '<a href="' . OSCOM::link(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page'] . '&oID=' . $orders_status['orders_status_id']) . '">' . HTML::image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
+                <td class="dataTableContent" align="center"><?php echo HTML::image(DIR_WS_IMAGES . 'icons/' . (($Qstatus->valueInt('public_flag') === 1) ? 'tick.gif' : 'cross.gif')); ?></td>
+                <td class="dataTableContent" align="center"><?php echo HTML::image(DIR_WS_IMAGES . 'icons/' . (($Qstatus->valueInt('downloads_flag') === 1) ? 'tick.gif' : 'cross.gif')); ?></td>
+                <td class="dataTableContent" align="right"><?php if (isset($oInfo) && is_object($oInfo) && ($Qstatus->valueInt('orders_status_id') === (int)$oInfo->orders_status_id)) { echo HTML::image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); } else { echo '<a href="' . OSCOM::link(FILENAME_ORDERS_STATUS, 'page=' . $_GET['page'] . '&oID=' . $Qstatus->valueInt('orders_status_id')) . '">' . HTML::image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
               </tr>
 <?php
   }
@@ -147,8 +162,8 @@
               <tr>
                 <td colspan="4"><table border="0" width="100%" cellspacing="0" cellpadding="2">
                   <tr>
-                    <td class="smallText" valign="top"><?php echo $orders_status_split->display_count($orders_status_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $_GET['page'], TEXT_DISPLAY_NUMBER_OF_ORDERS_STATUS); ?></td>
-                    <td class="smallText" align="right"><?php echo $orders_status_split->display_links($orders_status_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $_GET['page']); ?></td>
+                    <td class="smallText" valign="top"><?php echo $Qstatus->getPageSetLabel(TEXT_DISPLAY_NUMBER_OF_ORDERS_STATUS); ?></td>
+                    <td class="smallText" align="right"><?php echo $Qstatus->getPageSetLinks(); ?></td>
                   </tr>
 <?php
   if (empty($action)) {
