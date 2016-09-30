39f74b9e12040c39b171e2cbf50a4e9e09c1c07d
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-09-30 13:50:01 +0200

    initialize variable

--- catalog/includes/modules/boxes/bm_best_sellers.php
+++ catalog/includes/modules/boxes/bm_best_sellers.php
@@ -64,6 +64,8 @@
             $bestsellers_list .= '<li><a href="' . OSCOM::link('product_info.php', 'products_id=' . $b['products_id']) . '"><span itemprop="itemListElement">' . $b['products_name'] . '</span></a></li>';
           }
 
+          $num_best_sellers = count($best);
+
           ob_start();
           include('includes/modules/boxes/templates/best_sellers.php');
           $data = ob_get_clean();
