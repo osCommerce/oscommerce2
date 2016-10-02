bc4e138928e9b8e43c6b27f27ad780a35ee62fd6
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-02 02:05:19 +0200

    add fa-fw css class to icons

--- catalog/includes/modules/pages/tp_account.php
+++ catalog/includes/modules/pages/tp_account.php
@@ -22,31 +22,31 @@
                                                                    'sort_order' => 10,
                                                                    'links' => array('edit' => array('title' => MY_ACCOUNT_INFORMATION,
                                                                                                     'link' => OSCOM::link('account_edit.php', '', 'SSL'),
-                                                                                                    'icon' => 'fa fa-user'),
+                                                                                                    'icon' => 'fa fa-fw fa-user'),
                                                                                     'address_book' => array('title' => MY_ACCOUNT_ADDRESS_BOOK,
                                                                                                             'link' => OSCOM::link('address_book.php', '', 'SSL'),
-                                                                                                            'icon' => 'fa fa-home'),
+                                                                                                            'icon' => 'fa fa-fw fa-home'),
                                                                                     'password' => array('title' => MY_ACCOUNT_PASSWORD,
                                                                                                         'link' => OSCOM::link('account_password.php', '', 'SSL'),
-                                                                                                        'icon' => 'fa fa-cog'))),
+                                                                                                        'icon' => 'fa fa-fw fa-cog'))),
                                                 'orders' => array('title' => MY_ORDERS_TITLE,
                                                                   'sort_order' => 20,
                                                                   'links' => array('history' => array('title' => MY_ORDERS_VIEW,
                                                                                                       'link' => OSCOM::link('account_history.php', '', 'SSL'),
-                                                                                                      'icon' => 'fa fa-shopping-cart'))),
+                                                                                                      'icon' => 'fa fa-fw fa-shopping-cart'))),
                                                 'notifications' => array('title' => EMAIL_NOTIFICATIONS_TITLE,
                                                                          'sort_order' => 30,
                                                                          'links' => array('newsletters' => array('title' => EMAIL_NOTIFICATIONS_NEWSLETTERS,
                                                                                                                  'link' => OSCOM::link('account_newsletters.php', '', 'SSL'),
-                                                                                                                 'icon' => 'fa fa-envelope'),
+                                                                                                                 'icon' => 'fa fa-fw fa-envelope'),
                                                                                           'products' => array('title' => EMAIL_NOTIFICATIONS_PRODUCTS,
                                                                                                               'link' => OSCOM::link('account_notifications.php', '', 'SSL'),
-                                                                                                              'icon' => 'fa fa-send'))));
+                                                                                                              'icon' => 'fa fa-fw fa-send'))));
     }
 
     function build() {
       global $oscTemplate;
-      
+
       foreach ( $oscTemplate->_data[$this->group] as $key => $row ) {
         $arr[$key] = $row['sort_order'];
       }
@@ -67,7 +67,7 @@
           }
 
           $output .= (tep_not_null($entry['link'])) ? '<a href="' . $entry['link'] . '">' . $entry['title'] . '</a>' : $entry['title'];
-          
+
           $output .= '    </li>';
         }
 
