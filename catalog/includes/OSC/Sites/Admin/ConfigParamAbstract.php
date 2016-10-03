94cfd6767ab5cdf44d3cdbc7e31cfbd7dea56887
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-03 04:03:40 +0200

    add general configuration parameter abstract class to the Admin Site for Apps to use for admin configuration parameters

---
+++ catalog/includes/OSC/Sites/Admin/ConfigParamAbstract.php
@@ -0,0 +1,71 @@
+<?php
+/**
+  * osCommerce Online Merchant
+  *
+  * @copyright (c) 2016 osCommerce; https://www.oscommerce.com
+  * @license BSD; https://www.oscommerce.com/bsdlicense.txt
+  */
+
+namespace OSC\Sites\Admin;
+
+use OSC\OM\HTML;
+
+abstract class ConfigParamAbstract
+{
+    protected $code;
+    protected $key_prefix;
+    protected $key;
+    public $title;
+    public $description;
+    public $default;
+    public $sort_order = 0;
+
+    abstract protected function init();
+
+    public function __construct()
+    {
+        $this->code = (new \ReflectionClass($this))->getShortName();
+
+        $this->key = $this->key_prefix . $this->code;
+
+        $this->init();
+    }
+
+    protected function getInputValue()
+    {
+        $key = strtoupper($this->key);
+        $value = defined($key) ? constant($key) : null;
+
+        if (!isset($value) && isset($this->default)) {
+            $value = $this->default;
+        }
+
+        return $value;
+    }
+
+    public function getInputField()
+    {
+        $input = HTML::inputField($this->key, $this->getInputValue());
+
+        return $input;
+    }
+
+    public function getSetField()
+    {
+        $input = $this->getInputField();
+
+        $result = <<<EOT
+<div class="row">
+  <h4>{$this->title}</h4>
+
+  <p>{$this->description}</p>
+
+  <div>
+    {$input}
+  </div>
+</div>
+EOT;
+
+        return $result;
+    }
+}
