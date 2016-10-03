421f5cef0999781ddd8656f2f0b27bef98ffd81c
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-03 04:02:34 +0200

    add app scope to language for Apps; add general saveCfgParam/deleteCfgParam functions to AppAbstract; keep track of page actions

--- catalog/includes/OSC/OM/AppAbstract.php
+++ catalog/includes/OSC/OM/AppAbstract.php
@@ -9,6 +9,7 @@
 namespace OSC\OM;
 
 use OSC\OM\OSCOM;
+use OSC\OM\Registry;
 
 abstract class AppAbstract
 {
@@ -18,10 +19,16 @@ abstract class AppAbstract
     public $version;
     public $modules = [];
 
+    public $db;
+    public $lang;
+
     abstract protected function init();
 
     final public function __construct() {
         $this->setInfo();
+
+        $this->db = Registry::get('Db');
+        $this->lang = Registry::get('Language');
 
         $this->init();
     }
@@ -113,5 +120,106 @@ abstract class AppAbstract
         $this->title = $json['title'];
         $this->version = $json['version'];
         $this->modules = $json['modules'];
+    }
+
+    final public function getDef()
+    {
+        $args = func_get_args();
+
+        if (!isset($args[0])) {
+            $args[0] = null;
+        }
+
+        if (!isset($args[1])) {
+            $args[1] = null;
+        }
+
+        if (!isset($args[2])) {
+            $args[2] = $this->vendor . '-' . $this->code;
+        }
+
+        return call_user_func_array([$this->lang, 'getDef'], $args);
+    }
+
+    final public function hasDefinitionFile($filename, $language = null)
+    {
+        $language = isset($language) ? basename($language) : basename($_SESSION['language']);
+
+        $pathname = OSCOM::BASE_DIR . 'OSC/Apps/' . $this->vendor . '/' . $this->code . '/languages/' . $language . '/' . $filename;
+
+        if (file_exists($pathname)) {
+            return true;
+        }
+
+        if ($language != 'english') {
+            return $this->hasDefinitionFile($filename, 'english');
+        }
+
+        return false;
+    }
+
+    final public function loadDefinitionFile($filename, $language = null)
+    {
+        $language = isset($language) ? basename($language) : basename($_SESSION['language']);
+
+        if ($language != 'english') {
+            $this->loadDefinitionFile($filename, 'english');
+        }
+
+        $pathname = OSCOM::BASE_DIR . 'OSC/Apps/' . $this->vendor . '/' . $this->code . '/languages/' . $language . '/' . $filename;
+
+        if (file_exists($pathname)) {
+            $this->lang->loadDefinitionsFromFile($pathname, $this->vendor . '-' . $this->code);
+        } else {
+            trigger_error('OSC\OM\AppAbstract::loadDefinitionFile() - Filename does not exist: ' . $pathname);
+        }
+    }
+
+    final public function saveCfgParam($key, $value, $title = null, $description = null, $set_func = null)
+    {
+        if (is_null($value)) {
+            $value = '';
+        }
+
+        if (!defined($key)) {
+            if (!isset($title)) {
+                $title = 'Parameter [' . $this->getTitle() . ']';
+            }
+
+            if (!isset($description)) {
+                $description = 'Parameter [' . $this->getTitle() . ']';
+            }
+
+            $data = [
+                'configuration_title' => $title,
+                'configuration_key' => $key,
+                'configuration_value' => $value,
+                'configuration_description' => $description,
+                'configuration_group_id' => '6',
+                'sort_order' => '0',
+                'date_added' => 'now()'
+            ];
+
+            if (isset($set_func)) {
+                $data['set_function'] = $set_func;
+            }
+
+            $this->db->save('configuration', $data);
+
+            define($key, $value);
+        } else {
+            $this->db->save('configuration', [
+                'configuration_value' => $value
+            ], [
+                'configuration_key' => $key
+            ]);
+        }
+    }
+
+    final public function deleteCfgParam($key)
+    {
+        $this->db->delete('configuration', [
+            'configuration_key' => $key
+        ]);
     }
 }
