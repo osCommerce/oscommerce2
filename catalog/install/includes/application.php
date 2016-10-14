afca0fc7208af8ac75c9d00ff1bb687994e5d0ab
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-14 15:34:08 +0200

    manually include passwordhash class

--- catalog/install/includes/application.php
+++ catalog/install/includes/application.php
@@ -20,5 +20,8 @@
   require(OSCOM_BASE_DIR . 'OM/OSCOM.php');
   spl_autoload_register('OSC\OM\OSCOM::autoload');
 
+// include manually as OSC\OM\Hash relies on OSCOM::getConfig('dir_root', 'Shop') which is currently empty
+  require(dirname(OSCOM_BASE_DIR) . '/classes/passwordhash.php');
+
   OSCOM::initialize();
 ?>
