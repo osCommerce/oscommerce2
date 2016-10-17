0f20b3a348fead6562440ab76cdfac73e418c75c
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-17 05:25:05 +0200

    fix detection of mysql (github issue 368)

--- catalog/install/templates/pages/index.php
+++ catalog/install/templates/pages/index.php
@@ -81,7 +81,7 @@
           <td colspan="2"><strong>Required PHP Extensions</strong></td>
         </tr>
         <tr>
-          <td>MySQL</td>
+          <td>MySQL<?php echo extension_loaded('mysqli') ? 'i' : ''; ?></td>
           <td align="right"><img src="images/<?php echo (extension_loaded('mysql') || extension_loaded('mysqli') ? 'success.gif' : 'failed.gif'); ?>" width="16" height="16" /></td>
         </tr>
       </table>
@@ -143,8 +143,8 @@
     }
   }
 
-  if (!extension_loaded('mysql')) {
-    $warning_array['mysql'] = 'The MySQL extension is required but is not installed. Please enable it to continue installation.';
+  if (!extension_loaded('mysql') && !extension_loaded('mysqli')) {
+    $warning_array['mysql'] = 'The MySQLi or older MySQL extension is required but is not installed. Please enable either to continue installation.';
   }
 
   if ((sizeof($configfile_array) > 0) || (sizeof($warning_array) > 0)) {
