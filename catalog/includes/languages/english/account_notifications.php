d3532ef68ab55c6ecf3a3f471e358b71c44100e6
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-17 05:07:50 +0200

    fix typo (github pr344)

--- catalog/includes/languages/english/account_notifications.php
+++ catalog/includes/languages/english/account_notifications.php
@@ -19,7 +19,7 @@ define('MY_NOTIFICATIONS_TITLE', 'My Product Notifications');
 define('MY_NOTIFICATIONS_DESCRIPTION', 'The product notification list allows you to stay up to date on products you find of interest.<br /><br />To be up to date on all product changes, select <strong>Global Product Notifications</strong>.');
 
 define('GLOBAL_NOTIFICATIONS_TITLE', 'Global Product Notifications');
-define('GLOBAL_NOTIFICATIONS_DESCRIPTION', 'Recieve notifications on all available products.');
+define('GLOBAL_NOTIFICATIONS_DESCRIPTION', 'Receive notifications on all available products.');
 
 define('NOTIFICATIONS_TITLE', 'Product Notifications');
 define('NOTIFICATIONS_DESCRIPTION', 'To remove a product notification, clear the products checkbox and click on Continue.');
