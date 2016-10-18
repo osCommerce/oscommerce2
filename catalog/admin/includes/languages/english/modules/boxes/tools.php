287fef61ffbd046f926926255c926af01d0d841e
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-10-18 04:29:50 +0200

    update admin server info page

--- catalog/admin/includes/languages/english/modules/boxes/tools.php
+++ catalog/admin/includes/languages/english/modules/boxes/tools.php
@@ -5,6 +5,7 @@ define('ADMIN_MENU_SHOP_TOOLS_BACKUP', 'Database Backup');
 define('ADMIN_MENU_SHOP_TOOLS_BANNER_MANAGER', 'Banner Manager');
 define('ADMIN_MENU_SHOP_TOOLS_CACHE', 'Cache Control');
 define('ADMIN_MENU_SHOP_TOOLS_ONLINE_UPDATE', 'Online Update');
+define('ADMIN_MENU_SHOP_TOOLS_SERVER_INFO', 'Server Info');
 
 define('BOX_HEADING_TOOLS', 'Tools');
 define('BOX_TOOLS_DEFINE_LANGUAGE', 'Define Languages');
