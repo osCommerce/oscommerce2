f4ee7a7ce6a078161ec9963167d10006ad11d2c2
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-11-05 19:16:07 +0100

    change to MIT license

--- catalog/admin/includes/filenames.php
+++ catalog/admin/includes/filenames.php
@@ -1,15 +1,4 @@
 <?php
-/*
-  $Id$
-
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2007 osCommerce
-
-  Released under the GNU General Public License
-*/
-
 // define the filenames used in the project
   define('FILENAME_ACTION_RECORDER', 'action_recorder.php');
   define('FILENAME_ADMINISTRATORS', 'administrators.php');
