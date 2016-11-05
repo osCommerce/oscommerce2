323ea01f2679c686644d81f3475d36964f1f6986
osc2nuke <osc2nuke@gmail.com>
2016-11-05 20:38:29 +0100

    Remove un-used language *.php files.

--- catalog/admin/includes/languages/english/apps.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HEADING_TITLE', 'Apps');
-
-define('TABLE_HEADING_APPS', 'Installed Apps');
-define('TABLE_HEADING_VENDOR', 'Vendor');
-define('TABLE_HEADING_VERSION', 'Version');
-?>
