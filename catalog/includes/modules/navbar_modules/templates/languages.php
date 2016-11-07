a8e16c4840220c84aeef4e4154530a2efb71a495
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-11-07 10:28:55 +0100

    replace L: with a language icon, and C: with a money icon

--- catalog/includes/modules/navbar_modules/templates/languages.php
+++ catalog/includes/modules/navbar_modules/templates/languages.php
@@ -3,29 +3,19 @@ use OSC\OM\HTML;
 use OSC\OM\OSCOM;
 use OSC\OM\Registry;
 
-    $OSCOM_Language = Registry::get('Language');
-
+$OSCOM_Language = Registry::get('Language');
 ?>
 
 <li class="dropdown">
-  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo OSCOM::getDef('module_navbar_languages_selected_language'); ?></a>
-
-<?php
-if (count($OSCOM_Language->getAll()) > 1) {
-?>
+  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="fa fa-fw fa-language"></span> <?php echo OSCOM::getDef('module_navbar_languages_selected_language'); ?></a>
 
   <ul class="dropdown-menu">
 
 <?php
-  foreach ($OSCOM_Language->getAll() as $code => $value) {
-    echo '<li><a href="' . OSCOM::link($PHP_SELF, tep_get_all_get_params(array('language', 'currency')) . 'language=' . $code) . '">' . HTML::image('includes/languages/' .  $value['directory'] . '/images/' . $value['image'], $value['name'], null, null, null, false) . ' ' . $value['name'] . '</a></li>';
-  }
+foreach ($OSCOM_Language->getAll() as $code => $value) {
+  echo '<li><a href="' . OSCOM::link($PHP_SELF, tep_get_all_get_params(array('language', 'currency')) . 'language=' . $code) . '">' . HTML::image('includes/languages/' .  $value['directory'] . '/images/' . $value['image'], $value['name'], null, null, null, false) . ' ' . $value['name'] . '</a></li>';
+}
 ?>
 
   </ul>
-
-<?php
-}
-
-?>
 </li>
