7023b7c4f62b2db40e7f9aeb81701718c5a6d1bf
gburton <oscshops@gmail.com>
2016-11-19 19:04:19 +0000

    Update Category Template
    Remove panel-body as not needed in list layout

--- catalog/includes/modules/boxes/templates/categories.php
+++ catalog/includes/modules/boxes/templates/categories.php
@@ -3,5 +3,5 @@ use OSC\OM\OSCOM;
 ?>
 <div class="panel panel-default">
   <div class="panel-heading"><?php echo OSCOM::getDef('module_boxes_categories_box_title'); ?></div>
-  <div class="panel-body"><?php echo $category_tree; ?></div>
+  <?php echo $category_tree; ?>
 </div>
