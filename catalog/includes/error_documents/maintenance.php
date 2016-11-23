76e25afd467569944e0689349047e357dd538e64
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-11-23 02:00:31 +0100

    new maintenance page; update 404 page

--- catalog/includes/error_documents/maintenance.php
+++ catalog/includes/error_documents/maintenance.php
@@ -9,7 +9,8 @@
 use OSC\OM\HTML;
 use OSC\OM\OSCOM;
 
-http_response_code(404);
+http_response_code(503);
+header('Retry-After: 300');
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +18,7 @@ http_response_code(404);
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Error - Page Not Found</title>
+<title>Maintenance</title>
 <link href="<?= OSCOM::link('Shop/ext/bootstrap/css/bootstrap.min.css', null, false); ?>" rel="stylesheet">
 <link href="<?= OSCOM::link('Shop/ext/font-awesome/4.7.0/css/font-awesome.min.css', null, false); ?>" rel="stylesheet">
 <script src="<?= OSCOM::link('Shop/ext/jquery/jquery-3.1.1.min.js', null, false); ?>"></script>
@@ -25,11 +26,11 @@ http_response_code(404);
 <body>
 <div class="container">
   <div class="jumbotron" style="margin-top: 40px;">
-    <h1>This Page is Missing</h1>
+    <h1>We'll be back soon!</h1>
 
-    <p>It looks like this page is missing. Please continue back to our website and try again.</p>
+    <p>We're currently working on and improving our website. We'll be back in a few moments..</p>
 
-    <p style="margin-top: 40px;"><?= HTML::button('Return to website', 'fa fa-chevron-right', OSCOM::link('index.php'), null, 'btn-primary'); ?></p>
+    <p style="margin-top: 40px;"><?= HTML::button('Return to website', 'fa fa-refresh', OSCOM::link('index.php'), null, 'btn-primary'); ?></p>
   </div>
 </div>
 <script src="<?= OSCOM::link('Shop/ext/bootstrap/js/bootstrap.min.js', null, false); ?>"></script>
